173b919f49e35a7701c71d98f907706e5347c5fc
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2012-11-07 22:24:09 -0500

    [cleanup]

--- mne_rt_server/ex_matlab/mne_ex_rt.m
+++ mne_rt_server/ex_matlab/mne_ex_rt.m
@@ -5,9 +5,9 @@ clc;
 addpath('../../../mne-matlab/matlab');
 
 %% connection information
-mne_rt_server_ip            =	'localhost';%'172.21.16.63';
-mne_rt_server_commandPort	=	48332;
-mne_rt_server_fiffDataPort	=	48333;
+mne_rt_server_ip            =	'172.21.16.63';%'localhost';%'172.21.16.63';
+mne_rt_server_commandPort	=	59827;
+mne_rt_server_fiffDataPort	=	55029;
 
 %% create command client
 t_cmdClient = mne_rt_cmd_client(mne_rt_server_ip, mne_rt_server_commandPort);
@@ -16,18 +16,19 @@ t_cmdClient = mne_rt_cmd_client(mne_rt_server_ip, mne_rt_server_commandPort);
 t_dataClient = mne_rt_data_client(mne_rt_server_ip, mne_rt_server_fiffDataPort);
 
 %% set data client alias -> for convinience (optional)
-t_dataClient.setClientAlias('mne_ex_matlab');
+t_dataClient.setClientAlias('mne_ex_matlab'); % used in option 2 later on
 
 %% example commands
-clistInfo = t_cmdClient.getClientList();
-fprintf('### Client List ###\n%s',clistInfo);
+t_clistInfo = t_cmdClient.getClientList();
+fprintf('### Client List ###\n%s',t_clistInfo);
 
 %% read meas info
-%AliasOrId = t_dataClient.getClientId();
-AliasOrId = 'mne_ex_matlab';
-t_cmdClient.requestMeasInfo(AliasOrId);
-pause(0.5); % replace that by end block meas
-info = t_dataClient.readInfo();
+% Option 1
+t_aliasOrId = t_dataClient.getClientId();
+% Option 2
+%t_aliasOrId = 'mne_ex_matlab';
+t_cmdClient.requestMeasInfo(t_aliasOrId);
+t_measInfo = t_dataClient.readInfo();
 
 %% close the sockets
 t_cmdClient.close();
