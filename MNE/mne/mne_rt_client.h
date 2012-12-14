452c682cf8723a3943906b47b6d34653db00f136
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2012-12-14 14:52:59 -0500

    Introducing RtClient real-time connection class

---
+++ MNE/mne/mne_rt_client.h
@@ -0,0 +1,166 @@
+//=============================================================================================================
+/**
+* @file     mne_rt_client.h
+* @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
+*           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
+*           To Be continued...
+*
+* @version  1.0
+* @date     July, 2012
+*
+* @section  LICENSE
+*
+* Copyright (C) 2012, Christoph Dinh and Matti Hamalainen. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+* the following conditions are met:
+*     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+*       following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+*       the following disclaimer in the documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Massachusetts General Hospital nor the names of its contributors may be used
+*       to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+* PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MASSACHUSETTS GENERAL HOSPITAL BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*
+* @brief    Contains the declaration of the MNERtClient Class.
+*
+*/
+
+#ifndef MNE_RT_CLIENT_H
+#define MNE_RT_CLIENT_H
+
+//*************************************************************************************************************
+//=============================================================================================================
+// MNE INCLUDES
+//=============================================================================================================
+
+#include "mne_global.h"
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// FIFF INCLUDES
+//=============================================================================================================
+
+#include "../fiff/fiff_info.h"
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// QT INCLUDES
+//=============================================================================================================
+
+#include <QThread>
+#include <QMutex>
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// Eigen INCLUDES
+//=============================================================================================================
+
+#include "../3rdParty/Eigen/Core"
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// DEFINE NAMESPACE MNELIB
+//=============================================================================================================
+
+namespace MNELIB
+{
+
+//*************************************************************************************************************
+//=============================================================================================================
+// USED NAMESPACES
+//=============================================================================================================
+
+using namespace Eigen;
+using namespace FIFFLIB;
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// FORWARD DECLARATIONS
+//=============================================================================================================
+
+
+//=============================================================================================================
+/**
+* DECLARE MNE RT CLIENT CLASS
+* @brief The MNERtClient class provides an interface to communicate with a running mne_rt_server.
+*/
+class MNESHARED_EXPORT MNERtClient : public QThread
+{
+    Q_OBJECT
+public:
+
+    //=========================================================================================================
+    /**
+    * Destroys the RtClient.
+    *
+    * @param[in] p_sRtServerHostname    The IP address of the mne_rt_server
+    * @param[in] parent                 Parent QObject (optional)
+    */
+    explicit MNERtClient(QString& p_sRtServerHostname,QObject *parent = 0);
+    
+
+    //=========================================================================================================
+    /**
+    * Destroys the RtClient.
+    */
+    ~MNERtClient();
+
+
+    //=========================================================================================================
+    /**
+    * Request Fiff Info
+    */
+    void requestFiffInfo();
+
+
+
+    //=========================================================================================================
+    /**
+    * Stops the RtClient by stopping the producer's thread.
+    *
+    * @return true if succeeded, false otherwise
+    */
+    virtual bool stop();
+
+protected:
+    //=========================================================================================================
+    /**
+    * The starting point for the thread. After calling start(), the newly created thread calls this function.
+    * Returning from this method will end the execution of the thread.
+    * Pure virtual method inherited by QThread.
+    */
+    virtual void run();
+
+private:
+    QMutex      mutex;
+
+    bool        m_bIsRunning;           /**< Holds whether RtClient is running.*/
+    bool        m_bFiffInfoRequest;     /**< Holds whether the fiff Info should be emmited via signal fiffInfoReceived.*/
+
+    QString     m_sRtServerHostName;    /**< The IP Adress of mne_rt_server.*/
+
+    FiffInfo*   m_pFiffInfo;
+
+signals:
+    void rawBufferReceived(Eigen::MatrixXf p_rawBuffer);
+    void fiffInfoReceived(FIFFLIB::FiffInfo p_fiffInfo);
+};
+
+} // NAMESPACE
+
+#endif // MNE_RT_CLIENT_H
