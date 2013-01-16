58810212e4038d79ed45806fa79d554479994bd3
cpieloth <pieloth@labp.htwk-leipzig.de>
2013-01-16 19:05:20 +0100

    [CHANGE] Start using rtProtocol library. (IN PROGRESS)

--- mne_rt_server/mne_rt_server/fiffstreamserver.cpp
+++ mne_rt_server/mne_rt_server/fiffstreamserver.cpp
@@ -1,54 +1,56 @@
 //=============================================================================================================
 /**
-* @file     fiffstreamserver.cpp
-* @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
-*           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
-* @version  1.0
-* @date     July, 2012
-*
-* @section  LICENSE
-*
-* Copyright (C) 2012, Christoph Dinh and Matti Hamalainen. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without modification, are permitted provided that
-* the following conditions are met:
-*     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
-*       following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
-*       the following disclaimer in the documentation and/or other materials provided with the distribution.
-*     * Neither the name of the Massachusetts General Hospital nor the names of its contributors may be used
-*       to endorse or promote products derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-* PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MASSACHUSETTS GENERAL HOSPITAL BE LIABLE FOR ANY DIRECT,
-* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*
-*
-* @brief     implementation of the FiffStreamServer Class.
-*
-*/
+ * @file     fiffstreamserver.cpp
+ * @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
+ *           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
+ * @version  1.0
+ * @date     July, 2012
+ *
+ * @section  LICENSE
+ *
+ * Copyright (C) 2012, Christoph Dinh and Matti Hamalainen. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ * the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *       following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *       the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Massachusetts General Hospital nor the names of its contributors may be used
+ *       to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MASSACHUSETTS GENERAL HOSPITAL BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * @brief     implementation of the FiffStreamServer Class.
+ *
+ */
 
 //*************************************************************************************************************
 //=============================================================================================================
 // INCLUDES
 //=============================================================================================================
-
+#include <rtProtocol/commandRequests/RTCommandRequest.h>
+#include <rtProtocol/commandRequests/RTCmdMeasInfo.h>
+#include <rtProtocol/RTDefaultCommands.h>
+#include <rtProtocol/RTProtocolDefinitions.h> // TODO(pieloth): Remove when string parsing is done by rtProtocol
 #include "fiffstreamserver.h"
 #include "fiffstreamthread.h"
-
 
 //*************************************************************************************************************
 //=============================================================================================================
 // STL INCLUDES
 //=============================================================================================================
-
+#include <iostream>
+#include <string>
 #include <stdlib.h>
-
 
 //*************************************************************************************************************
 //=============================================================================================================
@@ -58,19 +60,16 @@
 using namespace MSERVER;
 using namespace FIFFLIB;
 
-
 //*************************************************************************************************************
 //=============================================================================================================
 // DEFINE MEMBER METHODS
 //=============================================================================================================
 
-FiffStreamServer::FiffStreamServer(QObject *parent)
-: QTcpServer(parent)
-, m_iNextClientId(0)
+FiffStreamServer::FiffStreamServer( QObject *parent ) :
+                QTcpServer( parent ), m_iNextClientId( 0 )
 {
 
 }
-
 
 //*************************************************************************************************************
 
@@ -80,172 +79,176 @@ FiffStreamServer::~FiffStreamServer()
 //    clearClients();
 }
 
-
 //*************************************************************************************************************
 
 QByteArray FiffStreamServer::availableCommands()
 {
     QByteArray t_blockCmdInfoList;
-    t_blockCmdInfoList.append("\tclist\t\t\tprints and sends all available FiffStreamClients\r\n");
+    t_blockCmdInfoList.append( "\tclist\t\t\tprints and sends all available FiffStreamClients\r\n" );
     return t_blockCmdInfoList;
 }
 
-
 //*************************************************************************************************************
 
-bool FiffStreamServer::parseCommand(QStringList& p_sListCommand, QByteArray& p_blockOutputInfo)
+bool FiffStreamServer::parseCommand( QStringList& p_sListCommand, QByteArray& p_blockOutputInfo )
 {
     bool success = false;
 
-    if(p_sListCommand[0].compare("clist",Qt::CaseInsensitive) == 0)
+    // TODO(pieloth): use of QString instead of QStringList. All string conversion should be done by rtProtocol!
+    QString rawRequest = p_sListCommand.join( RTSTREAMING::ARG_SEPARATOR );
+    rawRequest.append(RTSTREAMING::ETX);
+    RTSTREAMING::RTCommandRequest::SPtr request;
+    request = RTSTREAMING::RTCommandRequest::parseRequest( rawRequest );
+    // TODO(pieloth): use of request->getCommand(); in cout stream!
+    const std::string cmd = request->getCommand().toStdString();
+
+    if( request->isCommand( RTSTREAMING::Command::REQUEST_CONNECTORS ) )
     {
         //
         // client list
         //
-        printf("clist\n");
+        std::cout << cmd << std::endl;
 
-        p_blockOutputInfo.append("\tID\tAlias\r\n");
-        QMap<qint32, FiffStreamThread*>::iterator i;
-        for (i = this->m_qClientList.begin(); i != this->m_qClientList.end(); ++i)
+        p_blockOutputInfo.append( "\tID\tAlias\r\n" );
+        QMap< qint32, FiffStreamThread* >::iterator i;
+        for( i = this->m_qClientList.begin(); i != this->m_qClientList.end(); ++i )
         {
-            QString str = QString("\t%1\t%2\r\n").arg(i.key()).arg(i.value()->getAlias());
-            p_blockOutputInfo.append(str);
+            QString str = QString( "\t%1\t%2\r\n" ).arg( i.key() ).arg( i.value()->getAlias() );
+            p_blockOutputInfo.append( str );
         }
-        p_blockOutputInfo.append("\n");
-        success = true;
+        p_blockOutputInfo.append( "\n" );
+
+        return true;
     }
-    else if(p_sListCommand[0].compare("measinfo",Qt::CaseInsensitive) == 0)
+    if( request->isCommand( RTSTREAMING::Command::REQUEST_MEASUREMENT_INFO ) )
     {
         //
         // Measurement Info
         //
-        if(p_sListCommand.size() > 1)
+        RTSTREAMING::RTCmdMeasInfo::SPtr req = request.dynamicCast< RTSTREAMING::RTCmdMeasInfo >();
+        const int id = req->getClientId();
+        if( id != RTSTREAMING::RTCmdMeasInfo::NO_CLIENT_ID )
         {
-            qint32 t_id = -1;
-//            p_blockOutputInfo.append(parseToId(p_sListCommand[1],t_id));
+            std::cout << cmd << " " << id << std::endl;
 
-            bool t_isInt;
-            t_id = p_sListCommand[1].toInt(&t_isInt);
+            emit requestMeasInfo( id );        //requestMeasInfo(t_id);
 
-            if(t_isInt)// ToDo Check whether ID is correct --> move this parsing to fiff stream server
+            QString str = QString( "\tsend measurement info to FiffStreamClient (ID: %1)\r\n\n" ).arg( id );
+            p_blockOutputInfo.append( str );
+            return true;
+        }
+        else
+        {
+            std::cout << cmd << ": No wrong client id!" << std::endl;
+            return false;
+        }
+    }
+    else
+        if( p_sListCommand[0].compare( "meas", Qt::CaseInsensitive ) == 0 )
+        {
+            //
+            // meas
+            //
+            if( p_sListCommand.size() > 1 )
             {
-                printf("measinfo %d\r\n", t_id);
+                qint32 t_id = -1;
+                p_blockOutputInfo.append( parseToId( p_sListCommand[1], t_id ) );
+
+                printf( "meas %d\n", t_id );
+                if( t_id != -1 )
+                {
+                    emit startMeasFiffStreamClient( t_id );
+
+                    QString str = QString( "\tFiffStreamClient (ID: %1) is now set to accept raw buffers\r\n\n" ).arg( t_id );
+                    p_blockOutputInfo.append( str );
+                }
             }
-            if(t_id != -1)
+            success = true;
+        }
+        else
+            if( p_sListCommand[0].compare( "stop", Qt::CaseInsensitive ) == 0 )
             {
-                emit requestMeasInfo(t_id);//requestMeasInfo(t_id);
+                //
+                // stop
+                //
 
-                QString str = QString("\tsend measurement info to FiffStreamClient (ID: %1)\r\n\n").arg(t_id);
-                p_blockOutputInfo.append(str);
+                if( p_sListCommand.size() > 1 )
+                {
+                    qint32 t_id = -1;
+                    p_blockOutputInfo.append( parseToId( p_sListCommand[1], t_id ) );
+
+                    printf( "stop %d\n", t_id );
+
+                    if( t_id != -1 )
+                    {
+                        emit stopMeasFiffStreamClient( t_id );        //emit requestStopMeas(t_id);
+
+                        QString str = QString( "\tstop FiffStreamClient (ID: %1) from receiving raw Buffers.\r\n\n" ).arg( t_id );
+                        p_blockOutputInfo.append( str );
+                    }
+                }
                 success = true;
             }
-        }
-    }
-    else if(p_sListCommand[0].compare("meas",Qt::CaseInsensitive) == 0)
-    {
-        //
-        // meas
-        //
-        if(p_sListCommand.size() > 1)
-        {
-            qint32 t_id = -1;
-            p_blockOutputInfo.append(parseToId(p_sListCommand[1],t_id));
+            else
+                if( p_sListCommand[0].compare( "stop-all", Qt::CaseInsensitive ) == 0 )
+                {
+                    //
+                    // stop-all
+                    //
+                    emit stopMeasFiffStreamClient( -1 );
 
-            printf("meas %d\n", t_id);
-            if(t_id != -1)
-            {
-                emit startMeasFiffStreamClient(t_id);
+                    QString str = QString( "\tstop all FiffStreamClients from receiving raw buffers\r\n\n" );
+                    p_blockOutputInfo.append( str );
 
-                QString str = QString("\tFiffStreamClient (ID: %1) is now set to accept raw buffers\r\n\n").arg(t_id);
-                p_blockOutputInfo.append(str);
-            }
-        }
-        success = true;
-    }
-    else if(p_sListCommand[0].compare("stop",Qt::CaseInsensitive) == 0)
-    {
-        //
-        // stop
-        //
-
-        if(p_sListCommand.size() > 1)
-        {
-            qint32 t_id = -1;
-            p_blockOutputInfo.append(parseToId(p_sListCommand[1],t_id));
-
-            printf("stop %d\n", t_id);
-
-            if(t_id != -1)
-            {
-                emit stopMeasFiffStreamClient(t_id);//emit requestStopMeas(t_id);
-
-                QString str = QString("\tstop FiffStreamClient (ID: %1) from receiving raw Buffers.\r\n\n").arg(t_id);
-                p_blockOutputInfo.append(str);
-            }
-        }
-        success = true;
-    }
-    else if(p_sListCommand[0].compare("stop-all",Qt::CaseInsensitive) == 0)
-    {
-        //
-        // stop-all
-        //
-        emit stopMeasFiffStreamClient(-1);
-
-        QString str = QString("\tstop all FiffStreamClients from receiving raw buffers\r\n\n");
-        p_blockOutputInfo.append(str);
-
-        success = true;
-    }
+                    success = true;
+                }
 
     return success;
 }
 
-
 //*************************************************************************************************************
 
-QByteArray FiffStreamServer::parseToId(QString& p_sRawId, qint32& p_iParsedId)
+QByteArray FiffStreamServer::parseToId( QString& p_sRawId, qint32& p_iParsedId )
 {
     p_iParsedId = -1;
     QByteArray t_blockCmdIdInfo;
-    if(!p_sRawId.isEmpty())
+    if( !p_sRawId.isEmpty() )
     {
         bool t_isInt;
-        qint32 t_id = p_sRawId.toInt(&t_isInt);
+        qint32 t_id = p_sRawId.toInt( &t_isInt );
 
-        if(t_isInt && this->m_qClientList.contains(t_id))
+        if( t_isInt && this->m_qClientList.contains( t_id ) )
         {
             p_iParsedId = t_id;
         }
         else
         {
-            QMap<qint32, FiffStreamThread*>::iterator i;
-            for (i = this->m_qClientList.begin(); i != this->m_qClientList.end(); ++i)
+            QMap< qint32, FiffStreamThread* >::iterator i;
+            for( i = this->m_qClientList.begin(); i != this->m_qClientList.end(); ++i )
             {
-                if(i.value()->getAlias().compare(p_sRawId) == 0)
+                if( i.value()->getAlias().compare( p_sRawId ) == 0 )
                 {
                     p_iParsedId = i.key();
-                    QString str = QString("\tconvert alias '%1' => %2\r\n").arg(i.value()->getAlias()).arg(i.key());
-                    t_blockCmdIdInfo.append(str);
+                    QString str = QString( "\tconvert alias '%1' => %2\r\n" ).arg( i.value()->getAlias() ).arg( i.key() );
+                    t_blockCmdIdInfo.append( str );
                     break;
                 }
             }
         }
     }
 
-    if(p_iParsedId != -1)
+    if( p_iParsedId != -1 )
     {
-        QString str = QString("\tselect FiffStreamClient %1\r\n").arg(p_iParsedId);
-        t_blockCmdIdInfo.append(str);
+        QString str = QString( "\tselect FiffStreamClient %1\r\n" ).arg( p_iParsedId );
+        t_blockCmdIdInfo.append( str );
     }
     else
     {
-        t_blockCmdIdInfo.append("\twarning: requested FiffStreamClient not available\r\n\n");
+        t_blockCmdIdInfo.append( "\twarning: requested FiffStreamClient not available\r\n\n" );
     }
 
     return t_blockCmdIdInfo;
 }
-
 
 //*************************************************************************************************************
 
@@ -260,36 +263,32 @@ QByteArray FiffStreamServer::parseToId(QString& p_sRawId, qint32& p_iParsedId)
 //    m_qClientList.clear();
 //}
 
-
 //*************************************************************************************************************
 
-void FiffStreamServer::forwardMeasInfo(qint32 ID, FiffInfo::SDPtr p_FiffInfo)
+void FiffStreamServer::forwardMeasInfo( qint32 ID, FiffInfo::SDPtr p_FiffInfo )
 {
-    emit remitMeasInfo(ID, p_FiffInfo);
+    emit remitMeasInfo( ID, p_FiffInfo );
 }
-
 
 //*************************************************************************************************************
 //ToDo increase preformance --> try inline
-void FiffStreamServer::forwardRawBuffer(Eigen::MatrixXf m_matRawData)
+void FiffStreamServer::forwardRawBuffer( Eigen::MatrixXf m_matRawData )
 {
-    emit remitRawBuffer(m_matRawData);
+    emit remitRawBuffer( m_matRawData );
 }
-
-
 
 //*************************************************************************************************************
 
-void FiffStreamServer::incomingConnection(qintptr socketDescriptor)
+void FiffStreamServer::incomingConnection( qintptr socketDescriptor )
 {
-    FiffStreamThread* t_pStreamThread = new FiffStreamThread(m_iNextClientId, socketDescriptor, this);
+    FiffStreamThread* t_pStreamThread = new FiffStreamThread( m_iNextClientId, socketDescriptor, this );
 
-    m_qClientList.insert(m_iNextClientId, t_pStreamThread);
+    m_qClientList.insert( m_iNextClientId, t_pStreamThread );
     ++m_iNextClientId;
 
     //when thread has finished it gets deleted
-    connect(t_pStreamThread, SIGNAL(finished()), t_pStreamThread, SLOT(deleteLater()));
-    connect(this, SIGNAL(closeFiffStreamServer()), t_pStreamThread, SLOT(deleteLater()));
+    connect( t_pStreamThread, SIGNAL(finished()), t_pStreamThread, SLOT(deleteLater()) );
+    connect( this, SIGNAL(closeFiffStreamServer()), t_pStreamThread, SLOT(deleteLater()) );
 
     t_pStreamThread->start();
 }
