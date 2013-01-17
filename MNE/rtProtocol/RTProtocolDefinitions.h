09ee896544a0a572dbcf5a90eb1de81d42a38d1f
cpieloth <pieloth@labp.htwk-leipzig.de>
2013-01-17 15:38:16 +0100

    [ADD][CHANGE][STYLE] Extract create/parse methods for command request to protocol definitions. So it can be used without request objects.

--- MNE/rtProtocol/RTProtocolDefinitions.h
+++ MNE/rtProtocol/RTProtocolDefinitions.h
@@ -12,23 +12,48 @@
 
 namespace RTSTREAMING
 {
-    typedef QString CommandRequestT; /**< Type for a complete command request packet */
+typedef QString CommandRequestT; /**< Type for a complete command request packet */
 
-    typedef CommandRequestT CommandT; /**< Type for a request command. */
-    typedef CommandRequestT CommandArgT; /**< Type for an argument of a request command. */
-    typedef std::list< CommandArgT > CommandArgListT; /**< Type for an argument list of a request command. */
+typedef CommandRequestT CommandT; /**< Type for a request command. */
+typedef CommandRequestT CommandArgT; /**< Type for an argument of a request command. */
+typedef std::list<CommandArgT> CommandArgListT; /**< Type for an argument list of a request command. */
 
-    // TODO test newline and termination characters
-    const CommandRequestT ARG_SEPARATOR = " "; /** Separator for arguments. */
-    const CommandRequestT DESC_SEPARATOR = "\t"; /** Separator for description text. */
-    const CommandRequestT NEWLINE = "\n"; /** Telnet newline character. */
-    const CommandRequestT ETX = "\x0D\x0A"; /**< Telnet "message" termination: CRLF */
+// TODO test newline and termination characters
+const CommandRequestT ARG_SEPARATOR = " "; /** Separator for arguments. */
+const CommandRequestT DESC_SEPARATOR = "\t"; /** Separator for description text. */
+const CommandRequestT NEWLINE = "\n"; /** Telnet newline character. */
+const CommandRequestT ETX = "\x0D\x0A"; /**< Telnet "message" termination: CRLF */
 
-    typedef quint8 DataTypeT; /**< Type for packet header field "Data Type". */
-    typedef unsigned char DataT; /**< Type for user data (byte). */
-    typedef quint64 SizeT; /**< Type for packet header field "Data length". */
+typedef quint8 DataTypeT; /**< Type for packet header field "Data Type". */
+typedef unsigned char DataT; /**< Type for user data (byte). */
+typedef quint64 SizeT; /**< Type for packet header field "Data length". */
 
-    const DataT EOT = 0x04; /**< Termination byte for user data sequence. */
+const DataT EOT = 0x04; /**< Termination byte for user data sequence. */
+
+/**
+ * Creates a request packet out of the command and the argument list.
+ * Low-level API to create a command request.
+ *
+ * @param   cmd Command for request.
+ * @param   args A list of arguments for the request.
+ *
+ * @return  Ready-to-send request packet
+ */
+CommandRequestT createCommandRequest(const CommandT& cmd,
+        const CommandArgListT& args);
+
+/**
+ * Splits an incoming CommandRequestT packet to the CommandT and CommandArgListT parts.
+ * Low-level API to parse a command request.
+ *
+ * @param   req Request to parse.
+ * @param   cmd Pointer to store the command.
+ * @param   args Pointer to store argument list. Remaining items are lost!
+ *
+ * @return  true on success, false on error e.g. wrong structure.
+ */
+bool parseCommandRequest(const CommandRequestT& req, CommandT* cmd,
+        CommandArgListT* args);
 }
 
 #endif /* RTPROTOCOLDEFINITIONS_H_ */
--- MNE/rtProtocol/commandRequests/RTCommandRequest.cpp
+++ MNE/rtProtocol/commandRequests/RTCommandRequest.cpp
@@ -2,8 +2,7 @@
  * @author  Christof Pieloth
  */
 
-#include <QStringList>
-
+#include "../RTProtocolDefinitions.h"
 #include "../RTDefaultCommands.h"
 
 #include "RTCommandRequest.h"
@@ -17,139 +16,116 @@
 
 namespace RTSTREAMING
 {
-    RTCommandRequest::RTCommandRequest( CommandT cmd ) :
-                    m_cmd( cmd )
+RTCommandRequest::RTCommandRequest(CommandT cmd) :
+        m_cmd(cmd)
+{
+}
+
+RTCommandRequest::~RTCommandRequest()
+{
+}
+
+CommandT RTCommandRequest::getCommand() const
+{
+    return m_cmd;
+}
+
+bool RTCommandRequest::isCommand(const CommandT& cmd) const
+{
+    return m_cmd.compare(cmd) == 0;
+}
+
+CommandArgListT RTCommandRequest::getArguments() const
+{
+    return m_args;
+}
+
+void RTCommandRequest::setArguments(CommandArgListT args)
+{
+    m_args = args;
+}
+
+void RTCommandRequest::addArgument(CommandArgT arg)
+{
+    m_args.push_back(arg);
+}
+
+CommandRequestT RTCommandRequest::getRequest()
+{
+    CommandRequestT req = createCommandRequest(m_cmd, m_args);
+    return req;
+}
+
+RTCommandRequest::SPtr RTCommandRequest::parseRequest(
+        const CommandRequestT& req)
+{
+    RTCommandRequest::SPtr cmdRequest = RTCmdUnkown::SPtr(new RTCmdUnkown());
+
+    CommandT cmd;
+    CommandArgListT args;
+    if (!parseCommandRequest(req, &cmd, &args))
     {
-    }
-
-    RTCommandRequest::~RTCommandRequest()
-    {
-    }
-
-    CommandT RTCommandRequest::getCommand() const
-    {
-        return m_cmd;
-    }
-
-    bool RTCommandRequest::isCommand( const CommandT& cmd ) const
-    {
-        return m_cmd.compare( cmd ) == 0;
-    }
-
-    CommandArgListT RTCommandRequest::getArguments() const
-    {
-        return m_args;
-    }
-
-    void RTCommandRequest::setArguments( CommandArgListT args )
-    {
-        m_args = args;
-    }
-
-    void RTCommandRequest::addArgument( CommandArgT arg )
-    {
-        m_args.push_back( arg );
-    }
-
-    CommandRequestT RTCommandRequest::getRequest()
-    {
-        CommandRequestT request;
-        request.append( m_cmd );
-        if( !m_args.empty() )
-        {
-            CommandArgListT::iterator it = m_args.begin();
-            for( ; it != m_args.end(); ++it )
-            {
-                request.append( ARG_SEPARATOR );
-                request.append( *it );
-            }
-        }
-        request.append( ETX );
-        return request;
-    }
-
-    RTCommandRequest::SPtr RTCommandRequest::parseRequest( const CommandRequestT& req )
-    {
-        RTCommandRequest::SPtr cmdRequest = RTCmdUnkown::SPtr( new RTCmdUnkown() );
-        if( !req.endsWith( ETX ) )
-        {
-            return cmdRequest;
-        }
-
-        CommandRequestT request = req;
-        request.remove( ETX );
-        // TODO(pieloth): code smell - CommandRequestT -> QStringList
-        QStringList splits = request.split( ARG_SEPARATOR );
-
-        if( splits.empty() )
-        {
-            return cmdRequest;
-        }
-        QStringList::iterator it = splits.begin();
-        const CommandT cmd = *it;
-
-        cmdRequest = RTCommandRequest::getDefaultCommandInstance( cmd );
-
-        ++it;
-        for( ; it != splits.end(); ++it )
-        {
-            cmdRequest->addArgument( *it );
-        }
-
         return cmdRequest;
     }
 
-    RTCommandRequest::SPtr RTCommandRequest::getDefaultCommandInstance( const CommandT& cmd )
+    cmdRequest = RTCommandRequest::getDefaultCommandInstance(cmd);
+    cmdRequest->setArguments(args);
+
+    return cmdRequest;
+}
+
+RTCommandRequest::SPtr RTCommandRequest::getDefaultCommandInstance(
+        const CommandT& cmd)
+{
+    RTCommandRequest::SPtr request;
+
+    if (Command::REQUEST_CONNECTORS.compare(cmd) == 0)
     {
-        RTCommandRequest::SPtr request;
-
-        if( Command::REQUEST_CONNECTORS.compare( cmd ) == 0 )
-        {
-            request = RTCmdConList::SPtr( new RTCmdConList() );
-            return request;
-        }
-
-        if( Command::HELP.compare( cmd ) == 0 )
-        {
-            request = RTCmdHelp::SPtr( new RTCmdHelp() );
-            return request;
-        }
-        if( Command::REQUEST_MEASUREMENT_INFO.compare( cmd ) == 0 )
-        {
-            request = RTCmdMeasInfo::SPtr( new RTCmdMeasInfo() );
-            return request;
-        }
-        if( Command::SELECT_CONNECTOR.compare( cmd ) == 0 )
-        {
-            request = RTCmdSelCon::SPtr( new RTCmdSelCon() );
-            return request;
-        }
-        if( Command::START_MEASUREMENT.compare( cmd ) == 0 )
-        {
-            request = RTCmdStartMeas::SPtr( new RTCmdStartMeas() );
-            return request;
-        }
-        if( Command::STOP_ALL_MEASUREMENTS.compare( cmd ) == 0 )
-        {
-            request = RTCmdStopAll::SPtr( new RTCmdStopAll() );
-            return request;
-        }
-        // TODO(pieloth): Create instance of all default commands
-
-        request = RTCmdUnkown::SPtr( new RTCmdUnkown() );
+        request = RTCmdConList::SPtr(new RTCmdConList());
         return request;
     }
 
-    std::list< RTCommandRequest::SPtr > getDefaultCommandRequests()
+    if (Command::HELP.compare(cmd) == 0)
     {
-        std::list< CommandT > cmdList = Command::getDefaultCommands();
-        std::list< RTCommandRequest::SPtr > reqList;
-
-        std::list< CommandT >::iterator it = cmdList.begin();
-        for( ; it != cmdList.end(); ++it )
-        {
-            reqList.push_back( RTCommandRequest::getDefaultCommandInstance( *it ) );
-        }
-        return reqList;
+        request = RTCmdHelp::SPtr(new RTCmdHelp());
+        return request;
     }
+    if (Command::REQUEST_MEASUREMENT_INFO.compare(cmd) == 0)
+    {
+        request = RTCmdMeasInfo::SPtr(new RTCmdMeasInfo());
+        return request;
+    }
+    if (Command::SELECT_CONNECTOR.compare(cmd) == 0)
+    {
+        request = RTCmdSelCon::SPtr(new RTCmdSelCon());
+        return request;
+    }
+    if (Command::START_MEASUREMENT.compare(cmd) == 0)
+    {
+        request = RTCmdStartMeas::SPtr(new RTCmdStartMeas());
+        return request;
+    }
+    if (Command::STOP_ALL_MEASUREMENTS.compare(cmd) == 0)
+    {
+        request = RTCmdStopAll::SPtr(new RTCmdStopAll());
+        return request;
+    }
+    // TODO(pieloth): Create instance of all default commands
+
+    request = RTCmdUnkown::SPtr(new RTCmdUnkown());
+    return request;
+}
+
+std::list<RTCommandRequest::SPtr> getDefaultCommandRequests()
+{
+    std::list<CommandT> cmdList = Command::getDefaultCommands();
+    std::list<RTCommandRequest::SPtr> reqList;
+
+    std::list<CommandT>::iterator it = cmdList.begin();
+    for (; it != cmdList.end(); ++it)
+    {
+        reqList.push_back(RTCommandRequest::getDefaultCommandInstance(*it));
+    }
+    return reqList;
+}
 } /* namespace RTSTREAMING */
