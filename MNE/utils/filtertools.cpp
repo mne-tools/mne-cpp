d7ecbb860c130fe679775fdbdb58e0a21d1447a1
farndt <lix_fa@web.de>
2014-02-21 14:12:47 +0100

    recyling of [-Wmultichar, Wunused-but-set-variable, -Wparenthese, -Wsequence-point]

--- MNE/utils/filtertools.cpp
+++ MNE/utils/filtertools.cpp
@@ -65,17 +65,17 @@ FilterTools::FilterTools()
 
 void FilterTools::getStaticFilter(QString type, qint32 numberOfCoefficients, qint32 samplingRate, qint32 cutOffFreq, QVector<float> &impulseResponse)
 {
-    if(type == QString('HP'))
+    if(type == QString("HP"))
     {
         //HP from matlab
     }
 
-    if(type == QString('LP'))
+    if(type == QString("LP"))
     {
         //LP from matlab
     }
 
-    if(type == QString('BP'))
+    if(type == QString("BP"))
     {
         //BP from matlab
     }
@@ -106,18 +106,18 @@ void FilterTools::createDynamicFilter(QString type, qint32 numberOfCoefficients,
     }
 
     //Create final filter specified by the type parameter
-    if(type == QString('HP'))
+    if(type == QString("HP"))
     {
         for(int i=1; i<impulseResponse.size(); i=i+2)
             impulseResponse[i] = impulseResponse[i] * (-1);
     }
 
-    if(type == QString('LP'))
+    if(type == QString("LP"))
     {
         impulseResponse = impulseResponse;
     }
 
-    if(type == QString('BP'))
+    if(type == QString("BP"))
     {
         t = 0;
         for(int i=0; i<numberOfCoefficients; i++)
