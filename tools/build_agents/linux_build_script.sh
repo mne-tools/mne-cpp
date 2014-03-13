613a2ca0edd504b9f7b7b1f2f99a913ba3e2aa3d
Felix Arndt <lix_fa@web.de>
2014-03-13 13:59:20 +0100

    adding the packing of binary packages

--- tools/build_agents/linux_build_script.sh
+++ tools/build_agents/linux_build_script.sh
@@ -1,3 +1,13 @@
+# startdir = mne-cpp/tools/build_agents/
 cd ../..
 qmake -r
 make -j4
+
+# dir = ../ mne-cpp
+cd ..
+
+# user package
+tar cfvz binaries_only_unix.tar.gz mne-cpp/bin mne-cpp/lib/
+
+# development package
+tar cfvz binaries_all_unix.tar.gz mne-cpp/
