6ca94bddbe730856ff680b9cf5d04105c0862fd4
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2014-03-13 12:11:24 +0100

    cleanup

--- applications/mne_x_libs/xDisp/roi.h
+++ applications/mne_x_libs/xDisp/roi.h
@@ -1,5 +1,40 @@
-#ifndef NODE_H
-#define NODE_H
+//=============================================================================================================
+/**
+* @file     roi.h
+* @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
+*           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
+* @version  1.0
+* @date     February, 2013
+*
+* @section  LICENSE
+*
+* Copyright (C) 2013, Christoph Dinh and Matti Hamalainen. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+* the following conditions are met:
+*     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+*       following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+*       the following disclaimer in the documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Massachusetts General Hospital nor the names of its contributors may be used
+*       to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+* PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MASSACHUSETTS GENERAL HOSPITAL BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*
+* @brief    Declaration of the Roi Class.
+*
+*/
+
+#ifndef ROI_H
+#define ROI_H
 
 //*************************************************************************************************************
 //=============================================================================================================
@@ -9,9 +44,20 @@
 #include "xdisp_global.h"
 
 
+//*************************************************************************************************************
+//=============================================================================================================
+// QT INCLUDES
+//=============================================================================================================
+
 #include <QGraphicsItem>
 #include <QGraphicsSvgItem>
 #include <QList>
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// FORWARD DECLARATIONS
+//=============================================================================================================
 
 class QGraphicsSceneMouseEvent;
 
@@ -24,12 +70,31 @@ class QGraphicsSceneMouseEvent;
 namespace XDISPLIB
 {
 
+
+//*************************************************************************************************************
+//=============================================================================================================
+// FORWARD DECLARATIONS
+//=============================================================================================================
+
 class RoiSelectionWidget;
 
 
+//=============================================================================================================
+/**
+* DECLARE CLASS Roi
+*
+* @brief The Roi class provides a vectorized region of interest.
+*/
 class XDISPSHARED_EXPORT Roi : public QGraphicsItem
 {
 public:
+    //=========================================================================================================
+    /**
+    * Constructs a Roi.
+    *
+    * @param [in] fileName              the region of interest file without .svg-ending
+    * @param [in] roiSelectionWidget    graphics view this item is attached to
+    */
     Roi(const QString & fileName, RoiSelectionWidget *roiSelectionWidget);
 
     enum { Type = UserType + 1 };
@@ -46,14 +111,12 @@ protected:
     void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
 
 private:
-    QPointF newPos;
-    RoiSelectionWidget *m_pRoiSelectionWidget;
+    RoiSelectionWidget *m_pRoiSelectionWidget;      /**< The graphics view this item is attached to */
 
-    QGraphicsSvgItem *m_pGraphicsSvgItem;
-
-    QGraphicsSvgItem *m_pGraphicsSvgItemActive;
+    QGraphicsSvgItem *m_pGraphicsSvgItem;           /**< The vectorized graphic item; default state */
+    QGraphicsSvgItem *m_pGraphicsSvgItemActive;     /**< The vectorized graphic item; active state */
 };
 
 } // NAMESPACE
 
-#endif // NODE_H
+#endif // ROI_H
