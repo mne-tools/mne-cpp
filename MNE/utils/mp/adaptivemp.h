87f7611467a8dbeba15fc213d1ee261ba1df17a8
LostSign <martin.henfling@tu-ilmenau.de>
2014-07-10 10:46:44 +0200

    chg: comments

--- MNE/utils/mp/adaptivemp.h
+++ MNE/utils/mp/adaptivemp.h
@@ -1,3 +1,39 @@
+//=============================================================================================================
+/**
+* @file     adaptivemp.h
+* @author   Martin Henfling <martin.henfling@tu-ilmenau.de>
+*           Daniel Knobl <daniel.knobl@tu-ilmenau.de>
+*
+* @version  1.0
+* @date     July, 2014
+*
+* ported to mne-cpp by Martin Henfling and Daniel Knobl in May 2014
+* original code was
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+* the following conditions are met:
+*     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+*       following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+*       the following disclaimer in the documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Massachusetts General Hospital nor the names of its contributors may be used
+*       to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+* PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MASSACHUSETTS GENERAL HOSPITAL BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*
+* @brief    ADAPIVEMP class declaration, providing the implemetation of the Matching Pursuit Algorithm
+*           introduced by Stephane Mallat and Zhifeng Zhang. Matlabimplemetation of Marcij Gratkowski is
+*           used as Source and reference.
+*
+*/
 //MATCHING PURSUIT
 
 #ifndef ADAPTIVEMP_H
@@ -5,29 +41,48 @@
 
 //*************************************************************************************************************
 //=============================================================================================================
-// INCLUDES
+// STL INCLUDES
 //=============================================================================================================
 
 #include <iostream>
 #include <vector>
 #include <math.h>
+
+//*************************************************************************************************************
+//=============================================================================================================
+// Utils INCLUDES
+//=============================================================================================================
+
 #include <utils/mp/atom.h>
 #include <utils/utils_global.h>
 
-//Eigen
+//*************************************************************************************************************
+//=============================================================================================================
+// Eigen INCLUDES
+//=============================================================================================================
+
 #include <Eigen/Core>
 #include <Eigen/SparseCore>
 #include <unsupported/Eigen/FFT>
 
+//*************************************************************************************************************
+//=============================================================================================================
+// DEFINE NAMESPACE UTILSLIB
+//=============================================================================================================
+
 namespace UTILSLIB
 {
+
+//*************************************************************************************************************
 //=============================================================================================================
-// NAMESPACES
+// USED NAMESPACES
+//=============================================================================================================
 
 using namespace Eigen;
 using namespace std;
 
-//*************************************************************************************************************
+
+//=============================================================================================================
 
 enum ReturnValue{RETURNATOM, RETURNPARAMETERS};
 
