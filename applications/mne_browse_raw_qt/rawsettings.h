2915eb784652e0d1a1c059fc40b582a4270c06d3
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2014-08-27 16:25:13 +0200

    - started to implement editable eventmodel

--- applications/mne_browse_raw_qt/rawsettings.h
+++ applications/mne_browse_raw_qt/rawsettings.h
@@ -99,11 +99,11 @@
 #define EVENT_MARKER_WIDTH 3 // in pixels
 
 //Event marker opacity
-#define EVENT_MARKER_OPACITY 85 // opacity of the markers and backgorunds in the table columns
+#define EVENT_MARKER_OPACITY 110 // opacity of the markers and backgorunds in the table columns. Range: 0...255
 
 //Data marker
 #define DATA_MARKER_WIDTH 3 // in pixels
-#define DATA_MARKER_OPACITY 100 // opacity of the data marker and backgorunds in the table columns
+#define DATA_MARKER_OPACITY 200 // opacity of the data marker and backgorunds in the table columns. Range: 0...255
 
 //*************************************************************************************************************
 //=============================================================================================================
