5b1e81c4fa68cfef8fd31a3666690cf231fb6415
LostSign <martin.henfling@tu-ilmenau.de>
2014-09-16 17:47:18 +0200

    chg:settings

--- examples/matchingPursuit/settingwindow.h
+++ examples/matchingPursuit/settingwindow.h
@@ -16,6 +16,8 @@ public:
     explicit settingwindow(QWidget *parent = 0);
     ~settingwindow();
     void set_values();
+signals:
+    void change_info_label();
 private slots:
     void on_btt_close_clicked();
     void on_btt_cancel_clicked();
@@ -25,6 +27,16 @@ private slots:
     void on_btt_contraction_default_clicked();
     void on_btt_full_contraction_default_clicked();
     // end
+
+    void on_sl_boost_valueChanged(int value);
+
+    void on_sl_boost_sliderPressed();
+
+    void on_sl_boost_fixDict_valueChanged(int value);
+
+    void on_sl_boost_fixDict_sliderPressed();
+
+    void on_btt_delta_energy_default_clicked();
 
 private:
     Ui::settingwindow *ui;
