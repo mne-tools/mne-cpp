d75ac10a0ed83a325d4449d251bb97d5fca66303
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2015-02-12 18:51:01 +0100

    - add qt3d 2.0 functionality to brainview class in disp3DNew - test functionality with disp3DNewTutorial

--- examples/disp3DTutorial/main.cpp
+++ examples/disp3DTutorial/main.cpp
@@ -39,7 +39,7 @@
 // INCLUDES
 //=============================================================================================================
 
-#include <disp3D/brainview.h>
+#include <disp3DNew/brainview.h>
 
 
 //*************************************************************************************************************
@@ -55,8 +55,8 @@
 // USED NAMESPACES
 //=============================================================================================================
 
-using namespace FSLIB;
-using namespace DISP3DLIB;
+//using namespace FSLIB;
+using namespace DISP3DNEWLIB;
 
 
 //*************************************************************************************************************
@@ -77,87 +77,9 @@ int main(int argc, char *argv[])
 {
     QGuiApplication a(argc, argv);
 
-    QStringList args = QCoreApplication::arguments();
-    int w_pos = args.indexOf("-width");
-    int h_pos = args.indexOf("-height");
-    int w, h;
-    if (w_pos >= 0 && h_pos >= 0)
-    {
-        bool ok = true;
-        w = args.at(w_pos + 1).toInt(&ok);
-        if (!ok)
-        {
-            qWarning() << "Could not parse width argument:" << args;
-            return 1;
-        }
-        h = args.at(h_pos + 1).toInt(&ok);
-        if (!ok)
-        {
-            qWarning() << "Could not parse height argument:" << args;
-            return 1;
-        }
-    }
+    BrainView testWindow;
 
-    //
-    // pial
-    //
-    BrainView t_pialBrainView("sample", 2, "pial", "./MNE-sample-data/subjects");
-
-    if (t_pialBrainView.stereoType() != QGLView::RedCyanAnaglyph)
-        t_pialBrainView.camera()->setEyeSeparation(0.3f);
-
-    if (w_pos >= 0 && h_pos >= 0)
-        t_pialBrainView.resize(w, h);
-    else
-        t_pialBrainView.resize(800, 600);
-
-    t_pialBrainView.setTitle(QString("Pial surface"));
-    t_pialBrainView.show();
-
-    //
-    // inflated
-    //
-    BrainView t_inflatedBrainView("sample", 2, "inflated", "./MNE-sample-data/subjects");
-
-    if (t_inflatedBrainView.stereoType() != QGLView::RedCyanAnaglyph)
-        t_inflatedBrainView.camera()->setEyeSeparation(0.3f);
-    if (w_pos >= 0 && h_pos >= 0)
-        t_inflatedBrainView.resize(w, h);
-    else
-        t_inflatedBrainView.resize(800, 600);
-
-    t_inflatedBrainView.setTitle(QString("Inflated surface"));
-    t_inflatedBrainView.show();
-
-    //
-    // orig
-    //
-    BrainView t_originBrainView("sample", 2, "orig", "./MNE-sample-data/subjects");
-
-    if (t_originBrainView.stereoType() != QGLView::RedCyanAnaglyph)
-        t_originBrainView.camera()->setEyeSeparation(0.3f);
-    if (w_pos >= 0 && h_pos >= 0)
-        t_originBrainView.resize(w, h);
-    else
-        t_originBrainView.resize(800, 600);
-
-    t_originBrainView.setTitle(QString("Orig surface"));
-    t_originBrainView.show();
-
-    //
-    // white
-    //
-    BrainView t_whiteBrainView("sample", 2, "white", "./MNE-sample-data/subjects");
-
-    if (t_whiteBrainView.stereoType() != QGLView::RedCyanAnaglyph)
-        t_whiteBrainView.camera()->setEyeSeparation(0.3f);
-    if (w_pos >= 0 && h_pos >= 0)
-        t_whiteBrainView.resize(w, h);
-    else
-        t_whiteBrainView.resize(800, 600);
-
-    t_whiteBrainView.setTitle(QString("White surface"));
-    t_whiteBrainView.show();
+    testWindow.show();
 
     return a.exec();
 }
