a0e8ceb8e79a1b80cf3ed97c0e075d1a9c152ac7
CBoensel <carsten.boensel@gmx.de>
2015-07-06 19:20:22 +0200

    add support to load own files via pushButton in a QFileDialog

--- examples/mriTestViewer/mriviewer.cpp
+++ examples/mriTestViewer/mriviewer.cpp
@@ -1,20 +1,42 @@
 #include "mriviewer.h"
 #include "ui_mriviewer.h"
 
+#include <QFileDialog>
+
 MriViewer::MriViewer(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MriViewer)
 {
     ui->setupUi(this);
 
-    scene = new QGraphicsScene(this);
-    ui->graphicsView->setScene(scene);
+    filePath = "D:/Bilder/Freunde/Lorenz_Esch.jpg";
+    loadFile(filePath);
+}
 
+void MriViewer::on_pushButton_clicked()
+{
+    filePath = QFileDialog::getOpenFileName(
+                this,
+                tr("Open File"),
+                "",
+                tr(defFileFormat)
+                );
+
+    loadFile(filePath);
+}
+
+void MriViewer::loadFile(QString filePath)
+{
     mriImage = new QImage();
-    mriImage->load("D:/Bilder/Freunde/Lorenz_Esch.jpg");
+    mriImage->load(filePath);
 
     mriPixmap = QPixmap::fromImage(*mriImage);
+
+    scene = new QGraphicsScene(this);
     scene->addPixmap(mriPixmap);
+    scene->setSceneRect(mriPixmap.rect());
+
+    ui->graphicsView->setScene(scene);
 }
 
 MriViewer::~MriViewer()
--- examples/mriTestViewer/mriviewer.h
+++ examples/mriTestViewer/mriviewer.h
@@ -7,6 +7,7 @@
 #include <QGraphicsScene>
 #include <QPixmap>
 #include <QImage>
+#include <QString>
 
 namespace Ui {
 class MriViewer;
@@ -20,11 +21,19 @@ public:
     explicit MriViewer(QWidget *parent = 0);
     ~MriViewer();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MriViewer *ui;
     QGraphicsScene *scene;
     QPixmap mriPixmap;
     QImage *mriImage;
+    QString filePath;
+    const char *defFileFormat = "JPEG (*.jpg *.jpeg);;"
+                            "PNG (*.png)";
+//    QString defFileFormat = "MGH (*.mgh *.mgz)";
+    void loadFile(QString filePath);
 };
 
 #endif // MRIVIEWER_H
