cabb82e535517940af15802b9a8e2f5264d2ecb8
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2015-07-13 10:35:25 -0400

    [MX-40] Fix problem with fiff initialization in channel selection manager, being part of the realtimeevokedwidget. Get rid of debug couts.

--- MNE/rtInv/rtave.cpp
+++ MNE/rtInv/rtave.cpp
@@ -215,12 +215,9 @@ void RtAve::assemblePostStimulus(const QList<QPair<QList<qint32>, MatrixXd> > &p
 
 void RtAve::assemblePreStimulus(const QList<QPair<QList<qint32>, MatrixXd> > &p_qListRawMatBuf, qint32 p_iStimIdx)
 {
-    std::cout<<"assemblePreStimulus::START"<<std::endl;
-
     QMutexLocker locker(&m_qMutex);
     if(m_iPreStimSamples > 0)
     {
-        std::cout<<"assemblePreStimulus::1"<<std::endl;
         // stimulus containing buffer of the assembled buffers
         float ratio = (float)m_iPreStimSamples/((float)(m_iPreStimSamples+m_iPostStimSamples));
         qint32 t_iBuffWithStim = floor((p_qListRawMatBuf.size()-1)*ratio) + 1;
@@ -228,24 +225,20 @@ void RtAve::assemblePreStimulus(const QList<QPair<QList<qint32>, MatrixXd> > &p_
         qint32 ncols = p_qListRawMatBuf[t_iBuffWithStim].second.cols();
 
         //Stimulus channel row
-        std::cout<<"assemblePreStimulus::2"<<std::endl;
         qint32 t_iRowIdx = m_qListStimChannelIdcs[p_iStimIdx];
 
         qint32 nSampleCount = m_iPreStimSamples;
-        std::cout<<"assemblePreStimulus::2.1"<<std::endl;
         MatrixXd t_matPreStim(nrows, m_iPreStimSamples);
         qint32 t_curBufIdx = t_iBuffWithStim;
 
         qint32 t_iStart = 0;
 
         qint32 pos = 0;
-        std::cout<<"assemblePreStimulus::2.2"<<std::endl;
         p_qListRawMatBuf[t_iBuffWithStim].second.row(t_iRowIdx).maxCoeff(&pos);
 
         //
         // assemble prestimulus
         //
-        std::cout<<"assemblePreStimulus::3"<<std::endl;
         // start from the stimulus itself
         if(pos > 0)
         {
@@ -263,7 +256,6 @@ void RtAve::assemblePreStimulus(const QList<QPair<QList<qint32>, MatrixXd> > &p_
         }
         --t_curBufIdx;
 
-        std::cout<<"assemblePreStimulus::4"<<std::endl;
         // remaining samples
         while(nSampleCount > 0)
         {
@@ -282,13 +274,11 @@ void RtAve::assemblePreStimulus(const QList<QPair<QList<qint32>, MatrixXd> > &p_
 
             --t_curBufIdx;
         }
-        std::cout<<"assemblePreStimulus::5"<<std::endl;
         //
         //Store in right pre stimulus buffer vector
         //
         m_qListQListPreStimBuf[p_iStimIdx].append(t_matPreStim);
     }
-    std::cout<<"assemblePreStimulus::END"<<std::endl;
 }
 
 
@@ -438,7 +428,6 @@ void RtAve::run()
             //
             // Reset when stim size changed
             //
-            std::cout<<"Reset when stim size changed"<<std::endl;
             m_qMutex.lock();
             if(m_iNewPreStimSamples != m_iPreStimSamples || m_iNewPostStimSamples != m_iPostStimSamples)
             {
@@ -489,7 +478,6 @@ void RtAve::run()
             //
             // Acquire Data
             //
-            std::cout<<"Acquire Data"<<std::endl;
             MatrixXd rawSegment = m_pRawMatrixBuffer->pop();
             if(t_nSamplesPerBuf == 0)
                 t_nSamplesPerBuf = rawSegment.cols();
@@ -499,49 +487,47 @@ void RtAve::run()
             //
             // Detect Stimuli
             //
-            std::cout<<"Detect Stimuli"<<std::endl;
-            QMutableMapIterator<int,QList<int> >itClear(m_qMapDetectedTrigger);
-            while(itClear.hasNext()) {
-                itClear.next();
-                itClear.value().clear();
-            }
+//            QMutableMapIterator<int,QList<int> >itClear(m_qMapDetectedTrigger);
+//            while(itClear.hasNext()) {
+//                itClear.next();
+//                itClear.value().clear();
+//            }
 
-            DetectTrigger::detectTriggerFlanks(rawSegment, m_qMapDetectedTrigger, 0, 0.1);
-
-            QList<qint32> t_qListStimuli;
-            QMapIterator<int,QList<int> >it(m_qMapDetectedTrigger);
-            while(it.hasNext()) {
-                it.next();
-                t_qListStimuli.append(it.value());
-            }
+//            DetectTrigger::detectTriggerFlanks(rawSegment, m_qMapDetectedTrigger, 0, 0.1);
 
 //            QList<qint32> t_qListStimuli;
-//            for(i = 0; i < m_qListStimChannelIdcs.size(); ++i)
-//            {
-//                qint32 idx = m_qListStimChannelIdcs[i];
-//                RowVectorXd stimSegment = rawSegment.row(idx);
-//                RowVectorXd::Index indexMaxCoeff;
-//                int dMax = stimSegment.maxCoeff(&indexMaxCoeff);
-
-//                if(dMax > 0)
-//                    t_qListStimuli.append(i);
-
-//                //Find trigger using gradient/difference
-//                double gradient = 0;
-
-//                if(indexMaxCoeff-10<0)
-//                    gradient = stimSegment(indexMaxCoeff) - stimSegment(indexMaxCoeff+10);
-//                else
-//                    gradient = stimSegment(indexMaxCoeff) - stimSegment(indexMaxCoeff-10);
-
-////                std::cout<<"gradient: "<<gradient<<std::endl;
-////                std::cout<<"dMax: "<<dMax<<std::endl;
+//            QMapIterator<int,QList<int> >it(m_qMapDetectedTrigger);
+//            while(it.hasNext()) {
+//                it.next();
+//                t_qListStimuli.append(it.value());
 //            }
+
+            QList<qint32> t_qListStimuli;
+            for(i = 0; i < m_qListStimChannelIdcs.size(); ++i)
+            {
+                qint32 idx = m_qListStimChannelIdcs[i];
+                RowVectorXd stimSegment = rawSegment.row(idx);
+                RowVectorXd::Index indexMaxCoeff;
+                int dMax = stimSegment.maxCoeff(&indexMaxCoeff);
+
+                if(dMax > 0)
+                    t_qListStimuli.append(i);
+
+                //Find trigger using gradient/difference
+                double gradient = 0;
+
+                if(indexMaxCoeff-10<0)
+                    gradient = stimSegment(indexMaxCoeff) - stimSegment(indexMaxCoeff+10);
+                else
+                    gradient = stimSegment(indexMaxCoeff) - stimSegment(indexMaxCoeff-10);
+
+//                std::cout<<"gradient: "<<gradient<<std::endl;
+//                std::cout<<"dMax: "<<dMax<<std::endl;
+            }
 
             //
             // Store
             //
-            std::cout<<"Store"<<std::endl;
             t_qListRawMatBuf.push_back(qMakePair(t_qListStimuli, rawSegment));
 
             m_qMutex.lock();
@@ -553,7 +539,6 @@ void RtAve::run()
                 //
                 // Average
                 //
-                std::cout<<"Average"<<std::endl;
                 float ratio = (float)m_iPreStimSamples/((float)(m_iPreStimSamples+m_iPostStimSamples));
                 qint32 t_iBuffWithStim = floor((t_qListRawMatBuf.size()-1)*ratio) + 1;
 
@@ -569,93 +554,88 @@ void RtAve::run()
                             //
                             // assemble prestimulus
                             //
-                            std::cout<<"assemble prestimulus"<<std::endl;
                             this->assemblePreStimulus(t_qListRawMatBuf, t_iStimIndex);
 
-//                            //
-//                            // assemble poststimulus
-//                            //
-//                            std::cout<<"assemble poststimulus"<<std::endl;
-//                            this->assemblePostStimulus(t_qListRawMatBuf, t_iStimIndex);
+                            //
+                            // assemble poststimulus
+                            //
+                            this->assemblePostStimulus(t_qListRawMatBuf, t_iStimIndex);
 
-//                            qDebug() << m_pFiffInfo->ch_names[m_qListStimChannelIdcs[t_iStimIndex]] << ": Buffer Size" << m_qListQListPreStimBuf[t_iStimIndex].size();
-//                            //
-//                            // Prestimulus average
-//                            //
-//                            std::cout<<"Prestimulus average"<<std::endl;
-//                            m_qMutex.lock();
-//                            if(m_qListQListPreStimBuf[t_iStimIndex].size() >= m_iNumAverages)
-//                            {
-//                                while(m_qListQListPreStimBuf[t_iStimIndex].size() >= (m_iNumAverages+1))//if meanwhile number of averages was reduced
-//                                    m_qListQListPreStimBuf[t_iStimIndex].pop_front();
+                            qDebug() << m_pFiffInfo->ch_names[m_qListStimChannelIdcs[t_iStimIndex]] << ": Buffer Size" << m_qListQListPreStimBuf[t_iStimIndex].size();
+                            //
+                            // Prestimulus average
+                            //
+                            m_qMutex.lock();
+                            if(m_qListQListPreStimBuf[t_iStimIndex].size() >= m_iNumAverages)
+                            {
+                                while(m_qListQListPreStimBuf[t_iStimIndex].size() >= (m_iNumAverages+1))//if meanwhile number of averages was reduced
+                                    m_qListQListPreStimBuf[t_iStimIndex].pop_front();
 
-//                                m_qListPreStimAve[t_iStimIndex] = m_qListQListPreStimBuf[t_iStimIndex][0];
-//                                for(j = 1; j < m_qListQListPreStimBuf[t_iStimIndex].size(); ++j)
-//                                    m_qListPreStimAve[t_iStimIndex] += m_qListQListPreStimBuf[t_iStimIndex][j];
+                                m_qListPreStimAve[t_iStimIndex] = m_qListQListPreStimBuf[t_iStimIndex][0];
+                                for(j = 1; j < m_qListQListPreStimBuf[t_iStimIndex].size(); ++j)
+                                    m_qListPreStimAve[t_iStimIndex] += m_qListQListPreStimBuf[t_iStimIndex][j];
 
-//                                m_qListPreStimAve[t_iStimIndex].array() /= (double)m_iNumAverages;
+                                m_qListPreStimAve[t_iStimIndex].array() /= (double)m_iNumAverages;
 
-//                                m_qListQListPreStimBuf[t_iStimIndex].pop_front();
-//                            }
-//                            m_qMutex.unlock();
+                                m_qListQListPreStimBuf[t_iStimIndex].pop_front();
+                            }
+                            m_qMutex.unlock();
 
-//                            //
-//                            // Poststimulus average
-//                            //
-//                            std::cout<<"Poststimulus average"<<std::endl;
-//                            m_qMutex.lock();
-//                            if(m_qListQListPostStimBuf[t_iStimIndex].size() >= m_iNumAverages)
-//                            {
-//                                while(m_qListQListPostStimBuf[t_iStimIndex].size() >= (m_iNumAverages+1))//if meanwhile number of averages was reduced
-//                                    m_qListQListPostStimBuf[t_iStimIndex].pop_front();
+                            //
+                            // Poststimulus average
+                            //
+                            m_qMutex.lock();
+                            if(m_qListQListPostStimBuf[t_iStimIndex].size() >= m_iNumAverages)
+                            {
+                                while(m_qListQListPostStimBuf[t_iStimIndex].size() >= (m_iNumAverages+1))//if meanwhile number of averages was reduced
+                                    m_qListQListPostStimBuf[t_iStimIndex].pop_front();
 
-//                                m_qListPostStimAve[t_iStimIndex] = m_qListQListPostStimBuf[t_iStimIndex][0];
-//                                for(j = 1; j < m_qListQListPostStimBuf[t_iStimIndex].size(); ++j)
-//                                    m_qListPostStimAve[t_iStimIndex] += m_qListQListPostStimBuf[t_iStimIndex][j];
+                                m_qListPostStimAve[t_iStimIndex] = m_qListQListPostStimBuf[t_iStimIndex][0];
+                                for(j = 1; j < m_qListQListPostStimBuf[t_iStimIndex].size(); ++j)
+                                    m_qListPostStimAve[t_iStimIndex] += m_qListQListPostStimBuf[t_iStimIndex][j];
 
-//                                m_qListPostStimAve[t_iStimIndex].array() /= (double)m_iNumAverages;
+                                m_qListPostStimAve[t_iStimIndex].array() /= (double)m_iNumAverages;
 
-//                                m_qListQListPostStimBuf[t_iStimIndex].pop_front();
-//                            }
-//                            m_qMutex.unlock();
+                                m_qListQListPostStimBuf[t_iStimIndex].pop_front();
+                            }
+                            m_qMutex.unlock();
 
-//                            //if averages are available -> buffers are filled and first average is stored
-//                            std::cout<<"if averages are available -> buffers are filled and first average is stored"<<std::endl;
-//                            m_qMutex.lock();
-//                            if(m_qListPreStimAve[t_iStimIndex].size() > 0)
-//                            {
-//                                //
-//                                // concatenate pre + post stimulus to full stimulus
-//                                //
-//                                m_qListStimAve[t_iStimIndex].resize(m_qListPreStimAve[t_iStimIndex].rows(), m_qListPreStimAve[t_iStimIndex].cols() + m_qListPostStimAve[t_iStimIndex].cols());
-//                                // Pre
-//                                m_qListStimAve[t_iStimIndex].block(0,0,m_qListPreStimAve[t_iStimIndex].rows(),m_qListPreStimAve[t_iStimIndex].cols()) = m_qListPreStimAve[t_iStimIndex];
-//                                // Post
-//                                m_qListStimAve[t_iStimIndex].block(0,m_qListPreStimAve[t_iStimIndex].cols(),m_qListPostStimAve[t_iStimIndex].rows(),m_qListPostStimAve[t_iStimIndex].cols()) = m_qListPostStimAve[t_iStimIndex];
+                            //if averages are available -> buffers are filled and first average is stored
+                            m_qMutex.lock();
+                            if(m_qListPreStimAve[t_iStimIndex].size() > 0)
+                            {
+                                //
+                                // concatenate pre + post stimulus to full stimulus
+                                //
+                                m_qListStimAve[t_iStimIndex].resize(m_qListPreStimAve[t_iStimIndex].rows(), m_qListPreStimAve[t_iStimIndex].cols() + m_qListPostStimAve[t_iStimIndex].cols());
+                                // Pre
+                                m_qListStimAve[t_iStimIndex].block(0,0,m_qListPreStimAve[t_iStimIndex].rows(),m_qListPreStimAve[t_iStimIndex].cols()) = m_qListPreStimAve[t_iStimIndex];
+                                // Post
+                                m_qListStimAve[t_iStimIndex].block(0,m_qListPreStimAve[t_iStimIndex].cols(),m_qListPostStimAve[t_iStimIndex].rows(),m_qListPostStimAve[t_iStimIndex].cols()) = m_qListPostStimAve[t_iStimIndex];
 
-//                                //
-//                                // Emit evoked
-//                                //
-//                                QString t_sStimChName = m_pFiffInfo->ch_names[m_qListStimChannelIdcs[t_iStimIndex]];
-//                                FiffEvoked::SPtr t_pEvokedPreStim(new FiffEvoked(t_preStimEvoked));
-//                                t_pEvokedPreStim->nave = m_iNumAverages;
-//                                t_pEvokedPreStim->comment = t_sStimChName;
-//                                t_pEvokedPreStim->data = m_qListPreStimAve[t_iStimIndex];
-//                                emit evokedPreStim(t_pEvokedPreStim);
+                                //
+                                // Emit evoked
+                                //
+                                QString t_sStimChName = m_pFiffInfo->ch_names[m_qListStimChannelIdcs[t_iStimIndex]];
+                                FiffEvoked::SPtr t_pEvokedPreStim(new FiffEvoked(t_preStimEvoked));
+                                t_pEvokedPreStim->nave = m_iNumAverages;
+                                t_pEvokedPreStim->comment = t_sStimChName;
+                                t_pEvokedPreStim->data = m_qListPreStimAve[t_iStimIndex];
+                                emit evokedPreStim(t_pEvokedPreStim);
 
-//                                FiffEvoked::SPtr t_pEvokedPostStim(new FiffEvoked(t_postStimEvoked));
-//                                t_pEvokedPostStim->nave = m_iNumAverages;
-//                                t_pEvokedPostStim->comment = t_sStimChName;
-//                                t_pEvokedPostStim->data = m_qListPostStimAve[t_iStimIndex];
-//                                emit evokedPostStim(t_pEvokedPostStim);
+                                FiffEvoked::SPtr t_pEvokedPostStim(new FiffEvoked(t_postStimEvoked));
+                                t_pEvokedPostStim->nave = m_iNumAverages;
+                                t_pEvokedPostStim->comment = t_sStimChName;
+                                t_pEvokedPostStim->data = m_qListPostStimAve[t_iStimIndex];
+                                emit evokedPostStim(t_pEvokedPostStim);
 
-//                                FiffEvoked::SPtr t_pEvokedStim(new FiffEvoked(t_stimEvoked));
-//                                t_pEvokedStim->nave = m_iNumAverages;
-//                                t_pEvokedStim->comment = t_sStimChName;
-//                                t_pEvokedStim->data = m_qListStimAve[t_iStimIndex];
-//                                emit evokedStim(t_pEvokedStim);
-//                            }
-//                            m_qMutex.unlock();
+                                FiffEvoked::SPtr t_pEvokedStim(new FiffEvoked(t_stimEvoked));
+                                t_pEvokedStim->nave = m_iNumAverages;
+                                t_pEvokedStim->comment = t_sStimChName;
+                                t_pEvokedStim->data = m_qListStimAve[t_iStimIndex];
+                                emit evokedStim(t_pEvokedStim);
+                            }
+                            m_qMutex.unlock();
                         }
                     }
                 }
