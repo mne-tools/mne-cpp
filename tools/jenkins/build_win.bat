022e62453f2e99785654cde749a54149daa7a172
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2015-09-04 15:30:24 +0200

    Jenkins update

--- tools/jenkins/build_win.bat
+++ tools/jenkins/build_win.bat
@@ -4,6 +4,9 @@ set arg0=%0
 
 echo Starting MNE-CPP Win Build %arg1%
 
-qmake "../../mne-cpp.pro" -recursive
+mkdir MNE-CPP_shadow_build
+cd MNE-CPP_shadow_build
+
+qmake ../../../mne-cpp.pro -recursive
 
 nmake
