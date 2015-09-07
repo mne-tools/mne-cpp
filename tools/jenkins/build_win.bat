29e556bc2d6f38b11e5a18ccc0f19f651cc031e2
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2015-09-07 15:24:11 +0200

    Formatting

--- tools/jenkins/build_win.bat
+++ tools/jenkins/build_win.bat
@@ -7,7 +7,6 @@ echo Starting MNE-CPP Win Build %arg1%
 mkdir MNE-CPP_shadow_build
 cd MNE-CPP_shadow_build
 
-
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 
 qmake ../mne-cpp.pro -r
