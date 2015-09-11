e45074574cb9e5a1717801b64e32d9e8f5d3b5c0
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2015-09-11 14:31:45 -0400

    [MX-82] Update author docu in rtave and FiffEvoked

--- MNE/rtInv/rtave.h
+++ MNE/rtInv/rtave.h
@@ -2,13 +2,14 @@
 /**
 * @file     rtave.h
 * @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
+*           Lorenz Esch <Lorenz.Esch@tu-ilmenau.de>;
 *           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
 * @version  1.0
-* @date     July, 2012
+* @date     September, 2015
 *
 * @section  LICENSE
 *
-* Copyright (C) 2012, Christoph Dinh and Matti Hamalainen. All rights reserved.
+* Copyright (C) 2015, Christoph Dinh, Lorenz Esch and Matti Hamalainen. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 * the following conditions are met:
