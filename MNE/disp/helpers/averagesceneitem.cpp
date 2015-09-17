8badb1ac62b89322581d8cf23018a00f83f43c8a
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2015-09-17 11:54:05 -0400

    [MX-83] change to sign of plotting value in the rtbutterfly plot

--- MNE/disp/helpers/averagesceneitem.cpp
+++ MNE/disp/helpers/averagesceneitem.cpp
@@ -95,8 +95,8 @@ void AverageSceneItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *
     painter->drawStaticText(boundingRect().x(), boundingRect().y(), staticElectrodeName);
     painter->restore();
 
-//    //Plot bounding rect / drawing region of this item
-//    painter->drawRect(this->boundingRect());
+    //Plot bounding rect / drawing region of this item
+    painter->drawRect(this->boundingRect());
 
     //Plot stim time
     painter->save();
@@ -183,11 +183,11 @@ void AverageSceneItem::paintAveragePath(QPainter *painter)
         //Calculate downsampling factor of averaged data in respect to the items width
         int dsFactor;
         totalCols / boundingRect.width()<1 ? dsFactor = 1 : dsFactor = totalCols / boundingRect.width();
-        //if(dsFactor == 0)
+        if(dsFactor == 0)
             dsFactor = 1;
 
         //Create path
-        float offset = (*(averageData+(abs(m_firstLastSample.first)*m_iTotalNumberChannels)+m_iChannelNumber)); //choose offset to be the signal value at time instance 0
+        //float offset = (*(averageData+(abs(m_firstLastSample.first)*m_iTotalNumberChannels)+m_iChannelNumber)); //choose offset to be the signal value at time instance 0
         QPainterPath path = QPainterPath(QPointF(boundingRect.x(), boundingRect.y() + boundingRect.height()/2));
         QPen pen;
         pen.setStyle(Qt::SolidLine);
@@ -199,9 +199,9 @@ void AverageSceneItem::paintAveragePath(QPainter *painter)
 
         for(int i = 0; i < totalCols && path.elementCount() <= boundingRect.width(); i += dsFactor) {
             //evoked matrix is stored in column major
-            double val = ((*(averageData+(i*m_iTotalNumberChannels)+m_iChannelNumber))-offset) * dScaleY;
+            double val = ((*(averageData+(i*m_iTotalNumberChannels)+m_iChannelNumber))/*-offset*/) * dScaleY;
 
-            qSamplePosition.setY(val);
+            qSamplePosition.setY(-val);
             qSamplePosition.setX(path.currentPosition().x()+1);
 
             path.lineTo(qSamplePosition);
