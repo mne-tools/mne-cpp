f012f6adf5344ecea2e5bee03f76c395b0c3cec3
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2015-11-13 20:31:23 +0100

    [MX-87] Delete tmsi related stuff from gusbamp plugin [LIB-20] Change sulci gyri color

--- applications/mne_x/plugins/gusbamp/FormFiles/gusbampsetupwidget.cpp
+++ applications/mne_x/plugins/gusbamp/FormFiles/gusbampsetupwidget.cpp
@@ -70,54 +70,6 @@ GUSBAmpSetupWidget::GUSBAmpSetupWidget(GUSBAmp* pGUSBAmp, QWidget* parent)
 , m_pGUSBAmp(pGUSBAmp)
 {
     ui.setupUi(this);
-
-    //Connect device sampling properties
-    connect(ui.m_spinBox_SamplingFreq, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
-            this, &GUSBAmpSetupWidget::setDeviceSamplingProperties);
-    connect(ui.m_spinBox_NumberOfChannels, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
-            this, &GUSBAmpSetupWidget::setDeviceSamplingProperties);
-    connect(ui.m_spinBox_SamplesPerBlock, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
-            this, &GUSBAmpSetupWidget::setDeviceSamplingProperties);
-
-    //Connect channel corrections
-    connect(ui.m_checkBox_UseChExponent, static_cast<void (QCheckBox::*)(bool)>(&QCheckBox::clicked),
-            this, &GUSBAmpSetupWidget::setDeviceSamplingProperties);
-    connect(ui.m_checkBox_UseUnitGain, static_cast<void (QCheckBox::*)(bool)>(&QCheckBox::clicked),
-            this, &GUSBAmpSetupWidget::setDeviceSamplingProperties);
-    connect(ui.m_checkBox_UseUnitOffset, static_cast<void (QCheckBox::*)(bool)>(&QCheckBox::clicked),
-            this, &GUSBAmpSetupWidget::setDeviceSamplingProperties);
-
-    //Connect debug file
-    connect(ui.m_checkBox_WriteDriverDebugToFile, static_cast<void (QCheckBox::*)(bool)>(&QCheckBox::clicked),
-            this, &GUSBAmpSetupWidget::setWriteToFile);
-
-    //Connect trigger properties
-    connect(ui.m_spinBox_BeepLength, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
-            this, &GUSBAmpSetupWidget::setTriggerProperties);
-    connect(ui.m_checkBox_EnableBeep, static_cast<void (QCheckBox::*)(bool)>(&QCheckBox::clicked),
-            this, &GUSBAmpSetupWidget::setTriggerProperties);
-
-    //Connect about button
-    connect(ui.m_qPushButton_About, &QPushButton::released, this, &GUSBAmpSetupWidget::showAboutDialog);
-
-    //Connect split file options
-    connect(ui.m_checkBox_splitFiles, static_cast<void (QCheckBox::*)(bool)>(&QCheckBox::clicked),
-            this, &GUSBAmpSetupWidget::setSplitFile);
-    connect(ui.m_spinBox_splitFileSize, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
-            this, &GUSBAmpSetupWidget::setSplitFileSize);
-
-    //Fill info box
-    QFile file(m_pGUSBAmp->m_qStringResourcePath+"readme.txt");
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-        return;
-
-    QTextStream in(&file);
-    while (!in.atEnd())
-    {
-        QString line = in.readLine();
-        ui.m_qTextBrowser_Information->insertHtml(line);
-        ui.m_qTextBrowser_Information->insertHtml("<br>");
-    }
 }
 
 
@@ -133,70 +85,7 @@ GUSBAmpSetupWidget::~GUSBAmpSetupWidget()
 
 void GUSBAmpSetupWidget::initGui()
 {
-    //Init device sampling properties
-    ui.m_spinBox_SamplingFreq->setValue(m_pGUSBAmp->m_iSamplingFreq);
-    ui.m_spinBox_NumberOfChannels->setValue(m_pGUSBAmp->m_iNumberOfChannels);
-    ui.m_spinBox_SamplesPerBlock->setValue(m_pGUSBAmp->m_iSamplesPerBlock);
 
-    //Init channel corrections
-    ui.m_checkBox_UseChExponent->setChecked(m_pGUSBAmp->m_bUseChExponent);
-    ui.m_checkBox_UseUnitGain->setChecked(m_pGUSBAmp->m_bUseUnitGain);
-    ui.m_checkBox_UseUnitOffset->setChecked(m_pGUSBAmp->m_bUseUnitOffset);
-
-    //Init write to file
-    ui.m_checkBox_WriteDriverDebugToFile->setChecked(m_pGUSBAmp->m_bWriteDriverDebugToFile);
-
-    //Init trigger properties
-    ui.m_spinBox_BeepLength->setValue(m_pGUSBAmp->m_iTriggerInterval);
-    ui.m_checkBox_EnableBeep->setChecked(m_pGUSBAmp->m_bBeepTrigger);
-}
-
-
-//*************************************************************************************************************
-
-void GUSBAmpSetupWidget::setDeviceSamplingProperties()
-{
-    cout<<"changing "<<endl;
-    m_pGUSBAmp->m_iSamplingFreq = ui.m_spinBox_SamplingFreq->value();
-    m_pGUSBAmp->m_iNumberOfChannels = ui.m_spinBox_NumberOfChannels->value();
-    m_pGUSBAmp->m_iSamplesPerBlock = ui.m_spinBox_SamplesPerBlock->value();
-
-    m_pGUSBAmp->m_bUseChExponent = ui.m_checkBox_UseChExponent->isChecked();
-    m_pGUSBAmp->m_bUseUnitGain = ui.m_checkBox_UseUnitGain->isChecked();
-    m_pGUSBAmp->m_bUseUnitOffset = ui.m_checkBox_UseUnitOffset->isChecked();
-}
-
-
-//*************************************************************************************************************
-
-void GUSBAmpSetupWidget::setWriteToFile()
-{
-    m_pGUSBAmp->m_bWriteDriverDebugToFile = ui.m_checkBox_WriteDriverDebugToFile->isChecked();
-}
-
-
-//*************************************************************************************************************
-
-void GUSBAmpSetupWidget::setSplitFile(bool state)
-{
-    m_pGUSBAmp->m_bSplitFile = state;
-}
-
-
-//*************************************************************************************************************
-
-void GUSBAmpSetupWidget::setSplitFileSize(qint32 value)
-{
-    m_pGUSBAmp->m_iSplitFileSizeMs = value;
-}
-
-
-//*************************************************************************************************************
-
-void GUSBAmpSetupWidget::setTriggerProperties()
-{
-    m_pGUSBAmp->m_iTriggerInterval = ui.m_spinBox_BeepLength->value();
-    m_pGUSBAmp->m_bBeepTrigger = ui.m_checkBox_EnableBeep->isChecked();
 }
 
 
--- applications/mne_x/plugins/gusbamp/gusbampdriver.cpp
+++ applications/mne_x/plugins/gusbamp/gusbampdriver.cpp
@@ -58,65 +58,7 @@ using namespace GUSBAmpPlugin;
 
 GUSBAmpDriver::GUSBAmpDriver(GUSBAmpProducer* pGUSBAmpProducer)
 : m_pGUSBAmpProducer(pGUSBAmpProducer)
-, m_bDllLoaded(true)
-, m_bInitDeviceSuccess(false)
-, m_uiNumberOfChannels(138)
-, m_uiSamplingFrequency(1024)
-, m_uiSamplesPerBlock(1)
-, m_bUseChExponent(false)
-, m_bUseUnitGain(false)
-, m_bUseUnitOffset(false)
-, m_bWriteDriverDebugToFile(false)
-, m_sOutputFilePath("/mne_x_plugins/resources/GUSBAmp")
-{
-    //Initialise NULL pointers
-    m_oLibHandle = NULL ;
-    m_HandleMaster = NULL;
-    m_PSPDPMasterDevicePath = NULL;
-    m_lSignalBuffer = NULL;
-
-    //Open library
-//    TCHAR Path[MAX_PATH];
-//    GetSystemDirectory(Path, sizeof(Path) / sizeof(TCHAR) );
-//    lstrcat(Path, _T("\\GUSBAmpSDK.dll"));
-//    m_oLibHandle = LoadLibrary(Path);
-
-    //Check which driver dll to take: GUSBAmpSDK.dll oder GUSBAmpSDK32bit.dll
-//    if(GUSBAmpSDK)
-#ifdef TAKE_GUSBAmpSDK_DLL //32 bit system & 64 bit (with 64 bit compiler)
-        m_oLibHandle = ::LoadLibrary(L"C:\\Windows\\System32\\GUSBAmpSDK.dll");
-#elif TAKE_GUSBAmpSDK_32_DLL //64 bit (with 32 bit compiler)
-//    if(GUSBAmpSDK32)
-        m_oLibHandle = ::LoadLibrary(L"C:\\Windows\\SysWOW64\\GUSBAmpSDK32bit.dll");
-#endif
-
-    //If dll can't be open return
-    if( m_oLibHandle == NULL)
-    {
-        cout << "Plugin GUSBAmp - ERROR - Couldn't load DLL - Check if the driver for the GUSBAmp USB Fiber Connector installed in the system dir" << endl;
-        m_bDllLoaded = false;
-        return;
-    }
-
-    //Load DLL methods for initializing the driver
-    __load_dll_func__(m_oFpOpen, POPEN, "Open");
-    __load_dll_func__(m_oFpClose, PCLOSE, "Close");
-    __load_dll_func__(m_oFpStart, PSTART, "Start");
-    __load_dll_func__(m_oFpStop, PSTOP, "Stop");
-    __load_dll_func__(m_oFpGetSignalFormat, PGETSIGNALFORMAT, "GetSignalFormat");
-    __load_dll_func__(m_oFpSetSignalBuffer, PSETSIGNALBUFFER, "SetSignalBuffer");
-    __load_dll_func__(m_oFpGetSamples, PGETSAMPLES, "GetSamples");
-    __load_dll_func__(m_oFpGetBufferInfo, PGETBUFFERINFO, "GetBufferInfo");
-    __load_dll_func__(m_oFpFree, PFREE, "Free");
-    __load_dll_func__(m_oFpLibraryInit, PLIBRARYINIT, "LibraryInit");
-    __load_dll_func__(m_oFpLibraryExit, PLIBRARYEXIT, "LibraryExit");
-    __load_dll_func__(m_oFpGetDeviceList, PGETDEVICELIST, "GetDeviceList");
-    __load_dll_func__(m_oFpGetFrontEndInfo, PGETFRONTENDINFO, "GetFrontEndInfo");
-    __load_dll_func__(m_oFpSetRefCalculation, PSETREFCALCULATION, "SetRefCalculation");
-    __load_dll_func__(m_oFpSetMeasuringMode, PSETMEASURINGMODE, "SetMeasuringMode");
-    __load_dll_func__(m_oFpGetErrorCode, PGETERRORCODE, "GetErrorCode");
-
-    cout << "Plugin GUSBAmp - INFO - GUSBAmpDriver() - Successfully loaded all DLL functions" << endl;
+{    
 }
 
 
@@ -124,158 +66,15 @@ GUSBAmpDriver::GUSBAmpDriver(GUSBAmpProducer* pGUSBAmpProducer)
 
 GUSBAmpDriver::~GUSBAmpDriver()
 {
-    //cout << "GUSBAmpDriver::~GUSBAmpDriver()" << endl;
+    //qDebug() << "GUSBAmpDriver::~GUSBAmpDriver()" << endl;
 }
 
 
 //*************************************************************************************************************
 
-bool GUSBAmpDriver::initDevice(int iNumberOfChannels,
-                            int iSamplingFrequency,
-                            int iSamplesPerBlock,
-                            bool bUseChExponent,
-                            bool bUseUnitGain,
-                            bool bUseUnitOffset,
-                            bool bWriteDriverDebugToFile,
-                            QString sOutpuFilePath)
+bool GUSBAmpDriver::initDevice()
 {
-    //Check if the driver DLL was loaded
-    if(!m_bDllLoaded)
-        return false;
-
-    //Set global variables
-    m_uiNumberOfChannels = iNumberOfChannels;
-    m_uiSamplingFrequency = iSamplingFrequency;
-    m_uiSamplesPerBlock = iSamplesPerBlock;
-    m_bUseChExponent = bUseChExponent;
-    m_bUseUnitGain = bUseUnitGain;
-    m_bUseUnitOffset = bUseUnitOffset;
-    m_bWriteDriverDebugToFile = bWriteDriverDebugToFile;
-    m_sOutputFilePath = sOutpuFilePath;
-
-    //Open file to write to
-    if(m_bWriteDriverDebugToFile)
-        m_outputFileStream.open("mne_x_plugins/resources/GUSBAmp/GUSBAmp_Driver_Debug.txt", ios::trunc); //ios::trunc deletes old file data
-
-    //Check if device handler already exists and a connection was established before
-//    if(m_HandleMaster != NULL)
-//    {
-//        m_oFpClose(m_HandleMaster);
-//        m_HandleMaster = NULL;
-//    }
-    int ErrorCode = 0;
-    m_HandleMaster = m_oFpLibraryInit(GUSBAmpConnectionUSB, &ErrorCode);
-
-    if( ErrorCode != 0 )
-    {
-        cout << "Plugin GUSBAmp - ERROR - initDevice() - Can not initialize library" << endl;
-        return false;
-    }
-
-    //Get the device list of connected devices
-    char **DeviceList = NULL;
-    int NrOfDevices=0;
-
-    DeviceList = m_oFpGetDeviceList(m_HandleMaster, &NrOfDevices);
-
-    if( NrOfDevices == 0 )
-    {
-        cout << "Plugin GUSBAmp - ERROR - initDevice() - Frontend list NOT available - Maybe no devices are connected" << endl;
-        m_oFpLibraryExit(m_HandleMaster);
-        return false;
-    }
-
-    //Open device
-    BOOLEAN Status;
-    char *DeviceLocator = DeviceList[0] ;
-    Status = m_oFpOpen(m_HandleMaster, DeviceLocator);
-
-    //Stop the device from sampling. Just in case the device was not stopped correctly after the last sampling process
-    m_oFpStop(m_HandleMaster);
-
-    if(!Status)
-    {
-        cout << "Plugin GUSBAmp - ERROR - initDevice() - Failed to open connected device" << endl;
-        m_oFpLibraryExit(m_HandleMaster);
-        return false;
-    }
-
-    // Turn on the impendance mode
-    ULONG impedanceMode = 3;
-    ULONG normalMode = 0;
-
-    m_oFpSetMeasuringMode(m_HandleMaster, normalMode, 0);
-
-    //Get information about the connected device
-    FRONTENDINFO FrontEndInfo;
-    Status = m_oFpGetFrontEndInfo(m_HandleMaster, &FrontEndInfo);
-    unsigned short serial, hwVersion, swVersion, baseSf, maxRS232, nrOfChannels;
-
-    if(!Status)
-        cout << "Plugin GUSBAmp - ERROR - initDevice() - FrontendInfo NOT available" << endl;
-    else
-    {
-        serial = FrontEndInfo.Serial;
-        hwVersion = FrontEndInfo.HwVersion;
-        swVersion = FrontEndInfo.SwVersion;
-        baseSf = FrontEndInfo.BaseSf;
-        maxRS232 = FrontEndInfo.maxRS232;
-        nrOfChannels = FrontEndInfo.NrOfChannels;
-    }
-
-    //Get information about the signal format created by the device - UnitExponent, UnitGain, UnitOffSet
-    PSIGNAL_FORMAT pSignalFormat = m_oFpGetSignalFormat(m_HandleMaster, NULL);
-
-    if(pSignalFormat != NULL)
-    {
-        wcscpy_s(m_wcDeviceName, pSignalFormat->PortName);
-        m_ulSerialNumber = pSignalFormat->SerialNumber;
-        m_uiNumberOfAvailableChannels = pSignalFormat[0].Elements;
-
-        if(m_bWriteDriverDebugToFile)
-            m_outputFileStream << "Found "<< m_wcDeviceName << " device (" << m_ulSerialNumber << ") with " << m_uiNumberOfAvailableChannels << " available channels" << endl << endl;
-
-        for(uint i = 0 ; i < m_uiNumberOfAvailableChannels; i++ )
-        {
-            m_vExponentChannel.push_back(pSignalFormat[i].UnitExponent);
-            m_vUnitGain.push_back(pSignalFormat[i].UnitGain);
-            m_vUnitOffSet.push_back(pSignalFormat[i].UnitOffSet);
-
-            if(m_bWriteDriverDebugToFile)
-                m_outputFileStream << "Channel number: " << i << " has type " << pSignalFormat[i].Type << " , format " << pSignalFormat[i].Format << " exponent " << pSignalFormat[i].UnitExponent << " gain " << pSignalFormat[i].UnitGain << " offset " << pSignalFormat[i].UnitOffSet << endl;
-        }
-
-        if(m_bWriteDriverDebugToFile)
-            m_outputFileStream << endl;
-    }
-
-    //Initialise and set up (sample rate/frequency and buffer size) the internal driver signal buffer which is used by the driver to store the value
-    ULONG iSamplingFrequencyMilliHertz = m_uiSamplingFrequency*1000;    //Times 1000 because the driver works in millihertz
-    ULONG iBufferSize = MAX_BUFFER_SIZE;                                //see GUSBAmp doc file for more info. This size is not defined in bytes but in the number of elements which are to be sampled. A sample in this case is one conversion result for all input channels..
-
-    if(!m_oFpSetSignalBuffer(m_HandleMaster, &iSamplingFrequencyMilliHertz, &iBufferSize))
-    {
-        cout << "Plugin GUSBAmp - ERROR - initDevice() - Failed to allocate signal buffer" << endl;
-        m_oFpLibraryExit(m_HandleMaster);
-        return false;
-    }
-
-    //Start the sampling process
-    bool start = m_oFpStart(m_HandleMaster);
-    if(!start)
-    {
-        cout << "Plugin GUSBAmp - ERROR - initDevice() - Failed to start the sampling procedure" << endl;
-        m_oFpLibraryExit(m_HandleMaster);
-        return false;
-    }
-
-    //Create the buffers
-    //The sampling frequency is not needed here because it is only used to specify the internal buffer size used by the driver with setSignalBuffer()
-    m_lSignalBufferSize = m_uiSamplesPerBlock*m_uiNumberOfAvailableChannels*4;
-    m_lSignalBuffer = new LONG[m_lSignalBufferSize];
-
-    cout << "Plugin GUSBAmp - INFO - initDevice() - The device has been connected and initialised successfully" << endl;
-    m_bInitDeviceSuccess = true;
+    qDebug() << "Plugin GUSBAmp - INFO - initDevice() - The device has been connected and initialised successfully" << endl;
     return true;
 }
 
@@ -284,51 +83,7 @@ bool GUSBAmpDriver::initDevice(int iNumberOfChannels,
 
 bool GUSBAmpDriver::uninitDevice()
 {
-    //Clear the buffer which is used to store the received samples
-    m_vSampleBlockBuffer.clear();
-
-    //Check if the device was initialised
-    if(!m_bInitDeviceSuccess)
-    {
-        cout << "Plugin GUSBAmp - ERROR - uninitDevice() - Device was not initialised - therefore can not be uninitialised" << endl;
-        return false;
-    }
-
-    //Check if the driver DLL was loaded
-    if(!m_bDllLoaded)
-    {
-        cout << "Plugin GUSBAmp - ERROR - uninitDevice() - Driver DLL was not loaded" << endl;
-        return false;
-    }
-
-    //Close the output stream/file
-    if(m_outputFileStream.is_open() && m_bWriteDriverDebugToFile)
-    {
-        m_outputFileStream.close();
-        m_outputFileStream.clear();
-    }
-
-    if(!m_oFpStop(m_HandleMaster))
-    {
-        cout << "Plugin GUSBAmp - ERROR - uninitDevice() - Failed to stop the device" << endl;
-        return false;
-    }
-
-    if(!m_oFpClose(m_HandleMaster))
-    {
-        cout << "Plugin GUSBAmp - ERROR - uninitDevice() - Failed to close the device" << endl;
-        return false;
-    }
-
-    m_oFpLibraryExit(m_HandleMaster);
-
-    //Reset to NULL pointers
-    m_oLibHandle = NULL ;
-    m_HandleMaster = NULL;
-    m_PSPDPMasterDevicePath = NULL;
-    m_lSignalBuffer = NULL;
-
-    cout << "Plugin GUSBAmp - INFO - uninitDevice() - Successfully uninitialised the device" << endl;
+    qDebug() << "Plugin GUSBAmp - INFO - uninitDevice() - Successfully uninitialised the device" << endl;
     return true;
 }
 
@@ -337,102 +92,7 @@ bool GUSBAmpDriver::uninitDevice()
 
  bool GUSBAmpDriver::getSampleMatrixValue(MatrixXf& sampleMatrix)
 {
-    //Check if the driver DLL was loaded
-    if(!m_bDllLoaded)
-        return false;
-
-    //Check if device was initialised and connected correctly
-    if(!m_bInitDeviceSuccess)
-    {
-        cout << "Plugin GUSBAmp - ERROR - getSampleMatrixValue() - Cannot start to get samples from device because device was not initialised correctly" << endl;
-        return false;
-    }
-
     sampleMatrix.setZero(); // Clear matrix - set all elements to zero
-    uint iSamplesWrittenToMatrix = 0;
-    int channelMax = 0;
-    int sampleMax = 0;
-    int sampleIterator = 0;
-
-    //get samples from device until the complete matrix is filled, i.e. the samples per block size is met
-    while(iSamplesWrittenToMatrix < m_uiSamplesPerBlock)
-    {
-        //Get sample block from device
-        LONG ulSizeSamples = m_oFpGetSamples(m_HandleMaster, (PULONG)m_lSignalBuffer, m_lSignalBufferSize);
-        LONG ulNumSamplesReceived = ulSizeSamples/(m_uiNumberOfAvailableChannels*4);
-
-        //Only do the next steps if there was at least one sample received, otherwise skip and wait until at least one sample was received
-        if(ulNumSamplesReceived > 0)
-        {
-            int actualSamplesWritten = 0; //Holds the number of samples which are actually written to the matrix in this while procedure
-
-            //Write the received samples to an extra buffer, so that they are not getting lost if too many samples were received. These are then written to the next matrix (block)
-            for(int i=0; i<ulNumSamplesReceived; i++)
-            {
-                for(uint j=i*m_uiNumberOfAvailableChannels; j<(i*m_uiNumberOfAvailableChannels)+m_uiNumberOfChannels; j++)
-                    m_vSampleBlockBuffer.push_back((double)m_lSignalBuffer[j]);
-            }
-
-            //If the number of available channels is smaller than the number defined by the user -> set the channelMax to the smaller number
-            if(m_uiNumberOfAvailableChannels < m_uiNumberOfChannels)
-                channelMax = m_uiNumberOfAvailableChannels;
-            else
-                channelMax = m_uiNumberOfChannels;
-
-            //If the number of the samples which were already written to the matrix plus the last received number of samples is larger then the defined block size
-            //-> only fill until the matrix is completeley filled with samples. The other (unused) samples are still stored in the vector buffer m_vSampleBlockBuffer and will be used in the next matrix which is to be sent to the circular buffer
-            if(iSamplesWrittenToMatrix + ulNumSamplesReceived > m_uiSamplesPerBlock)
-                sampleMax = m_uiSamplesPerBlock - iSamplesWrittenToMatrix + sampleIterator;
-            else
-                sampleMax = ulNumSamplesReceived + sampleIterator;
-
-            //Read the needed number of samples from the vector buffer to store them in the matrix
-            for(; sampleIterator < sampleMax; sampleIterator++)
-            {
-                for(int channelIterator = 0; channelIterator < channelMax; channelIterator++)
-                {
-                    sampleMatrix(channelIterator, sampleIterator) = ((m_vSampleBlockBuffer.first() * (m_bUseUnitGain ? m_vUnitGain[channelIterator] : 1)) + (m_bUseUnitOffset ? m_vUnitOffSet[channelIterator] : 0)) * (m_bUseChExponent ? pow(10., (double)m_vExponentChannel[channelIterator]) : 1);
-                    m_vSampleBlockBuffer.pop_front();
-                }
-
-                actualSamplesWritten ++;
-            }
-
-            iSamplesWrittenToMatrix = iSamplesWrittenToMatrix + actualSamplesWritten;
-        }
-
-        if(m_outputFileStream.is_open() && m_bWriteDriverDebugToFile)
-        {
-            m_outputFileStream << "samples in buffer: " << m_vSampleBlockBuffer.size()/m_uiNumberOfChannels << endl;
-            m_outputFileStream << "ulSizeSamples: " << ulSizeSamples << endl;
-            m_outputFileStream << "ulNumSamplesReceived: " << ulNumSamplesReceived << endl;
-            m_outputFileStream << "sampleMax: " << sampleMax << endl;
-            m_outputFileStream << "sampleIterator: " << sampleIterator << endl;
-            m_outputFileStream << "iSamplesWrittenToMatrix: " << iSamplesWrittenToMatrix << endl << endl;
-        }
-    }
-
-    if(/*m_outputFileStream.is_open() &&*/ m_bWriteDriverDebugToFile)
-    {
-        //Get device buffer info
-        ULONG ulOverflow;
-        ULONG ulPercentFull;
-        m_oFpGetBufferInfo(m_HandleMaster, &ulOverflow, &ulPercentFull);
-
-        m_outputFileStream <<  "Unit offset: " << endl;
-        for(int w = 0; w<<m_vUnitOffSet.size(); w++)
-            cout << float(m_vUnitOffSet[w]) << "  ";
-        m_outputFileStream << endl << endl;
-
-        m_outputFileStream <<  "Unit gain: " << endl;
-        for(int w = 0; w<<m_vUnitGain.size(); w++)
-            m_outputFileStream << float(m_vUnitGain[w]) << "  ";
-        m_outputFileStream << endl << endl;
-
-        m_outputFileStream << "----------<See output file for sample matrix>----------" <<endl<<endl;
-        m_outputFileStream << "----------<Internal driver buffer is "<<ulPercentFull<<" full>----------"<<endl;
-        m_outputFileStream << "----------<Internal driver overflow is "<<ulOverflow<< ">----------"<<endl;
-    }
 
     return true;
 }
