3abb8e4c622ba2b51e356a89bd8a830778029eb1
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2016-01-22 08:54:34 +0100

    [LIB-37] Jenkins Update

--- tools/jenkins/build_win.bat
+++ tools/jenkins/build_win.bat
@@ -1,3 +1,5 @@
+:: ###startdir### %WORKSPACE%/mne-cpp/..
+
 echo off
 :: %0 Build Number
 set arg0=%0
@@ -14,5 +16,5 @@ nmake clean
 nmake
 
 cd ..
-del mne-cpp-windows-x86_64_1.0.0.zip
-call 7z a mne-cpp-windows-x86_64_1.0.0.zip ./mne-cpp/bin
+del mne-cpp-windows-x86_64-1.0.0-beta.zip
+call 7z a mne-cpp-windows-x86_64-1.0.0-beta.zip ./mne-cpp/bin
