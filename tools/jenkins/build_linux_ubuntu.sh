c83209ac70801a316eab2ba5ab9a78a026d7efee
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-01-22 21:57:37 +0100

    Linux fixes

--- tools/jenkins/build_linux_ubuntu.sh
+++ tools/jenkins/build_linux_ubuntu.sh
@@ -7,7 +7,10 @@ echo Starting MNE-CPP Linux Ubuntu Build
 mkdir mne-cpp_shadow_build
 cd mne-cpp_shadow_build
 
-qmake ../mne-cpp/mne-cpp.pro -r
+QT_BIN_DIR='/opt/Qt5.6.0/5.6/gcc_64/bin'
+QT_LIB_DIR='/opt/Qt5.6.0/5.6/gcc_64/lib'
+
+$QT_BIN_DIR/qmake ../mne-cpp/mne-cpp.pro -r
 make clean
 make -j4
 
