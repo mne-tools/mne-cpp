02f78b93b23e17baf5a27d5567cab730bd19e8d4
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-02-01 09:30:03 +0100

    [Lib-58] Jenkins Linux Build

--- tools/jenkins/build_linux_ubuntu.sh
+++ tools/jenkins/build_linux_ubuntu.sh
@@ -1,4 +1,4 @@
-# ###startdir### %WORKSPACE%/mne-cpp/..
+# ===startdir=== %WORKSPACE%/mne-cpp/..
 # %0 Build Number
 # arg0=%0
 
@@ -10,14 +10,22 @@ cd mne-cpp_shadow_build
 QT_BIN_DIR='/opt/Qt5.6.0/5.6/gcc_64/bin'
 QT_LIB_DIR='/opt/Qt5.6.0/5.6/gcc_64/lib'
 
+QT_LIBS=(libQt5Core libQt5Gui libQt5Concurrent)
+
 $QT_BIN_DIR/qmake ../mne-cpp/mne-cpp.pro -r
 make clean
 make -j4
 
-# user package
+
+# === Copy Libs ===
+n_elements=${#QT_LIBS[@]}
+for ((i = 0; i < n_elements; i++)); do
+    libpath="$QT_LIB_DIR/${QT_LIBS[i]}.*"
+    cp $libpath ../mne-cpp/lib
+done
+
+
+# === user package ===
 cd ..
 rm mne-cpp-linux-x86_64-1.0.0-beta.tar.gz
 tar cfvz mne-cpp-linux-x86_64-1.0.0-beta.tar.gz ./mne-cpp/bin ./mne-cpp/lib
-
-# development package
-# tar cfvz mne-cpp-linux-x86_64-1.0.0-beta_dev.tar.gz *
