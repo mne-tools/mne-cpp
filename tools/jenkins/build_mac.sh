18e1be48bf572cfc795736f9285eda3df36cbada
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-02-09 23:59:23 +0100

    [LIB-59] Mac OS-X Setup

--- tools/jenkins/build_mac.sh
+++ tools/jenkins/build_mac.sh
@@ -11,11 +11,22 @@ cd mne-cpp_shadow_build
 QT_BIN_DIR='/Users/Shared/Jenkins/Qt5.6.0/5.6/clang_64/bin'
 QT_LIB_DIR='/Users/Shared/Jenkins/Qt5.6.0/5.6/clang_64/lib'
 
+MNECPP_LIB_WORKSPACE='/Users/Shared/Jenkins/Home/jobs/MNE-CPP/workspace/mne-cpp/lib'
+
+TANGIBLES=(mne_x mne_browse_raw_qt mne_analyze_qt)
+
 PATH=$QT_BIN_DIR:$PATH
 export PATH
-
-qmake -v
+DYLD_LIBRARY_PATH=$MNECPP_LIB_WORKSPACE:$DYLD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH
 
 qmake ../mne-cpp/mne-cpp.pro -r
 make clean
 make -j4
+
+# === Deployment ===
+n_elements=${#TANGIBLES[@]}
+for ((i = 0; i < n_elements; i++)); do
+    tangible="../mne-cpp/bin/${TANGIBLES[i]}.app"
+    macdeployqt $tangible
+done
