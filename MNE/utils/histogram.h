d30a3f188d16b436cae319c0863dd494c2c9e53e
rickytjen <ricky270@student.sgu.ac.id>
2016-03-29 16:08:17 +0200

    - finish histogram class in utils library - update histogram in application example

--- MNE/utils/histogram.h
+++ MNE/utils/histogram.h
@@ -8,7 +8,7 @@
 *
 * @section  LICENSE
 *
-* Copyright (C) 2016, Ricky Tjen. All rights reserved.
+* Copyright (C) 2016, Ricky Tjen and Matti Hamalainen. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 * the following conditions are met:
