2ad971cd860a2c15a5d451ad4976f7df1306288f
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2016-04-27 22:13:31 +0200

    Fix wrong variabel declaration in layoutloader class

--- MNE/utils/layoutloader.h
+++ MNE/utils/layoutloader.h
@@ -127,7 +127,7 @@ public:
     * @param [in] channel data holds the x,y and channel number for every channel. The map keys are the channel names (i.e. 'MEG 0113').
     * @return bool true if reading was successful, false otherwise.
     */
-    static bool readMNELoutFile(QString path, QMap<QString, QPointF> &channelData);
+    static bool readMNELoutFile(const QString &path, QMap<QString, QPointF> &channelData);
 
 private:
 
