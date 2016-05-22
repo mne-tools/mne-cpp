9035e65f19451a6fce19e2dae9f8d7afccc6e959
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-05-22 18:48:25 +0200

    test fiff read write read

--- tools/travis/linux_tests/after_success.sh
+++ tools/travis/linux_tests/after_success.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #set -ev
 
+# Clone MNE-CPP test data
+git clone https://github.com/mne-tools/mne-cpp-test-data.git mne-cpp-test-data
+
+#tbd: later on do a grep of all cpps within testframe
 # Create Code Coverage
 ./bin/test_codecov
-#tbd: later on do a grep of all cpps within testframe
+./bin/test_fiff_rwr
+
 gcov ./testframes/test_codecov/test_codecov.cpp
+gcov ./testframes/test_fiff_rwr/test_fiff_rwr.cpp
 
 # Report code coverage; instead of "bash <(curl -s https://codecov.io/bash)" use python "codecov"
 codecov
