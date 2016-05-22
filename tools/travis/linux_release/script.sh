b4a9930f7b22e6165b311311e0ad6aac29348b3f
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2016-05-22 12:56:32 +0200

    create separate test machine

--- tools/travis/linux_release/script.sh
+++ tools/travis/linux_release/script.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -ev
 
-# QA: static code analysis with CppCheck
-cppcheck --enable=all -f -q -i./include/3rdParty/eigen3 -i/opt/qt56/include ./MNE ./applications ./testframes ./examples
-
-# Configure with dynamic code analysis
-qmake -v
-qmake -r MNECPP_CONFIG+=withCodeCov
+# Configure without tests
+qmake -r MNECPP_CONFIG+=noTests
 
 # Build
 make -j2
