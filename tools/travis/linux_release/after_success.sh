c05ee51716723560b4e137aa80675c4cdbe16ef9
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2016-05-22 12:02:18 +0200

    changed path

--- tools/travis/linux_release/after_success.sh
+++ tools/travis/linux_release/after_success.sh
@@ -6,5 +6,5 @@ set -ev
 #tbd: later on do a grep of all cpps within testframe
 gcov ./testframes/test_codecov/test_codecov.cpp
 
-# Report code coverage; instead of: bash <(curl -s https://codecov.io/bash): use python codecov
+# Report code coverage; instead of "bash <(curl -s https://codecov.io/bash)" use python "codecov"
 codecov
