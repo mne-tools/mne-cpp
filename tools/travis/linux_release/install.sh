8ce312c772eb09676c9f76e7887b8468644b1391
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2016-05-22 12:16:33 +0200

    sourcing qt env fails

--- tools/travis/linux_release/install.sh
+++ tools/travis/linux_release/install.sh
@@ -3,7 +3,7 @@ set -ev
 
 # Install Qt 5.6, cppcheck, lcov
 sudo apt-get install -qq qt56base qt563d qt56svg qt56serialport cppcheck lcov
-source /opt/qt56/bin/qt56-env.sh
+#source /opt/qt56/bin/qt56-env.sh
 
 # Install Codecov
 sudo pip install codecov
