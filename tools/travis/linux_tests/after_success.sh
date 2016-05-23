4bc40b0e4587dfcaefe972ad07636ab9232d1b93
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-05-23 23:24:24 +0200

    [LIB-91] change to path

--- tools/travis/linux_tests/after_success.sh
+++ tools/travis/linux_tests/after_success.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
+#set -ev
 set -ev
 
 # Clone MNE-CPP test data
 git clone https://github.com/mne-tools/mne-cpp-test-data.git mne-cpp-test-data
 
 # Set Environment variable
-export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH 
+export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH
+MNECPP_ROOT=$(pwd)
 
 #tbd: later on do a grep of all cpps within testframe
 # Create Code Coverage
 ./bin/test_codecov
 ./bin/test_fiff_rwr
 
-gcov ./testframes/test_codecov/test_codecov.cpp -p
 ls
+cd ./testframes/test_codecov
+gcov ./testframes/test_codecov/test_codecov.cpp -r
+ls
+
+cd $(MNECPP_ROOT)
+ls
+
 cd ./testframes/test_fiff_rwr
-gcov ./test_fiff_rwr.cpp -p
+gcov ./test_fiff_rwr.cpp -r
+ls
 
 # Report code coverage; instead of "bash <(curl -s https://codecov.io/bash)" use python "codecov"
 codecov
