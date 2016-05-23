648587d9acd9a8da915aa21ff305081f0d36abf0
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-05-23 20:26:05 +0200

    gcov test

--- tools/travis/linux_tests/after_success.sh
+++ tools/travis/linux_tests/after_success.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -ev
+set -ev
 
 # Clone MNE-CPP test data
 git clone https://github.com/mne-tools/mne-cpp-test-data.git mne-cpp-test-data
@@ -13,7 +13,8 @@ export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH
 ./bin/test_fiff_rwr
 
 gcov ./testframes/test_codecov/test_codecov.cpp -p
-gcov ./testframes/test_fiff_rwr/test_fiff_rwr.cpp -p
+cd /testframes/test_fiff_rwr
+gcov ./test_fiff_rwr.cpp -p
 
 # Report code coverage; instead of "bash <(curl -s https://codecov.io/bash)" use python "codecov"
 codecov
