1a66aa01928d30d79799f05d9f287fd14840d1a1
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2016-06-04 22:06:20 +0200

    [LIB-91] Travis doxygen upload

--- tools/travis/linux_release/after_success.sh
+++ tools/travis/linux_release/after_success.sh
@@ -4,3 +4,15 @@
 # Run Doxygen
 cd doc
 doxygen mne-cpp_doxyfile_release
+
+# zip documentation build artefact 
+tar cfvz mne-cpp_doc.tar.gz ./html ./qt-creator_doc
+
+# upload documentation
+ftp -n $DOC_REMOTE_SERVER <<INPUT_END
+quote user $DOC_LOGIN
+quote pass $DOC_PASSWORD
+prompt off
+mput mne-cpp_doc.tar.gz
+exit
+INPUT_END
