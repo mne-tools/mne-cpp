049bf1737c0f3dbd848a803eb5784ba8cb83bb2a
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-06-09 23:00:41 +0200

    [LIB-91] Doc change remote server

--- tools/travis/documentation/after_success.sh
+++ tools/travis/documentation/after_success.sh
@@ -9,7 +9,7 @@ doxygen mne-cpp_doxyfile_release
 tar cfvz mne-cpp_doc.tar.gz ./html ./qt-creator_doc
 
 # upload documentation
-curl -u $DOC_LOGIN:$DOC_PASSWORD -T mne-cpp_doc.tar.gz ftp://$DOC_REMOTE_SERVER/
+curl -u $DOC_LOGIN:$DOC_PASSWORD -T mne-cpp_doc.tar.gz ftp://$REMOTE_SERVER/
 
 # update the docu
 wget -O – -q http://doc.mne-cpp.org/maintenance/update.php
