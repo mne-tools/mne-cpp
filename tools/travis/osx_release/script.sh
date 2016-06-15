e800a732ae7a4a65a9885fb9a61b8a3e3d2ebbbf
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-06-15 22:51:55 +0200

    [LIB-91] install dylib bundler

--- tools/travis/osx_release/script.sh
+++ tools/travis/osx_release/script.sh
@@ -10,15 +10,26 @@ QMAKE_VERSION=`qmake -v`
 QT_LIB_DIR=${QMAKE_VERSION##*in }
 QT_BIN_DIR=${QT_LIB_DIR%/*}/bin
 
+MNE_LIB_DIR=$(pwd)/lib
+
 PATH=$QT_BIN_DIR:$PATH
 export PATH
 
-MNE_LIB_DIR=$(pwd)/lib
-
-echo $MNE_LIB_DIR
-echo $QT_LIB_DIR
-echo $QT_BIN_DIR
-echo $PATH
+DYLD_LIBRARY_PATH=$MNE_LIB_DIR:$DYLD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH
+DYLD_FALLBACK_LIBRARY_PATH=$MNE_LIB_DIR
+export DYLD_FALLBACK_LIBRARY_PATH
 
 
+# === Deployment ===
 TANGIBLES=(mne_x mne_browse_raw_qt mne_analyze_qt)
+n_elements=${#TANGIBLES[@]}
+for ((i = 0; i < n_elements; i++)); do
+    fixfile="./bin/${TANGIBLES[i]}.app/Contents/MacOS/${TANGIBLES[i]}"
+    destdir="./bin/${TANGIBLES[i]}.app/Contents/Frameworks/"
+
+	dylibbundler -od -b -x $fixfile -d $destdir -p @executable_path/../Frameworks/
+	
+    tangible="./bin/${TANGIBLES[i]}.app"
+    macdeployqt $tangible -dmg
+done
