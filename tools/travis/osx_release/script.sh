72b1d03e1b84183fd494778b82a22dc24f7a83b8
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-06-15 21:45:19 +0200

    [LIB-91] MNE_LIB_DIR added

--- tools/travis/osx_release/script.sh
+++ tools/travis/osx_release/script.sh
@@ -8,7 +8,15 @@
 
 QMAKE_VERSION=`qmake -v`
 QT_LIB_DIR=${QMAKE_VERSION##*in }
-QT_BIN_DIR=$QT_LIB_DIR/../bin/
-echo $QT_LIB_DIR
-echo $QT_BIN_DIR
+QT_BIN_DIR=$QT_LIB_DIR/../bin
+
+PATH=$QT_BIN_DIR:$PATH
+export PATH
+
+MNE_LIB_DIR=$(pwd)/lib
+
+echo $MNE_LIB_DIR
 echo $PATH
+
+
+TANGIBLES=(mne_x mne_browse_raw_qt mne_analyze_qt)
