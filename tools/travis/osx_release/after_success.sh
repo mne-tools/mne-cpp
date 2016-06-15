7d274ba480711eff68d1e98ee0691f80b174d789
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-06-15 23:22:33 +0200

    [LIB-91] Enable full Mac OS build again

--- tools/travis/osx_release/after_success.sh
+++ tools/travis/osx_release/after_success.sh
@@ -2,9 +2,34 @@
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
 
+	#Extract Qt dirs from actual version
+    QMAKE_VERSION=`qmake -v`
+    QT_LIB_DIR=${QMAKE_VERSION##*in }
+    QT_BIN_DIR=${QT_LIB_DIR%/*}/bin
+ 
+    MNE_LIB_DIR=$(pwd)/lib
+
+    PATH=$QT_BIN_DIR:$PATH
+    export PATH
+
+    DYLD_LIBRARY_PATH=$MNE_LIB_DIR:$DYLD_LIBRARY_PATH
+    export DYLD_LIBRARY_PATH
+    DYLD_FALLBACK_LIBRARY_PATH=$MNE_LIB_DIR
+    export DYLD_FALLBACK_LIBRARY_PATH
 
 
+    # === Deployment ===
+    TANGIBLES=(mne_x mne_browse_raw_qt mne_analyze_qt)
+    n_elements=${#TANGIBLES[@]}
+    for ((i = 0; i < n_elements; i++)); do
+        fixfile="./bin/${TANGIBLES[i]}.app/Contents/MacOS/${TANGIBLES[i]}"
+        destdir="./bin/${TANGIBLES[i]}.app/Contents/Frameworks/"
 
+        dylibbundler -od -b -x $fixfile -d $destdir -p @executable_path/../Frameworks/
+		
+        tangible="./bin/${TANGIBLES[i]}.app"
+        macdeployqt $tangible -dmg
+	done
 
 
     #Master
--- tools/travis/osx_release/install.sh
+++ tools/travis/osx_release/install.sh
@@ -3,6 +3,6 @@
 # Install Qt 5.6
 brew install qt5
 brew link --force qt5
+
 # Install dylibbundler
 brew install dylibbundler
-brew link --force dylibbundler
