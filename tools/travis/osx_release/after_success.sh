cdce72bac261cf898d29645bb89d880989a2c0c8
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-06-15 20:12:11 +0200

    [LIB-91] MAC OS Deploy

--- tools/travis/osx_release/after_success.sh
+++ tools/travis/osx_release/after_success.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+
+
+
+
+
+
+    #Master
+#    if [[ $TRAVIS_BRANCH == 'master' ]]; then
+#        # upload artifacts
+#        curl -u $MASTER_LOGIN:$MASTER_PASSWORD -T $archive_name ftp://$REMOTE_SERVER/
+#    elif [[ $TRAVIS_BRANCH == '1.0.0' ]]; then
+#        # upload artifacts
+#        curl -u $ONEOO_LOGIN:$ONEOO_PASSWORD -T $archive_name ftp://$REMOTE_SERVER/
+#    fi
+fi
