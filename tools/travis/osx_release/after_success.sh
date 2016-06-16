0306620b2c66191c017e46adde3ba69de0baf37e
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-06-16 08:06:13 +0200

    [LIB-91] Tangible upload

--- tools/travis/osx_release/after_success.sh
+++ tools/travis/osx_release/after_success.sh
@@ -31,13 +31,23 @@ if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
         macdeployqt $tangible -dmg
 	done
 
-
-    #Master
-#    if [[ $TRAVIS_BRANCH == 'master' ]]; then
-#        # upload artifacts
-#        curl -u $MASTER_LOGIN:$MASTER_PASSWORD -T $archive_name ftp://$REMOTE_SERVER/
-#    elif [[ $TRAVIS_BRANCH == '1.0.0' ]]; then
-#        # upload artifacts
-#        curl -u $ONEOO_LOGIN:$ONEOO_PASSWORD -T $archive_name ftp://$REMOTE_SERVER/
-#    fi
+	
+    # === Copy Tangibles ===
+    if [[ $TRAVIS_BRANCH == 'master' ]]; then
+        for ((i = 0; i < n_elements; i++)); do
+            cp "./bin/${TANGIBLES[i]}.dmg" "./"
+            tangible_name="${TANGIBLES[i]}-$TRAVIS_BRANCH.dmg"
+            mv "./${TANGIBLES[i]}.dmg" $tangible_name
+            # upload artifacts
+            curl -u $MASTER_LOGIN:$MASTER_PASSWORD -T $tangible_name ftp://$REMOTE_SERVER/
+        done
+    elif [[ $TRAVIS_BRANCH == '1.0.0' ]]; then
+        for ((i = 0; i < n_elements; i++)); do
+            cp "./bin/${TANGIBLES[i]}.dmg" "./"
+            tangible_name="${TANGIBLES[i]}-$TRAVIS_BRANCH.dmg"
+            mv "./${TANGIBLES[i]}.dmg" $tangible_name
+            # upload artifacts
+            curl -u $ONEOO_LOGIN:$ONEOO_PASSWORD -T $tangible_name ftp://$REMOTE_SERVER/
+        done
+    fi
 fi
