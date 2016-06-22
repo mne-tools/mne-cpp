066facb6b32cc9807376dd07c94b2e9ad0fdc0cc
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-06-22 23:44:10 +0200

    [LIB-91] Cleanup and GraphViz adjustments

--- tools/travis/documentation/after_success.sh
+++ tools/travis/documentation/after_success.sh
@@ -5,12 +5,10 @@
 cd doc
 doxygen mne-cpp_doxyfile
 
-# zip documentation build artefact 
-#tar cfvz mne-cpp_doc.tar.gz ./html ./qt-creator_doc
+# zip documentation build artefact
 zip -r mne-cpp_doc.zip ./html ./qt-creator_doc
 
 # upload documentation
-#curl -u $DOC_LOGIN:$DOC_PASSWORD -T mne-cpp_doc.tar.gz ftp://$REMOTE_SERVER/
 curl -u $DOC_LOGIN:$DOC_PASSWORD -T mne-cpp_doc.zip ftp://$REMOTE_SERVER/
 
 # update the docu
