31002358fe69c3c4c6cc2f75e602445752034f3e
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-06-22 23:11:58 +0200

    [LIB-91] Create zip archive due to tar name restrictions

--- tools/travis/documentation/after_success.sh
+++ tools/travis/documentation/after_success.sh
@@ -6,10 +6,12 @@ cd doc
 doxygen mne-cpp_doxyfile
 
 # zip documentation build artefact 
-tar cfvz mne-cpp_doc.tar.gz ./html ./qt-creator_doc
+#tar cfvz mne-cpp_doc.tar.gz ./html ./qt-creator_doc
+zip -r mne-cpp_doc.zip ./html ./qt-creator_doc
 
 # upload documentation
-curl -u $DOC_LOGIN:$DOC_PASSWORD -T mne-cpp_doc.tar.gz ftp://$REMOTE_SERVER/
+#curl -u $DOC_LOGIN:$DOC_PASSWORD -T mne-cpp_doc.tar.gz ftp://$REMOTE_SERVER/
+curl -u $DOC_LOGIN:$DOC_PASSWORD -T mne-cpp_doc.zip ftp://$REMOTE_SERVER/
 
 # update the docu
 wget -O – -q http://doc.mne-cpp.org/maintenance/update.php
