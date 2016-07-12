8d42cc7c544135b1418af09557bce9c3673b877e
rickytjen <rickytjen@yahoo.com>
2016-07-12 17:18:50 +0200

    [LIB-102]add qt+= charts in disp3D.pro

---
+++ applications/mne_x/plugins/eegosports/eemagine/sdk/version.h
@@ -0,0 +1,9 @@
+#ifndef __eemagine_sdk_version_h__
+#define __eemagine_sdk_version_h__
+
+#define EEGO_SDK_VERSION_MAJOR 1
+#define EEGO_SDK_VERSION_MINOR 3
+#define EEGO_SDK_VERSION_MICRO 6
+#define EEGO_SDK_VERSION       33416
+
+#endif
