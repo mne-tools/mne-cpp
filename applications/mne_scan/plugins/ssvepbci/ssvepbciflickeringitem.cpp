70bd553cfa3178f63168657196720ab2ac619563
ViktorKL <viktor.klueber@tu-ilmenau.de>
2016-07-18 20:05:40 +0200

    [SC-115] edit screen-keyboard

--- applications/mne_scan/plugins/ssvepbci/ssvepbciflickeringitem.cpp
+++ applications/mne_scan/plugins/ssvepbci/ssvepbciflickeringitem.cpp
@@ -132,12 +132,12 @@ void ssvepBCIFlickeringItem::paint(QPaintDevice *paintDevice)
 
     QRect rectangle(m_dPosX*paintDevice->width(),m_dPosY*paintDevice->height(),m_dWidth*paintDevice->width(),m_dHeight*paintDevice->height());
 
-    if(true){
+    if(m_bFlickerState){
         p.fillRect(rectangle,Qt::white);
         p.drawText(rectangle, Qt::AlignCenter, m_sSign);
     }
-//    else
-//        p.fillRect(m_dPosX*paintDevice->width(),m_dPosY*paintDevice->height(),m_dWidth*paintDevice->width(),m_dHeight*paintDevice->height(),Qt::black);
+    else
+        p.fillRect(m_dPosX*paintDevice->width(),m_dPosY*paintDevice->height(),m_dWidth*paintDevice->width(),m_dHeight*paintDevice->height(),Qt::black);
 
 
 }
