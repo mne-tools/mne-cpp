3d6a0daf575a18f5432077c079a958c3ac3927fc
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-11-01 12:01:02 -0400

    change to default sample data files

--- examples/clusteredInverse/main.cpp
+++ examples/clusteredInverse/main.cpp
@@ -274,9 +274,9 @@ int main(int argc, char *argv[])
     //Source Estimate end
     //########################################################################################
 
-//    SurfaceSet t_surfSet("./MNE-sample-data/subjects/sample/surf/lh.white", "./MNE-sample-data/subjects/sample/surf/rh.white");
+    SurfaceSet t_surfSet("./MNE-sample-data/subjects/sample/surf/lh.white", "./MNE-sample-data/subjects/sample/surf/rh.white");
 //    SurfaceSet t_surfSet("/home/chdinh/sl_data/subjects/mind006/surf/lh.white", "/home/chdinh/sl_data/subjects/mind006/surf/rh.white");
-    SurfaceSet t_surfSet("E:/Data/sl_data/subjects/mind006/surf/lh.white", "E:/Data/sl_data/subjects/mind006/surf/rh.white");
+//    SurfaceSet t_surfSet("E:/Data/sl_data/subjects/mind006/surf/lh.white", "E:/Data/sl_data/subjects/mind006/surf/rh.white");
 
 //    //only one time point - P100
 //    qint32 sample = 0;
