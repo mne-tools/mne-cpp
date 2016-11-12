b2c75b39740b561de09a74b982a0528cb6ccf8ed
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2016-11-12 13:13:18 -0500

    Added Quit MNE Launch

--- applications/mne_launch/main.cpp
+++ applications/mne_launch/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+    engine.load(QUrl(QLatin1String("qrc:/qml/main.qml")));
 
     return app.exec();
 }
