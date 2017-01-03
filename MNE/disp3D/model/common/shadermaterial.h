415f052a3a1966fcfd7d78727067091eb34f2e20
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2017-01-03 19:32:18 +0100

    [LIB-62] Improve default shader handling

--- MNE/disp3D/model/common/shadermaterial.h
+++ MNE/disp3D/model/common/shadermaterial.h
@@ -153,26 +153,28 @@ private:
     */
     void init();
 
-    QPointer<Qt3DRender::QEffect>            m_pVertexEffect;
+    bool                                    m_bShaderInit;
 
-    QPointer<Qt3DRender::QParameter>         m_pAmbientParameter;
-    QPointer<Qt3DRender::QParameter>         m_pDiffuseParameter;
-    QPointer<Qt3DRender::QParameter>         m_pSpecularParameter;
-    QPointer<Qt3DRender::QParameter>         m_pShininessParameter;
-    QPointer<Qt3DRender::QParameter>         m_pAlphaParameter;
-    QPointer<Qt3DRender::QFilterKey>         m_pFilterKey;
+    QPointer<Qt3DRender::QEffect>           m_pVertexEffect;
 
-    QPointer<Qt3DRender::QParameter>         m_pInnerTessParameter;
-    QPointer<Qt3DRender::QParameter>         m_pOuterTessParameter;
-    QPointer<Qt3DRender::QParameter>         m_pTriangleScaleParameter;
+    QPointer<Qt3DRender::QParameter>        m_pAmbientParameter;
+    QPointer<Qt3DRender::QParameter>        m_pDiffuseParameter;
+    QPointer<Qt3DRender::QParameter>        m_pSpecularParameter;
+    QPointer<Qt3DRender::QParameter>        m_pShininessParameter;
+    QPointer<Qt3DRender::QParameter>        m_pAlphaParameter;
+    QPointer<Qt3DRender::QFilterKey>        m_pFilterKey;
 
-    QPointer<Qt3DRender::QTechnique>         m_pVertexGL3Technique;
-    QPointer<Qt3DRender::QRenderPass>        m_pVertexGL3RenderPass;
-    QPointer<Qt3DRender::QShaderProgram>     m_pVertexGL3Shader;
+    QPointer<Qt3DRender::QParameter>        m_pInnerTessParameter;
+    QPointer<Qt3DRender::QParameter>        m_pOuterTessParameter;
+    QPointer<Qt3DRender::QParameter>        m_pTriangleScaleParameter;
 
-    QPointer<Qt3DRender::QNoDepthMask>                   m_pNoDepthMask;
-    QPointer<Qt3DRender::QBlendEquationArguments>        m_pBlendState;
-    QPointer<Qt3DRender::QBlendEquation>                 m_pBlendEquation;
+    QPointer<Qt3DRender::QTechnique>        m_pVertexGL3Technique;
+    QPointer<Qt3DRender::QRenderPass>       m_pVertexGL3RenderPass;
+    QPointer<Qt3DRender::QShaderProgram>    m_pVertexGL3Shader;
+
+    QPointer<Qt3DRender::QNoDepthMask>                  m_pNoDepthMask;
+    QPointer<Qt3DRender::QBlendEquationArguments>       m_pBlendState;
+    QPointer<Qt3DRender::QBlendEquation>                m_pBlendEquation;
 };
 
 } // namespace DISP3DLIB
