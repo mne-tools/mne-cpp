85afab9d602395a8e67949fad3cb5ddcea63c16c
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2017-01-06 19:39:04 +0100

    [LIB-62] Fix includes

--- examples/connectivityTutorial/main.cpp
+++ examples/connectivityTutorial/main.cpp
@@ -42,7 +42,7 @@
 
 #include <disp3D/view3D.h>
 #include <disp3D/control/control3dwidget.h>
-#include <disp3D/model/sourceactivity/mneestimatetreeitem.h>
+#include <disp3D/model/items/sourceactivity/mneestimatetreeitem.h>
 #include <disp3D/model/data3Dtreemodel.h>
 
 #include <disp/imagesc.h>
