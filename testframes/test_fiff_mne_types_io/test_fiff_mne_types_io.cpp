a3de436df0266cde08403b352449698a3a112a07
Christoph Dinh <christoph.dinh@tu-ilmenau.de>
2017-01-22 13:00:22 -0500

    [LIB-129] Added new Fiff MNE Struct IO test & Moved deprecated tests

--- testframes/test_fiff_mne_types_io/test_fiff_mne_types_io.cpp
+++ testframes/test_fiff_mne_types_io/test_fiff_mne_types_io.cpp
@@ -4,11 +4,11 @@
 * @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
 *           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
 * @version  1.0
-* @date     July, 2012
+* @date     December, 2015
 *
 * @section  LICENSE
 *
-* Copyright (C) 2012, Christoph Dinh and Matti Hamalainen. All rights reserved.
+* Copyright (C) 2015, Christoph Dinh and Matti Hamalainen. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 * the following conditions are met:
@@ -29,21 +29,19 @@
 * POSSIBILITY OF SUCH DAMAGE.
 *
 *
-* @brief    Implements the main() application function.
+* @brief    The fiff mne types io unit test
 *
 */
+
 
 //*************************************************************************************************************
 //=============================================================================================================
 // INCLUDES
 //=============================================================================================================
 
-#include <disp3D/view3D.h>
-#include <disp3D/control/control3dwidget.h>
+#include <fiff/fiff.h>
 
-#include "mainwindow.h"
-
-#include <mne/mne_forwardsolution.h>
+#include <iostream>
 
 
 //*************************************************************************************************************
@@ -51,7 +49,7 @@
 // QT INCLUDES
 //=============================================================================================================
 
-#include <QApplication>
+#include <QtTest>
 
 
 //*************************************************************************************************************
@@ -59,9 +57,97 @@
 // USED NAMESPACES
 //=============================================================================================================
 
-using namespace MNELIB;
-using namespace DISP3DLIB;
-using namespace MNELIB;
+using namespace FIFFLIB;
+
+//=============================================================================================================
+/**
+* DECLARE CLASS TestFiffRWR
+*
+* @brief The TestFiffRWR class provides read write read fiff verification tests
+*
+*/
+class TestFiffMneTypesIO: public QObject
+{
+    Q_OBJECT
+
+public:
+    TestFiffMneTypesIO();
+
+private slots:
+    void initTestCase();
+    void checkFiffCoordTrans();
+    void cleanupTestCase();
+
+private:
+    double epsilon;
+
+    QString rawName;
+    QString evokedName;
+};
+
+
+//*************************************************************************************************************
+
+TestFiffMneTypesIO::TestFiffMneTypesIO()
+: epsilon(0.000001)
+, rawName("./mne-cpp-test-data/MEG/sample/sample_audvis_raw_short.fif")
+, evokedName("./mne-cpp-test-data/MEG/sample/sample_audvis-ave.fif")
+{
+}
+
+
+//*************************************************************************************************************
+
+void TestFiffMneTypesIO::initTestCase()
+{
+    qDebug() << "Epsilon" << epsilon;
+    qDebug() << "Raw File Name" << rawName;
+    qDebug() << "Evoked File Name" << evokedName;
+}
+
+//*************************************************************************************************************
+
+void TestFiffMneTypesIO::checkFiffCoordTrans()
+{
+    QFile file(evokedName);
+    FiffStream::SPtr stream(new FiffStream(&file));
+    if(!stream->open())
+        QFAIL("Failed to open data file.");
+
+    FiffTag::SPtr t_pTag;
+    fiffCoordTransRec reference_trans;
+    FiffCoordTrans trans;
+
+    bool trans_found = false;
+
+    for (int k = 0; k < stream->dir().size(); k++) {
+        if(stream->dir()[k]->kind == FIFF_COORD_TRANS) {
+            if(!FiffTag::read_tag(stream, t_pTag, stream->dir()[k]->pos))
+                QFAIL("Failed to read FIFF_COORD_TRANS tag.");
+            reference_trans = *(fiffCoordTrans)t_pTag->data();
+            trans = t_pTag->toCoordTrans();
+
+            //CHECKS
+            QVERIFY(reference_trans.from == trans.from);
+            QVERIFY(reference_trans.to == trans.to);
+
+            qDebug() << "Some more checks required";
+
+            trans_found = true;
+        }
+    }
+
+    if(!trans_found)
+        QFAIL("No FIFF_COORD_TRANS found.");
+
+    stream->device()->close();
+}
+
+//*************************************************************************************************************
+
+void TestFiffMneTypesIO::cleanupTestCase()
+{
+}
 
 
 //*************************************************************************************************************
@@ -69,40 +155,5 @@ using namespace MNELIB;
 // MAIN
 //=============================================================================================================
 
-//=============================================================================================================
-/**
-* The function main marks the entry point of the program.
-* By default, main has the storage class extern.
-*
-* @param [in] argc (argument count) is an integer that indicates how many arguments were entered on the command line when the program was started.
-* @param [in] argv (argument vector) is an array of pointers to arrays of character objects. The array objects are null-terminated strings, representing the arguments that were entered on the command line when the program was started.
-* @return the value that was set to exit() (which is 0 if exit() is called via quit()).
-*/
-int main(int argc, char *argv[])
-{
-//    QGuiApplication app(argc, argv);
-    QApplication app(argc, argv);
-
-    MainWindow w;
-    w.show();
-
-    QFile t_File("./MNE-sample-data/MEG/sample/sample_audvis-meg-eeg-oct-6-fwd.fif");
-    MNEForwardSolution t_forwardSolution(t_File);
-
-    View3D::SPtr testWindow = View3D::SPtr(new View3D());
-    testWindow->addBrainData("Subject01", "ForwardSolution", t_forwardSolution);
-
-    Control3DWidget::SPtr control3DWidget = Control3DWidget::SPtr(new Control3DWidget());
-    control3DWidget->setView3D(testWindow);
-    control3DWidget->show();
-
-    QWidget *container = QWidget::createWindowContainer(testWindow.data());
-//    container->setMinimumSize(...);
-//    container->setMaximumSize(...);
-    container->setFocusPolicy(Qt::TabFocus);
-
-    w.setCentralWidget(container);
-//    widgetLayout->addWidget(container);
-
-    return app.exec();
-}
+QTEST_APPLESS_MAIN(TestFiffMneTypesIO)
+#include "test_fiff_mne_types_io.moc"
