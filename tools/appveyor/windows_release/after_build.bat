fde79c3c8edb457f4f6f2011335a4b0a1d121831
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2017-02-01 15:43:26 -0500

    [LIB-153] Appveyor after build config

--- tools/appveyor/windows_release/after_build.bat
+++ tools/appveyor/windows_release/after_build.bat
@@ -1,7 +1,12 @@
 :: ###startdir### %WORKSPACE%/mne-cpp/..
-
 echo off
 :: ### %0 Batch filename itself ###
 set arg0=%0
 
-echo Called Filename: %arg0%
+echo %arg0%
+
+echo %APPVEYOR_REPO_BRANCH%
+
+call .\tools\appveyor\windows_release\compress_binaries.bat %APPVEYOR_REPO_BRANCH%
+call .\tools\appveyor\windows_release\build_installer.bat %APPVEYOR_REPO_BRANCH%
+dir
