62a10e99d14b89b831296e0f64dce534b9bbe729
Viktor <viktor.klueber@tu-ilmenau.de>
2017-02-03 12:30:04 +0100

    [SC-142] modify GUI of ReRef project

--- applications/mne_scan/plugins/reref/reref.cpp
+++ applications/mne_scan/plugins/reref/reref.cpp
@@ -9,7 +9,7 @@
 *
 * @section  LICENSE
 *
-* Copyright (C) 2016, Viktor Klüber, Lorenz Esch and Matti Hamalainen. All rights reserved.
+* Copyright (C) 2017, Viktor Klüber, Lorenz Esch and Matti Hamalainen. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 * the following conditions are met:
