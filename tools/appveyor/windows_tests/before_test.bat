770073ae6ba0d5f166f915a01aa56e02967060b3
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2017-02-03 18:52:29 -0500

    [LIB-153] Check if Appveyor fails when test fails

--- tools/appveyor/windows_tests/before_test.bat
+++ tools/appveyor/windows_tests/before_test.bat
@@ -2,6 +2,6 @@
 cd bin
 
 :: ### Clone MNE-CPP test data ###
-call git clone https://github.com/mne-tools/mne-cpp-test-data.git mne-cpp-test-data
+:: call git clone https://github.com/mne-tools/mne-cpp-test-data.git mne-cpp-test-data
 
 cd ..
