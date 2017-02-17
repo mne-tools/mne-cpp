60c0e155bcf3544309aae1eef44bd858d365256d
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2017-02-17 14:42:22 +0100

    [SC-140] Further forward declarations in EEGoSports plugin

--- applications/mne_scan/plugins/eegosports/FormFiles/eegosportssetupwidget.h
+++ applications/mne_scan/plugins/eegosports/FormFiles/eegosportssetupwidget.h
@@ -79,6 +79,7 @@ class EEGoSports;
 class EEGoSportsSetupWidget : public QWidget
 {
     Q_OBJECT
+
 public:
 
     //=========================================================================================================
