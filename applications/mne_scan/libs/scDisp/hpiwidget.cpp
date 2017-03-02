900f7de6c08eeafdf4119ecb5a8bb9cc5be25128
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2017-03-02 17:24:52 +0100

    [LIB-131] Add SSP and SGM usage to HPI in Fiff Simulator.

--- applications/mne_scan/libs/scDisp/hpiwidget.cpp
+++ applications/mne_scan/libs/scDisp/hpiwidget.cpp
@@ -289,7 +289,7 @@ QList<FiffDigPoint> HPIWidget::readPolhemusDig(QString fileName)
     QVector<double> vGof;
     vGof << 0.0 << 0.0 << 0.0 << 0.0;
 
-    this->setDigitizerDataToView3D(t_digSet, FiffDigPointSet(), vGof);
+    //this->setDigitizerDataToView3D(t_digSet, FiffDigPointSet(), vGof);
 
     //Set loaded number of digitizers
     ui->m_label_numberLoadedCoils->setNum(numHPI);
@@ -425,14 +425,12 @@ void HPIWidget::performHPIFitting(const QVector<int>& vFreqs)
             transDevHead.from = 1;
             transDevHead.to = 4;
 
-            qDebug() << "HPIWidget::performHPIFitting - 0";
             pRtHpis->singleHPIFit(m_matValue,
                                   transDevHead,
                                   vFreqs,
                                   vGof,
                                   t_fittedSet);
 
-            qDebug() << "HPIWidget::performHPIFitting - 1";
             m_matValue.resize(0,0);
 
             //Set newly calculated transforamtion amtrix to fiff info
