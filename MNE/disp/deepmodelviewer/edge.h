acfd706a84c4b54356c62205c5e778dff9eee25c
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2017-03-28 00:15:29 -0400

    [LIB-145] Fix comments

--- MNE/disp/deepmodelviewer/edge.h
+++ MNE/disp/deepmodelviewer/edge.h
@@ -110,18 +110,25 @@ public:
     * @param [in] weight    The weight to set
     */
     void setWeight(float weight);
+    //=========================================================================================================
+    /**
+    * Returns the weight of the node
+    *
+    * @return Node weight
+    */
     inline float weight() const;
 
     enum { Type = UserType + 2 };
     int type() const override { return Type; }
 
-    inline bool attached() const;
-    inline void setAttached(bool attached);
-
 protected:
     QRectF boundingRect() const override;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 
+    //=========================================================================================================
+    /**
+    * Updates the edge color based on the current weight
+    */
     void updateColor();
 
 private:
@@ -136,8 +143,6 @@ private:
     float   m_weight;           /**< The Weight related to the edge */
 
     Network* m_pNetwork;        /**< The network this edge is part of.*/
-
-    bool m_bIsAttached;         /**< If node is attached to a scene TODO: Move this to Edge, Node basis class*/
 };
 
 //*************************************************************************************************************
@@ -149,24 +154,6 @@ float Edge::weight() const
 {
     return m_weight;
 }
-
-
-//*************************************************************************************************************
-
-bool Edge::attached() const
-{
-    return m_bIsAttached;
-}
-
-
-//*************************************************************************************************************
-
-void Edge::setAttached(bool attached)
-{
-    m_bIsAttached = attached;
-}
-
-
 
 } // NAMESPACE
 
