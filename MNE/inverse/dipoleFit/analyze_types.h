f814401f65a83607636694067d5fe8c38c0fabb0
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2017-04-09 11:46:02 +0200

    [LIB-161]Init fiff digitizer data class needed for alignment coded

--- MNE/inverse/dipoleFit/analyze_types.h
+++ MNE/inverse/dipoleFit/analyze_types.h
@@ -214,4 +214,69 @@ typedef struct {
   mneUserFreeFunc  user_data_free;                   /* Called to free the above object */
 } *mshMegEegData,mshMegEegDataRec;
 
+typedef struct {
+  FIFFLIB::FiffSparseMatrix* map;		/* Multiply the data in the from surface with this to get to
+                 * 'this' surface from the 'from' surface */
+  int *best;			/* For each point on 'this' surface, the closest point on 'from' surface */
+  int from_kind;		/* The kind field of the other surface */
+  char *from_subj;		/* Name of the subject of the other surface */
+} *morphMap,morphMapRec;
+
+typedef struct {
+  int   vert;			/* Vertex # */
+  int   sparse;			/* Is this a isolated point? */
+} *mshPicked,mshPickedRec;
+
+typedef struct {		/* Display surface properties */
+  char           *filename;	/* Where did this surface come from? */
+  time_t         time_loaded;	/* When was the surface loaded */
+  char           *subj;		/* The name of the subject in SUBJECTS_DIR */
+  char           *surf_name;	/* The name of the surface */
+  MNELIB::MneSurfaceOld*     s;		/* This is the surface */
+  float          eye[3];	/* Eye position for viewing */
+  float          up[3];		/* Up vector for viewing */
+  float          rot[3];        /* Rotation angles of the MRI (in radians) */
+  float          move[3];	/* Possibly move the origin, too */
+
+  float          fov;		/* Field of view (extent of the surface) */
+  float          fov_scale;	/* How much space to leave */
+  float          minv[3];	/* Minimum values along the three coordinate axes */
+  float          maxv[3];	/* Maximum values along the three coordinate axes */
+  float          *trans;	/* Extra transformation for this surface */
+  int            sketch;	/* Use sketch mode if decimated triangulation is available? */
+
+  morphMap       *maps;		/* Morphing maps from other surfaces to this */
+  int            nmap;		/* Normally just one */
+
+  int   overlay_type;	        /* What are the overlay values? */
+  float *overlay_values;	/* Overlay value array */
+  int   alt_overlay_type;	/* A second choice for overlay */
+  float *alt_overlay_values;
+  float *marker_values;		/* Marker values (will be shown in shades of marker color) */
+
+  float *vertex_colors;		/* Vertex color array */
+  mshColorScaleDef* color_scale; /* Color scale used to define these colors */
+  int   nvertex_colors;		/* How many components? */
+  float even_vertex_color[4];	/* This is going to be employed in case of uniform coloring */
+
+  float *marker_colors;		/* Vertex color array (for the markers) */
+  int   nmarker_colors;		/* How many components? */
+  int   **marker_tri;	        /* Triangles containing markers */
+  int   *marker_tri_no;		/* Numbers of the marker triangles */
+  int   nmarker_tri;		/* How many */
+  float marker_color[4];	/* Marker color */
+  int   curvature_color_mode;	/* How to show curvature */
+
+  int   overlay_color_mode;	/* How to show overlay data */
+  int   transparent;		/* Is this surface going to be transparent? */
+
+  int   show_aux_data;		/* Show auxilliary data related to this surface */
+
+  mshPicked* picked;		/* Picked locations in world coordinates */
+  int       npicked;		/* How many */
+
+  void              *user_data;       /* Can be used to store whatever */
+  mneUserFreeFunc*   user_data_free;   /* Function to free the above */
+} *mshDisplaySurface,mshDisplaySurfaceRec;
+
 #endif
