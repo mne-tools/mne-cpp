6c27c771d712de42f59f5e7a75696473836d2ede
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2017-04-13 16:33:57 -0400

    [LIB-145] Fixed Extensions

--- applications/mne_analyze/extensions/stcbrowser/stcbrowser.cpp
+++ applications/mne_analyze/extensions/stcbrowser/stcbrowser.cpp
@@ -118,13 +118,10 @@ QMenu *STCBrowser::getMenu()
 QDockWidget *STCBrowser::getControl()
 {
     if(!m_pControl) {
-        qDebug() << "[1]";
         m_pControl = new QDockWidget(tr("STC Browser"));
         m_pControl->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
         m_pControl->setMinimumWidth(180);
         m_pControl->setWidget(m_pStcControl);
-
-        qDebug() << "[2]";
     }
 
     return m_pControl;
