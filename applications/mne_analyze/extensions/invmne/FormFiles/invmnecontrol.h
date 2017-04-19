f7e8eb7a6ea62b95d080cccb4f4876388f61352b
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2017-04-19 01:05:38 -0400

    [LIB-145] Calculate MNE

--- applications/mne_analyze/extensions/invmne/FormFiles/invmnecontrol.h
+++ applications/mne_analyze/extensions/invmne/FormFiles/invmnecontrol.h
@@ -1,12 +1,65 @@
+//=============================================================================================================
+/**
+* @file     invmnecontrol.h
+* @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
+*           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
+* @version  1.0
+* @date     February, 2017
+*
+* @section  LICENSE
+*
+* Copyright (C) 2017 Christoph Dinh and Matti Hamalainen. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+* the following conditions are met:
+*     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+*       following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+*       the following disclaimer in the documentation and/or other materials provided with the distribution.
+*     * Neither the name of MNE-CPP authors nor the names of its contributors may be used
+*       to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+* PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*
+* @brief    Contains the declaration of the InvMNEControl class.
+*
+*/
+
 #ifndef INVMNECONTROL_H
 #define INVMNECONTROL_H
 
+//*************************************************************************************************************
+//=============================================================================================================
+// INCLUDES
+//=============================================================================================================
+
 #include <QWidget>
 
+
+//*************************************************************************************************************
+//=============================================================================================================
+// FORWARD DECLARATIONS
+//=============================================================================================================
+
 namespace Ui {
-class InvMNEControl;
+    class InvMNEControl;
 }
 
+
+//=============================================================================================================
+/**
+* InvMNEControl Extension Control
+*
+* @brief The InvMNEControl class provides the extension control.
+*/
 class InvMNEControl : public QWidget
 {
     Q_OBJECT
@@ -14,6 +67,24 @@ class InvMNEControl : public QWidget
 public:
     explicit InvMNEControl(QWidget *parent = 0);
     ~InvMNEControl();
+
+signals:
+    void calculate_signal();
+
+private slots:
+    void on_m_qLineEditEvoked_editingFinished();
+
+    void on_m_qComboBoxEventType_currentIndexChanged(const QString &arg1);
+
+    void on_m_qLineEditInv_editingFinished();
+
+    void on_doubleSpinBox_valueChanged(double arg1);
+
+    void on_m_qComboBoxMethod_currentIndexChanged(const QString &arg1);
+
+    void on_m_qLineEditSTC_editingFinished();
+
+    void on_m_qPushButtonCalculate_released();
 
 private:
     Ui::InvMNEControl *ui;
