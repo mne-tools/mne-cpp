be4aea40541785e2f8d8708ff46852fed39d12d2
Christoph Dinh <chdinh@nmr.mgh.harvard.edu>
2017-04-19 01:05:38 -0400

    [LIB-145] Calculate MNE

--- applications/mne_analyze/extensions/invmne/FormFiles/invmnecontrol.cpp
+++ applications/mne_analyze/extensions/invmne/FormFiles/invmnecontrol.cpp
@@ -1,5 +1,53 @@
+//=============================================================================================================
+/**
+* @file     invmnecontrol.cpp
+* @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
+*           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
+* @version  1.0
+* @date     February, 2017
+*
+* @section  LICENSE
+*
+* Copyright (C) 2017 Christoph Dinh and Matti Hamalainen. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+* the following conditions are met:
+*     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+*       following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+*       the following disclaimer in the documentation and/or other materials provided with the distribution.
+*     * Neither the name of MNE-CPP authors nor the names of its contributors may be used
+*       to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+* PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*
+* @brief    Contains the implementation of the InvMNEControl class.
+*
+*/
+
+//*************************************************************************************************************
+//=============================================================================================================
+// INCLUDES
+//=============================================================================================================
+
 #include "invmnecontrol.h"
 #include "ui_invmnecontrol.h"
+
+#include <QDebug>
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// DEFINE MEMBER METHODS
+//=============================================================================================================
 
 InvMNEControl::InvMNEControl(QWidget *parent) :
     QWidget(parent),
@@ -8,7 +56,66 @@ InvMNEControl::InvMNEControl(QWidget *parent) :
     ui->setupUi(this);
 }
 
+
+//*************************************************************************************************************
+
 InvMNEControl::~InvMNEControl()
 {
     delete ui;
+}
+
+
+//*************************************************************************************************************
+
+void InvMNEControl::on_m_qLineEditEvoked_editingFinished()
+{
+    qDebug() << "on_m_qLineEditEvoked_editingFinished()";
+}
+
+
+//*************************************************************************************************************
+
+void InvMNEControl::on_m_qComboBoxEventType_currentIndexChanged(const QString &arg1)
+{
+    qDebug() << "on_m_qComboBoxEventType_currentIndexChanged()";
+}
+
+
+//*************************************************************************************************************
+
+void InvMNEControl::on_m_qLineEditInv_editingFinished()
+{
+    qDebug() << "on_m_qLineEditInv_editingFinished()";
+}
+
+
+//*************************************************************************************************************
+
+void InvMNEControl::on_doubleSpinBox_valueChanged(double arg1)
+{
+    qDebug() << "on_doubleSpinBox_valueChanged()";
+}
+
+
+//*************************************************************************************************************
+
+void InvMNEControl::on_m_qComboBoxMethod_currentIndexChanged(const QString &arg1)
+{
+    qDebug() << "on_m_qComboBoxMethod_currentIndexChanged()";
+}
+
+
+//*************************************************************************************************************
+
+void InvMNEControl::on_m_qLineEditSTC_editingFinished()
+{
+    qDebug() << "on_m_qLineEditSTC_editingFinished()";
+}
+
+
+//*************************************************************************************************************
+
+void InvMNEControl::on_m_qPushButtonCalculate_released()
+{
+    emit calculate_signal();
 }
--- applications/mne_analyze/extensions/invmne/invmne.cpp
+++ applications/mne_analyze/extensions/invmne/invmne.cpp
@@ -48,6 +48,8 @@
 #include <mne/mne.h>
 #include <mne/mne_sourceestimate.h>
 
+#include <inverse/minimumNorm/minimumnorm.h>
+
 #include <utils/mnemath.h>
 
 #include <iostream>
@@ -64,6 +66,7 @@ using namespace MNELIB;
 using namespace FSLIB;
 using namespace FIFFLIB;
 using namespace UTILSLIB;
+using namespace INVERSELIB;
 
 
 //*************************************************************************************************************
@@ -100,7 +103,10 @@ QSharedPointer<IExtension> InvMNE::clone() const
 
 void InvMNE::init()
 {
-    m_pInvMNEControl = new InvMNEControl;
+    if(!m_pInvMNEControl) {
+        m_pInvMNEControl = new InvMNEControl;
+        connect(m_pInvMNEControl, &InvMNEControl::calculate_signal, this, &InvMNE::calculate);
+    }
 }
 
 
@@ -155,15 +161,26 @@ QWidget *InvMNE::getView()
 
 void InvMNE::calculate()
 {
+
+
+//    printf("Data contains %d sets\n",evokedSet.evoked.size());
+//    for(qint32 i = 0; i < evokedSet.evoked.size(); ++i) {
+//        printf("%s\n", evokedSet.evoked[i].comment.toUtf8().constData());
+//    }
+
+
     //*********************************************************************************************************
     // MNE Settings
     //*********************************************************************************************************
 
-    QString sampleEvokedFile("./MNE-sample-data/MEG/sample/sample_audvis-ave.fif");
+    QString evokedFile("./MNE-sample-data/MEG/sample/sample_audvis-ave.fif");
     QString invFile("./MNE-sample-data/MEG/sample/sample_audvis-meg-eeg-oct-6-meg-eeg-inv.fif");
-    QString method("dSPM");//"MNE" | "dSPM" | "sLORETA"
+
+    QString t_sFileNameStc("");
 
     float snr = 1.0f;//2.0f
+    QString method("dSPM");//"MNE" | "dSPM" | "sLORETA"
+
     float lambda2 = pow(1.0f / snr, 2.0f);
 
     int setno = 0;
@@ -173,7 +190,7 @@ void InvMNE::calculate()
     // LOAD DATA
     //*********************************************************************************************************
 
-    QFile t_fileEvoked(sampleEvokedFile);
+    QFile t_fileEvoked(evokedFile);
     QFile t_fileInv(invFile);
 
     bool dSPM = false;
@@ -188,126 +205,52 @@ void InvMNE::calculate()
     //
     //   Read the data first
     //
-    FiffEvokedSet evokedSet(t_fileEvoked);
+    QPair<QVariant, QVariant> baseline(QVariant(), 0);
+
+    FiffEvoked evoked(t_fileEvoked, setno, baseline);
+    if(evoked.isEmpty())
+        return;
 
     //
-    //   Then the inverse operator
+    //   Read the inverse operator
     //
-    MNEInverseOperator inv_raw(t_fileInv);
+    MNEInverseOperator inverse_operator(t_fileInv);
 
 
     //*********************************************************************************************************
     // Compute MNE
     //*********************************************************************************************************
 
-    printf("Data contains %d sets\n",evokedSet.evoked.size());
-    for(qint32 i = 0; i < evokedSet.evoked.size(); ++i) {
-        printf("%s\n", evokedSet.evoked[i].comment.toUtf8().constData());
-    }
 
-    printf(">>>>>>>>>>>>>>>>>>>>>>>>> Compute MNE for %s >>>>>>>>>>>>>>>>>>>>>>>>>\n", evokedSet.evoked[setno].comment.toUtf8().constData());
+    printf(">>>>>>>>>>>>>>>>>>>>>>>>> Compute MNE for %s >>>>>>>>>>>>>>>>>>>>>>>>>\n", evoked.comment.toUtf8().constData());
 
     //
-    //   Set up the inverse according to the parameters
+    // Compute inverse solution
     //
-    qint32 nave = evokedSet.evoked[setno].nave;
-
-    MNEInverseOperator inv = inv_raw.prepare_inverse_operator(nave,lambda2,dSPM,sLORETA);
-    //
-    //   Pick the correct channels from the data
-    //
-    FiffEvokedSet newEvokedSet = evokedSet.pick_channels(inv.noise_cov->names);
-
-    evokedSet = newEvokedSet;
-
-    printf("Picked %d channels from the data\n",evokedSet.info.nchan);
-    printf("Computing inverse...");
-    //
-    //   Simple matrix multiplication followed by combination of the
-    //   three current components
-    //
-    //   This does all the data transformations to compute the weights for the
-    //   eigenleads
-    //
-    SparseMatrix<double> reginv(inv.reginv.rows(),inv.reginv.rows());
-    // put this in the MNE algorithm class derived from inverse algorithm
-    //ToDo put this into a function of inv data
-    qint32 i;
-    for(i = 0; i < inv.reginv.rows(); ++i)
-        reginv.insert(i,i) = inv.reginv(i,0);
-
-    MatrixXd trans = reginv*inv.eigen_fields->data*inv.whitener*inv.proj*evokedSet.evoked[setno].data;
-    //
-    //   Transformation into current distributions by weighting the eigenleads
-    //   with the weights computed above
-    //
-    MatrixXd sol;
-    if (inv.eigen_leads_weighted)
-    {
-        //
-        //     R^0.5 has been already factored in
-        //
-        printf("(eigenleads already weighted)...");
-        sol = inv.eigen_leads->data*trans;
-    }
-    else
-    {
-        //
-        //     R^0.5 has to factored in
-        //
-       printf("(eigenleads need to be weighted)...");
-
-       SparseMatrix<double> sourceCov(inv.source_cov->data.rows(),inv.source_cov->data.rows());
-       for(i = 0; i < inv.source_cov->data.rows(); ++i)
-           sourceCov.insert(i,i) = sqrt(inv.source_cov->data(i,0));
-
-       sol   = sourceCov*inv.eigen_leads->data*trans;
-    }
-
-    if (inv.source_ori == FIFFV_MNE_FREE_ORI)
-    {
-        printf("combining the current components...");
-        MatrixXd sol1(sol.rows()/3,sol.cols());
-        for(i = 0; i < sol.cols(); ++i)
-        {
-            VectorXd* tmp = MNE::combine_xyz(sol.block(0,i,sol.rows(),1));
-            sol1.block(0,i,sol.rows()/3,1) = tmp->cwiseSqrt();
-            delete tmp;
-        }
-        sol.resize(sol1.rows(),sol1.cols());
-        sol = sol1;
-    }
-    if (dSPM)
-    {
-        printf("(dSPM)...");
-        sol = inv.noisenorm*sol;
-    }
-    else if (sLORETA)
-    {
-        printf("(sLORETA)...");
-        sol = inv.noisenorm*sol;
-    }
-    printf("[done]\n");
-
-    //Results
-    float tmin = ((float)evokedSet.evoked[setno].first) / evokedSet.info.sfreq;
-    float tstep = 1/evokedSet.info.sfreq;
-
-    std::cout << "\npart ( block( 0, 0, 10, 10) ) of the inverse solution:\n" << sol.block(0,0,10,10) << std::endl;
-
-    qDebug() << "Dimensionalities";
-    qDebug() << "sol" << sol.rows() << "x" << sol.cols();
-    qDebug() << "evoked" << evokedSet.evoked[setno].data.rows() << "x" << evokedSet.evoked[setno].data.cols();
-
-
-    printf("tmin = %f s\n", tmin);
-    printf("tstep = %f s\n", tstep);
-
-    qDebug() << "TODO: Output needs to be labeled, i.e. mapped to 0 and 1";
-
-//    m_features = evokedSet.evoked[setno].data.transpose();
-
-//    m_labels = sol.transpose();
+    MinimumNorm minimumNorm(inverse_operator, lambda2, method);
+    MNESourceEstimate sourceEstimate = minimumNorm.calculateInverse(evoked);
 
     printf("<<<<<<<<<<<<<<<<<<<<<<<<< Compute MNE Finished <<<<<<<<<<<<<<<<<<<<<<<<<\n");
+
+
+    //*********************************************************************************************************
+    // Store MNE Solution
+    //*********************************************************************************************************
+
+    std::cout << "\npart ( block( 0, 0, 10, 10) ) of the inverse solution:\n" << sourceEstimate.data.block(0,0,10,10) << std::endl;
+    printf("tmin = %f s\n", sourceEstimate.tmin);
+    printf("tstep = %f s\n", sourceEstimate.tstep);
+
+    if(!t_sFileNameStc.isEmpty())
+    {
+        QFile t_fileStc(t_sFileNameStc);
+        sourceEstimate.write(t_fileStc);
+
+        //test if everything was written correctly
+        MNESourceEstimate readSourceEstimate(t_fileStc);
+
+        std::cout << "\npart ( block( 0, 0, 10, 10) ) of the inverse solution:\n" << readSourceEstimate.data.block(0,0,10,10) << std::endl;
+        printf("tmin = %f s\n", readSourceEstimate.tmin);
+        printf("tstep = %f s\n", readSourceEstimate.tstep);
+    }
 }
