8033e2d4cd8b85097dcd3b5480a20acdde35c04e
SugandhaSachdeva <sugandha.sachdeva@tu-ilmenau.de>
2017-06-17 14:39:45 +0200

    Revert "Merge remote-tracking branch 'origin/interpolation' into interpolation"
    This reverts commit a043e114d6f1638ea1013188fe8db02223e43a72.

--- MNE/geometryInfo/geometryinfo.cpp
+++ MNE/geometryInfo/geometryinfo.cpp
@@ -87,21 +87,12 @@ using namespace MNELIB;
 
 QSharedPointer<MatrixXd> GeometryInfo::scdc(const MNEBemSurface &inSurface, const QVector<qint32> &vertSubset, double cancelDist)
 {
-    // create matrix and check for empty subset:
+    // create matrix:
     size_t cols;
-    QVector<qint32> finalSubset;
     if(!vertSubset.empty()) {
-        // caller has provided a valid subset
-        finalSubset = vertSubset;
         cols = vertSubset.size();
     }
     else {
-        // caller passed an empty subset, need to fill in all vertex IDs
-        qDebug() << "[WARNING] SCDC received empty subset, calculating full distance table, make sure you have enough memory !";
-        finalSubset.reserve(inSurface.rr.rows());
-        for(qint32 id = 0; id < inSurface.rr.rows(); ++id) {
-            finalSubset.push_back(id);
-        }
         cols = inSurface.rr.rows();
     }
     // convention: first dimension in distance table is "from", second dimension "to"
@@ -113,18 +104,18 @@ QSharedPointer<MatrixXd> GeometryInfo::scdc(const MNEBemSurface &inSurface, cons
         // assume that we have at least two available cores
         cores = 2;
     }
-    // start threads with their respective parts of the final subset
-    qint32 subArraySize = ceil(finalSubset.size() / cores);
+    // start threads with their respective parts of the subset
+    qint32 subArraySize = ceil(vertSubset.size() / cores);
     QVector<QFuture<void> > threads(cores - 1);
     qint32 begin = 0;
     qint32 end = subArraySize;
     for (int i = 0; i < threads.size(); ++i) {
-        threads[i] = QtConcurrent::run(std::bind(iterativeDijkstra, ptr, std::cref(inSurface), std::cref(finalSubset), begin, end, cancelDist));
+        threads[i] = QtConcurrent::run(std::bind(iterativeDijkstra, ptr, std::cref(inSurface), std::cref(vertSubset), begin, end, cancelDist));
         begin += subArraySize;
         end += subArraySize;
     }
-    // use main thread to calculate last part of the final subset
-    iterativeDijkstra(ptr, inSurface, finalSubset, begin, finalSubset.size(), cancelDist);
+    // use main thread to calculate last part of the vertSubset
+    iterativeDijkstra(ptr, inSurface, vertSubset, begin, vertSubset.size(), cancelDist);
 
     // wait for all other threads to finish
     bool finished = false;
