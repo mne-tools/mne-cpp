1df8d53ce235d6ffa2ca591967caf13b3a40c02f
sheinke <simon.j.w.heinke@gmail.com>
2017-06-19 19:57:19 +0400

    [SWP-49] Changed row sum test to use real data instead of randomly generated

--- testframes/test_interpolation/test_interpolation.cpp
+++ testframes/test_interpolation/test_interpolation.cpp
@@ -43,6 +43,7 @@
 #include <interpolation/interpolation.h>
 #include <mne/mne_bem.h>
 #include <mne/mne_bem_surface.h>
+#include <string>
 
 //*************************************************************************************************************
 //=============================================================================================================
@@ -152,64 +153,49 @@ void TestInterpolation::testDimensionsForInterpolation() {
 
 //*************************************************************************************************************
 
-void TestInterpolation::testSumOfRow()
-{
-    // scdc:
-    // generate small test mesh with 100 vertices:
-    MNEBemSurface testMesh;
-    // generate random vertex positions
-    MatrixX3f vertPos(100, 3);
-    for(qint8 i = 0; i < 100; i++) {
-        float x = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-        float y = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-        float z = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-
-        vertPos(i, 0) = x;
-        vertPos(i, 1) = y;
-        vertPos(i, 2) = z;
+void TestInterpolation::testSumOfRow() {
+    //acquire real data
+    QFile t_filesensorSurfaceVV("./MNE-sample-data/subjects/sample/bem/sample-head.fif");
+    MNEBem t_sensorSurfaceVV(t_filesensorSurfaceVV);
+    MNEBemSurface testSurface = t_sensorSurfaceVV[0];
+    QFile t_fileEvoked("./MNE-sample-data/MEG/sample/sample_audvis-ave.fif");
+    fiff_int_t setno = 0;
+    QPair<QVariant, QVariant> baseline(QVariant(), 0);
+    FiffEvoked evoked(t_fileEvoked, setno, baseline);
+    if(evoked.isEmpty()) {
+        return;
     }
-    testMesh.rr = vertPos;
-
-    // generate random adjacency, assume that every vertex has 4 neighbors
-    for (int i = 0; i < 100; ++i) {
-        QVector<int> neighborList;
-        for (int a = 0; a < 4; ++a) {
-            // this allows duplicates, probably is not a problem
-            neighborList.push_back(rand() % 100);
+    QVector<Vector3f> megSensors;
+    for( const FiffChInfo &info : evoked.info.chs) {
+        if(info.kind == FIFFV_MEG_CH) {
+            megSensors.push_back(info.chpos.r0);
         }
-        testMesh.neighbor_vert.push_back(neighborList);
     }
+    // projecting with MEG:
+    QSharedPointer<QVector<qint32>> mappedSubSet = GeometryInfo::projectSensors(testSurface, megSensors);
+    // SCDC with cancel distance 0.03:
+    QSharedPointer<MatrixXd> distanceMatrix = GeometryInfo::scdc(testSurface, *mappedSubSet, 0.03);
+    // weight matrix creation
+    Interpolation::createInterpolationMat(*mappedSubSet, distanceMatrix, Interpolation::linear, 0.03);
 
-    // generate random subset of test mesh of size subsetSize
-    int subsetSize = rand() % 100;
-    QVector<qint32> testSubset;
-    for (int b = 0; b <= subsetSize; b++) {
-        // this allows duplicates, probably is not a problem
-        testSubset.push_back(rand() % 100);
-    }
+    QSharedPointer<SparseMatrix<double> > w = Interpolation::getResult();
+    qint32 n = w->rows(), m = w->cols();
 
-    // create weight matrix from distance table
-    QSharedPointer<MatrixXd> distTable = GeometryInfo::scdc(testMesh, testSubset);
-    Interpolation::createInterpolationMat(testSubset, distTable, Interpolation::linear);
-    QSharedPointer<SparseMatrix<double>> testWeightMatrix = Interpolation::getResult();
-    //GeometryInfo::matrixDump(testWeightMatrix, "./matrixDump.txt");
+    const double LOWER_TRESH = 0.99999999;
+    const double UPPER_TRESH = 1.000000001;
+    qint32 overallSum = 0;
 
-    const size_t n = testWeightMatrix->rows();
-    const size_t m = testWeightMatrix->cols();
-
-    double sumCols = 0;
     for (int r = 0; r < n; ++r) {
-        double sumRow = 0;
-        VectorXd row = testWeightMatrix->row(r);
-        for (int q = 0; q < m; ++q) {
-            //std::cout << "Cell: " << r << " " << q << " " << row[q] << "\n";
-            sumRow += row[q];
+        double rowSum = 0.0;
+        for (int c = 0; c < m; ++c) {
+            rowSum += w->coeff(r, c);
         }
-        sumCols += sumRow;
-        //std::cout << "sum of Row: " << sumRow << "\n";
+        if (rowSum >= LOWER_TRESH && rowSum <= UPPER_TRESH) {
+            overallSum += 1;
+        }
     }
-    qDebug() << "Calculated sum of Columns: " << sumCols << " Colums of testWeightMatrix: " << n << "\n";
-    QVERIFY(double(sumCols) == double(n));
+
+    QVERIFY(overallSum == n);
 }
 
 //*************************************************************************************************************
