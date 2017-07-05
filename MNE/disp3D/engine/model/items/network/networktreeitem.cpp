d82a4d2cdaf55aed8006be2fe47f281ec724fe3f
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2017-07-05 10:19:35 +0200

    Fix connectivity thresholding

--- MNE/disp3D/engine/model/items/network/networktreeitem.cpp
+++ MNE/disp3D/engine/model/items/network/networktreeitem.cpp
@@ -228,7 +228,7 @@ void NetworkTreeItem::plotNetwork(const Network& tNetworkData, const QVector3D& 
     for(int i = 0; i < lNetworkNodes.size(); ++i) {
         //Plot in edges
         for(int j = 0; j < lNetworkNodes.at(i)->getEdgesIn().size(); ++j) {
-            if(abs(lNetworkNodes.at(i)->getEdgesIn().at(j)->getWeight()) >= vecThreshold.x()) {
+            if(fabs(lNetworkNodes.at(i)->getEdgesIn().at(j)->getWeight()) >= vecThreshold.x()) {
                 tMatLines.conservativeResize(count+1,2);
                 tMatLines(count,0) = lNetworkNodes.at(i)->getEdgesIn().at(j)->getStartNode()->getId();
                 tMatLines(count,1) = lNetworkNodes.at(i)->getEdgesIn().at(j)->getEndNode()->getId();
@@ -238,7 +238,7 @@ void NetworkTreeItem::plotNetwork(const Network& tNetworkData, const QVector3D& 
 
         //Plot out edges
         for(int j = 0; j < lNetworkNodes.at(i)->getEdgesOut().size(); ++j) {
-            if(abs(lNetworkNodes.at(i)->getEdgesOut().at(j)->getWeight()) >= vecThreshold.x()) {
+            if(fabs(lNetworkNodes.at(i)->getEdgesOut().at(j)->getWeight()) >= vecThreshold.x()) {
                 tMatLines.conservativeResize(count+1,2);
                 tMatLines(count,0) = lNetworkNodes.at(i)->getEdgesOut().at(j)->getStartNode()->getId();
                 tMatLines(count,1) = lNetworkNodes.at(i)->getEdgesOut().at(j)->getEndNode()->getId();
