ad5940e8d8f11fa7c2e35c773822ccb77c55b7ae
1DIce <lars.debor@tu-ilmenau.de>
2017-08-28 15:34:50 +0200

    Added ComputeFramegraph class

--- MNE/computeShader/computeframegraph.cpp
+++ MNE/computeShader/computeframegraph.cpp
@@ -4,11 +4,11 @@
 * @author   Lars Debor <lars.debor@tu-ilmenau.de>;
 *           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
 * @version  1.0
-* @date     Month, Year
+* @date     August, 2017
 *
 * @section  LICENSE
 *
-* Copyright (C) Year, Lars Debor and Matti Hamalainen. All rights reserved.
+* Copyright (C) 2017, Lars Debor and Matti Hamalainen. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 * the following conditions are met:
@@ -53,6 +53,17 @@
 // QT INCLUDES
 //=============================================================================================================
 
+#include <Qt3DCore/QNode>
+
+#include <Qt3DRender/QRenderSurfaceSelector>
+#include <Qt3DRender/QClearBuffers>
+#include <Qt3DRender/QNoDraw>
+#include <Qt3DRender/QDispatchCompute>
+#include <Qt3DRender/QTechniqueFilter>
+#include <Qt3DRender/QCameraSelector>
+#include <Qt3DRender/QMemoryBarrier>
+#include <Qt3DRender/QFilterKey>
+#include <Qt3DRender/QCamera>
 
 //*************************************************************************************************************
 //=============================================================================================================
@@ -66,6 +77,7 @@
 //=============================================================================================================
 
 using namespace CSH;
+using namespace Qt3DRender;
 
 
 //*************************************************************************************************************
@@ -79,8 +91,80 @@ using namespace CSH;
 // DEFINE MEMBER METHODS
 //=============================================================================================================
 
-ComputeFramegraph::ComputeFramegraph()
+ComputeFramegraph::ComputeFramegraph(Qt3DCore::QNode *parent)
+    : QViewport(parent)
+    , m_pSurfaceSelector(new QRenderSurfaceSelector(this))
+    , m_pClearBuffers(new QClearBuffers(m_pSurfaceSelector))
+    , m_pNoDraw(new QNoDraw(m_pClearBuffers))
+    , m_pDispatchCompute(new QDispatchCompute(m_pSurfaceSelector))
+    , m_pComputeFilter(new QTechniqueFilter(m_pDispatchCompute))
+    , m_pCameraSelector(new QCameraSelector(m_pSurfaceSelector))
+    , m_pDrawFilter(new QTechniqueFilter(m_pCameraSelector))
+    , m_pMemoryBarrier(new QMemoryBarrier(m_pDrawFilter))
+    , m_pDrawKey(new QFilterKey)
+    , m_pComputeKey(new QFilterKey)
+    //, m_pCamera(new QCamera)
 {
+    init();
+}
+
+void ComputeFramegraph::setCamera(QCamera *pCamera)
+{
+    m_pCameraSelector->setCamera(pCamera);
+}
+
+void ComputeFramegraph::setWorkGroupSize(const unsigned int x, const unsigned int y, const unsigned int z)
+{
+    m_pDispatchCompute->setWorkGroupX(x);
+    m_pDispatchCompute->setWorkGroupY(y);
+    m_pDispatchCompute->setWorkGroupZ(z);
+
+}
+
+//QCamera *ComputeFramegraph::getCamera()
+//{
+//    return m_pCamera.data();
+//}
+
+void ComputeFramegraph::init()
+{
+    this->setNormalizedRect(QRectF(0.0f, 0.0f, 1.0f, 1.0f));
+
+    //Set ClearBuffers
+    m_pClearBuffers->setBuffers(QClearBuffers::ColorDepthBuffer);
+    m_pClearBuffers->setClearColor(Qt::black);
+
+    //Set Workgroup size
+    //@TO-DO set correct workgroup size
+    m_pDispatchCompute->setWorkGroupX(50);
+    m_pDispatchCompute->setWorkGroupY(1);
+    m_pDispatchCompute->setWorkGroupZ(1);
+
+    //Set FilterKeys
+    m_pComputeKey->setName(QStringLiteral("type"));
+    m_pComputeKey->setValue(QStringLiteral("compute"));
+
+    m_pDrawKey->setName(QStringLiteral("type"));
+    m_pDrawKey->setValue(QStringLiteral("draw"));
+
+    //Add Matches
+    m_pComputeFilter->addMatch(m_pComputeKey);
+    m_pDrawFilter->addMatch(m_pDrawKey);
+
+
+    //Set Memory Barrier
+    //@TO-DO change this if needed
+    m_pMemoryBarrier->setWaitOperations(QMemoryBarrier::VertexAttributeArray);
+
+    //Set camera
+//    m_pCamera->setProjectionType(QCameraLens::PerspectiveProjection);
+//    m_pCamera->setViewCenter(QVector3D(0, 0, 0));
+//    m_pCamera->setPosition(QVector3D(0, 0, -800.0));
+//    m_pCamera->setNearPlane(0.1f);
+//    m_pCamera->setFarPlane(1000.0f);
+//    m_pCamera->setFieldOfView(25.0f);
+//    m_pCamera->setAspectRatio(1.33f);
+//    m_pCameraSelector->setCamera(m_pCamera);
 }
 
 
