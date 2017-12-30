1a153a0ad967088c74b550f30949887e5e002bc1
1Dice <lars.debor@tu-ilmenau.de>
2017-12-30 16:06:56 +0100

    [LIB-192] Fixed faster rotation speed of digitizers.

--- libraries/disp3D/engine/model/items/sensorspace/sensorpositiontreeitem.h
+++ libraries/disp3D/engine/model/items/sensorspace/sensorpositiontreeitem.h
@@ -8,7 +8,7 @@
 *
 * @section  LICENSE
 *
-* Copyright (C) 2017, Lroenz Esch and Matti Hamalainen. All rights reserved.
+* Copyright (C) 2017, Lorenz Esch and Matti Hamalainen. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 * the following conditions are met:
