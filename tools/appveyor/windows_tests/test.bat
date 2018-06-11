8dc2c8a2a19a730928e87eb62b8eafb98fbcd781
LorenzE <Lorenz.Esch@tu-ilmenau.de>
2018-06-11 12:59:20 -0400

    only include test_fiff_rwr

--- tools/appveyor/windows_tests/test.bat
+++ tools/appveyor/windows_tests/test.bat
@@ -2,7 +2,8 @@
 cd bin
 
 :: Array of tests to run
-set tests=test_fiff_rwr test_dipole_fit test_fiff_mne_types_io test_fiff_cov test_fiff_digitizer test_mne_msh_display_surface_set test_geometryinfo test_interpolation
+:: set tests=test_fiff_rwr test_dipole_fit test_fiff_mne_types_io test_fiff_cov test_fiff_digitizer test_mne_msh_display_surface_set test_geometryinfo test_interpolation
+set tests=test_fiff_rwr
 
 :: Run tests
 (for %%t in (%tests%) do ( 
