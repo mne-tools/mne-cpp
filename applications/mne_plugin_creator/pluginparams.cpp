61cb63d164526dc3948ede8259ead2e1b15da23b
Erik Hornberger <hornbergererik@gmail.com>
2018-09-10 09:20:20 +0900

    Organized variables more logically

--- applications/mne_plugin_creator/pluginparams.cpp
+++ applications/mne_plugin_creator/pluginparams.cpp
@@ -2,28 +2,42 @@
 
 
   PluginParams::PluginParams(QString name, QString nameSpace, QString author, QString email) {
-      m_name = name;
+
+      // Basic parameters
       m_email = email;
       m_author = author;
       m_namespace = nameSpace;
       m_targetName = baseFileName();
+
+      // Class names
+      m_name = name;
       m_widgetName= name + "Widget";
       m_aboutWidgetName = name + "AboutWidget";
       m_setupWidgetName = name + "SetupWidget";
+
+      // File names
       m_proFileName = baseFileName() + ".pro";
       m_jsonFileName = baseFileName() + ".json";
       m_globalsFileName = baseFileName() + "_global.h";
+
+      // Header files
       m_headerFileName = baseFileName() + ".h";
-      m_sourceFileName = baseFileName() + ".cpp";
       m_widgetHeaderFileName = baseFileName() + "widget.h";
-      m_widgetSourceFileName = baseFileName() + "widget.cpp";
-      m_widgetFormFileName = baseFileName() + "widget.ui";
       m_aboutWidgetHeaderFileName = baseFileName()  + "aboutwidget.h";
-      m_aboutWidgetSourceFileName = baseFileName() + "aboutwidget.cpp";
-      m_aboutWidgetFormFileName = baseFileName() + "aboutwidget.ui";
       m_setupWidgetHeaderFileName = baseFileName() + "setupwidget.h";
+
+      // Source files
+      m_sourceFileName = baseFileName() + ".cpp";
+      m_widgetSourceFileName = baseFileName() + "widget.cpp";
+      m_aboutWidgetSourceFileName = baseFileName() + "aboutwidget.cpp";
       m_setupWidgetSourceFileName = baseFileName() + "setupwidget.cpp";
+
+      // Form files
+      m_widgetFormFileName = baseFileName() + "widget.ui";
+      m_aboutWidgetFormFileName = baseFileName() + "aboutwidget.ui";
       m_setupWidgetFormFileName = baseFileName() + "setupwidget.ui";
+
+      // #define
       m_globalHeaderDefine = baseDefine() + "_GLOBAL_H";
       m_libraryDefine = baseDefine() + "_LIBRARY";
       m_exportDefine = baseDefine() + "SHARED_EXPORT";
