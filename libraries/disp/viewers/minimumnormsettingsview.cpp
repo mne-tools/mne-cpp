909e9ca7b2a3d2aea160a7fa3a07270952a4634d
Lorenz Esch <lorenz.esch@tu-ilmenau.de>
2018-09-13 14:34:26 -0400

    [SC-169] Add view for minimum norm settings

---
+++ libraries/disp/viewers/minimumnormsettingsview.cpp
@@ -0,0 +1,124 @@
+//=============================================================================================================
+/**
+* @file     minimumnormsettingsview.cpp
+* @author   Lorenz Esch <lesch@mgh.harvard.edu>;
+*           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
+* @version  1.0
+* @date     September, 2018
+*
+* @section  LICENSE
+*
+* Copyright (C) 2018, Lorenz Esch and Matti Hamalainen. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+* the following conditions are met:
+*     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+*       following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+*       the following disclaimer in the documentation and/or other materials provided with the distribution.
+*     * Neither the name of MNE-CPP authors nor the names of its contributors may be used
+*       to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+* PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*
+* @brief    Definition of the MinimumNormSettingsView Class.
+*
+*/
+
+//*************************************************************************************************************
+//=============================================================================================================
+// INCLUDES
+//=============================================================================================================
+
+#include "minimumnormsettingsview.h"
+
+#include "ui_minimumnormsettingsview.h"
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// Qt INCLUDES
+//=============================================================================================================
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// Eigen INCLUDES
+//=============================================================================================================
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// USED NAMESPACES
+//=============================================================================================================
+
+using namespace DISPLIB;
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// DEFINE MEMBER METHODS
+//=============================================================================================================
+
+MinimumNormSettingsView::MinimumNormSettingsView(QWidget *parent,
+                                                 Qt::WindowFlags f)
+: QWidget(parent, f)
+, ui(new Ui::MinimumNormSettingsViewWidget)
+{
+    ui->setupUi(this);
+
+    connect(ui->m_comboBox_method, static_cast<void (QComboBox::*)(const QString&)>(&QComboBox::currentTextChanged),
+            this, &MinimumNormSettingsView::onMethodChanged);
+
+    connect(ui->m_comboBox_triggerType, static_cast<void (QComboBox::*)(const QString&)>(&QComboBox::currentTextChanged),
+            this, &MinimumNormSettingsView::onTriggerTypeChanged);
+
+    this->setWindowTitle("MinimumNorm Settings");
+    this->setMinimumWidth(330);
+    this->setMaximumWidth(330);
+}
+
+
+//*************************************************************************************************************
+
+MinimumNormSettingsView::~MinimumNormSettingsView()
+{
+    delete ui;
+}
+
+
+//*************************************************************************************************************
+
+void MinimumNormSettingsView::setTriggerTypes(const QStringList& lTriggerTypes)
+{
+    for(int i = 0; i < lTriggerTypes.size(); ++i) {
+        if(ui->m_comboBox_triggerType->findText(lTriggerTypes.at(i)) == -1) {
+            ui->m_comboBox_triggerType->addItem(lTriggerTypes.at(i));
+        }
+    }
+}
+
+
+//*************************************************************************************************************
+
+void MinimumNormSettingsView::onMethodChanged(const QString& method)
+{
+    emit methodChanged(method);
+}
+
+
+
+//*************************************************************************************************************
+
+void MinimumNormSettingsView::onTriggerTypeChanged(const QString& triggerType)
+{
+    emit triggerTypeChanged(triggerType);
+}
---
+++ libraries/disp/viewers/minimumnormsettingsview.h
@@ -0,0 +1,170 @@
+//=============================================================================================================
+/**
+* @file     minimumnormsettingsview.h
+* @author   Lorenz Esch <lesch@mgh.harvard.edu>;
+*           Matti Hamalainen <msh@nmr.mgh.harvard.edu>
+* @version  1.0
+* @date     September, 2018
+*
+* @section  LICENSE
+*
+* Copyright (C) 2018, Lorenz Esch and Matti Hamalainen. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+* the following conditions are met:
+*     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+*       following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+*       the following disclaimer in the documentation and/or other materials provided with the distribution.
+*     * Neither the name of MNE-CPP authors nor the names of its contributors may be used
+*       to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+* PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*
+* @brief    Declaration of the MinimumNormSettingsView Class.
+*
+*/
+
+#ifndef MINIMUMNORMSETTINGSVIEW_H
+#define MINIMUMNORMSETTINGSVIEW_H
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// INCLUDES
+//=============================================================================================================
+
+#include "../disp_global.h"
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// QT INCLUDES
+//=============================================================================================================
+
+#include <QWidget>
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// Eigen INCLUDES
+//=============================================================================================================
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// FORWARD DECLARATIONS
+//=============================================================================================================
+
+namespace Ui {
+    class MinimumNormSettingsViewWidget;
+}
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// FORWARD DECLARATIONS
+//=============================================================================================================
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// DEFINE NAMESPACE DISPLIB
+//=============================================================================================================
+
+namespace DISPLIB
+{
+
+
+//*************************************************************************************************************
+//=============================================================================================================
+// DISPLIB FORWARD DECLARATIONS
+//=============================================================================================================
+
+
+//=============================================================================================================
+/**
+* DECLARE CLASS MinimumNormSettingsView
+*
+* @brief The MinimumNormSettingsView class provides a view to control settings for estiamting functional connectivity
+*/
+class DISPSHARED_EXPORT MinimumNormSettingsView : public QWidget
+{
+    Q_OBJECT
+
+public:    
+    typedef QSharedPointer<MinimumNormSettingsView> SPtr;              /**< Shared pointer type for MinimumNormSettingsView. */
+    typedef QSharedPointer<const MinimumNormSettingsView> ConstSPtr;   /**< Const shared pointer type for MinimumNormSettingsView. */
+
+    //=========================================================================================================
+    /**
+    * Constructs a MinimumNormSettingsView which is a child of parent.
+    *
+    * @param [in] parent        parent of widget.
+    */
+    MinimumNormSettingsView(QWidget *parent = 0,
+                Qt::WindowFlags f = Qt::Widget);
+
+    //=========================================================================================================
+    /**
+    * Destroys the MinimumNormSettingsView.
+    */
+    ~MinimumNormSettingsView();
+
+    //=========================================================================================================
+    /**
+    * Destroys the MinimumNormSettingsView.
+    *
+    * @param [in] lTriggerTypes        The new trigger types.
+    */
+    void setTriggerTypes(const QStringList& lTriggerTypes);
+
+protected:
+    //=========================================================================================================
+    /**
+    * Slot called when the method changed.
+    *
+    * @param [in] method        The new method.
+    */
+    void onMethodChanged(const QString& method);
+
+    //=========================================================================================================
+    /**
+    * Slot called when the trigger type changed.
+    *
+    * @param [in] triggerType        The new trigger type.
+    */
+    void onTriggerTypeChanged(const QString& triggerType);
+
+    Ui::MinimumNormSettingsViewWidget* ui;
+
+signals:
+    //=========================================================================================================
+    /**
+    * Emit signal whenever the connectivity metric changed.
+    *
+    * @param [in] metric        The new metric.
+    */
+    void methodChanged(const QString& metric);
+
+    //=========================================================================================================
+    /**
+    * Emit signal whenever the trigger type changed.
+    *
+    * @param [in] triggerType        The new trigger type.
+    */
+    void triggerTypeChanged(const QString& triggerType);
+
+};
+
+} // NAMESPACE
+
+#endif // MinimumNormSettingsView_H
