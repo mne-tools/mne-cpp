d1b4c41e3613a689959bc4c08eda25d403914c58
Lorenz Esch <lorenz.esch@tu-ilmenau.de>
2018-09-24 17:25:04 -0400

    [SC-169] Calculate spectogram in parallel and fix x axis time plotting in TFPlot

--- applications/mne_scan/plugins/neuronalconnectivity/neuronalconnectivity.cpp
+++ applications/mne_scan/plugins/neuronalconnectivity/neuronalconnectivity.cpp
@@ -518,6 +518,7 @@ void NeuronalConnectivity::run()
             //Send the data to the connected plugins and the online display
             if(!connectivityResult.isEmpty()) {
                 //qDebug()<<"NeuronalConnectivity::run - Total time"<<m_timer.elapsed();
+                //connectivityResult.setFrequencyBins(26,33);
                 m_pRTCEOutput->data()->setValue(connectivityResult);
             }
         }
