4f12d9b882e83de5748e163cb939e7deed0e2aaf
LorenzE <lorenzesch@hotmail.com>
2018-11-30 14:27:51 -0500

    [SC-170] Remove sceneUpdateRequested signal. Use update() instead

--- libraries/disp/viewers/helpers/averagescene.cpp
+++ libraries/disp/viewers/helpers/averagescene.cpp
@@ -109,9 +109,6 @@ void AverageScene::repaintItems(const QList<QGraphicsItem *> &selectedChannelIte
                                                                       selectionSceneItemTemp->m_iChannelUnit,
                                                                       m_colGlobalItemSignalColor);
 
-        connect(averageSceneItemTemp, &AverageSceneItem::sceneUpdateRequested,
-                    this, &AverageScene::updateScene);
-
         this->addItem(averageSceneItemTemp);
     }
 }
