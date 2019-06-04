59780de61c406fd4e20db7bc8f2515f565fd569c
imsorryk <wayne.isk@gmail.com>
2019-06-03 21:25:41 -0500

    linuxdeployqt added

--- tools/travis/linux_release/after_success.sh
+++ tools/travis/linux_release/after_success.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 #set -ev
-
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
     echo -e "Packaging binaries and libs"
 
-    # === Copy Libs ===
-	QT_LIB_DIR=/opt/qt510/lib
-    QT_LIBS=(libQt5Charts libQt5Concurrent libQt5Core libQt5Gui libQt5Network libQt5PrintSupport libQt5Qml libQt5Svg libQt5Test libQt5Widgets libQt5Xml libQt53DCore libQt53DExtras libQt53DInput libQt53DLogic libQt53DRender )
-    n_elements=${#QT_LIBS[@]}
-    for ((i = 0; i < n_elements; i++)); do
-        libpath="$QT_LIB_DIR/${QT_LIBS[i]}.*"
-        cp $libpath ./lib
-    done
+    # === Linux Deploy Qt ===
+	#sourcing qt510 again, this is for linuxdeployqt; verified redundant, so commented out. 
+	#source /opt/qt510/bin/qt510-env.sh
 
-    # === user package ===
-    archive_name="mne-cpp-linux-x86_64-$TRAVIS_BRANCH.tar.gz"
-    tar cfvz $archive_name ./bin ./lib
+	#setting linuxdeployqt to variable
+	linuxdeployqt=linuxdeployqt-continuous-x86_64.AppImage
+
+	#archive file name created
+	archive_name="mne-cpp-linux-x86_64-$TRAVIS_BRANCH.tar.gz"
+
+	#copying built data to folder for easy packaging 	
+	cp -r ./bin ./lib mne-cpp/
+
+	#dropping into folder to easily package all results from linuxdeployqt
+	cd mne-cpp
+
+	#linuxdeployqt uses mne_scan binary to resolve dependencies in current directory. 
+	../$linuxdeployqt bin/mne_scan
+
+	#creating archive of everything in current directory
+	tar cfvz ../$archive_name ./*
+	
+	#Moving up one directory level.
+	cd .. 
 
     #Master
     if [[ $TRAVIS_BRANCH == 'master' ]]; then
