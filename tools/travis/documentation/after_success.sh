4e76e831f4c4bd9a0ed70583db83719007c46422
LorenzE <lorenzesch@hotmail.com>
2019-07-31 19:22:49 -0400

    Fix after_success.sh, add PR guards and delete obsolete files

--- tools/travis/documentation/after_success.sh
+++ tools/travis/documentation/after_success.sh
@@ -5,9 +5,9 @@
 cd doc
 doxygen mne-cpp_doxyfile
 
-#if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
     git clone -b gh-pages --single-branch --no-checkout --depth 1 https://github.com/mne-tools/mne-cpp mne_cpp.gh-pages
-	touch build/mne_cpp.github.io/.nojekyll
+	touch mne_cpp.gh-pages/.nojekyll
 
 	cp -r html/* mne_cpp.gh-pages
 	cd mne_cpp.gh-pages
@@ -15,4 +15,4 @@ doxygen mne-cpp_doxyfile
 	git add .nojekyll
 	git commit -a -m 'Build docu'
 	git push https://${GIT_TOKEN}@github.com/mne-tools/mne-cpp.git --all
-#fi
+fi
