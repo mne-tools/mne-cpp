f6f928ec922a376093892aa6360e081e95e36c7f
LorenzE <lorenz.esch@tu-ilmenau.de>
2019-08-01 16:21:55 -0700

    Update some gerneal docu info and produce files with shorter names

--- tools/travis/documentation/after_success.sh
+++ tools/travis/documentation/after_success.sh
@@ -6,13 +6,20 @@ cd doc
 doxygen mne-cpp_doxyfile
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-    git clone -b gh-pages --single-branch --no-checkout --depth 1 https://github.com/mne-tools/mne-cpp mne_cpp.gh-pages
-	touch mne_cpp.gh-pages/.nojekyll
+    	git clone -b gh-pages --single-branch --no-checkout --depth 1 https://github.com/mne-tools/mne-cpp mne_cpp.gh-pages
+	cd mne_cpp.gh-pages
+	git rm *
+	git commit -a -m 'Delete all old docu files'
 
+	touch .nojekyll
+	
+	cd ..
 	cp -r html/* mne_cpp.gh-pages
+	cp -r qt-creator_doc/mne-cpp.qch mne_cpp.gh-pages
+
 	cd mne_cpp.gh-pages
 	git add *
 	git add .nojekyll
-	git commit -a -m 'Build docu'
+	git commit -a -m 'Add updated docu'
 	git push https://${GIT_TOKEN}@github.com/mne-tools/mne-cpp.git --all
 fi
