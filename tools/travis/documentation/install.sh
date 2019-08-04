5c2ae8d735ca077d68eba943c139b35eabaf042c
LorenzE <lorenz.esch@tu-ilmenau.de>
2019-08-03 21:03:38 -0700

    Use git lfs to upload .qch file which larger than 100mb

--- tools/travis/documentation/install.sh
+++ tools/travis/documentation/install.sh
@@ -3,3 +3,7 @@
 # Install Doxygen
 sudo apt-get install -qq qttools5-dev-tools doxygen graphviz
 
+# Instal lgit lfs
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs install
