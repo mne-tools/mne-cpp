f80c9222b2f02f56ac9954a4b046d49b281ab3f3
LorenzE <lorenz.esch@tu-ilmenau.de>
2019-08-03 21:03:38 -0700

    Use git lfs to upload .qch file which larger than 100mb

--- tools/travis/documentation/after_success.sh
+++ tools/travis/documentation/after_success.sh
@@ -6,18 +6,22 @@ cd doc
 doxygen mne-cpp_doxyfile
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-    	git clone -b gh-pages --single-branch --no-checkout --depth 1 https://github.com/mne-tools/mne-cpp mne_cpp.gh-pages
-	cd mne_cpp.gh-pages
-	git rm *
+    	git clone -b gh-pages --single-branch --no-checkout --depth 1 https://github.com/mne-tools/mne-cpp gh-pages
+	cd gh-pages
+	git rm * -r
 	git commit -a -m 'Delete all old docu files'
 
 	touch .nojekyll
 	
 	cd ..
-	cp -r html/* mne_cpp.gh-pages
-	cp -r qt-creator_doc/mne-cpp.qch mne_cpp.gh-pages
+	cp -r html/* gh-pages
+	cp -r qt-creator_doc/mne-cpp.qch gh-pages
 
-	cd mne_cpp.gh-pages
+	cd gh-pages
+
+	# Use git lfs for files over 100mb
+	git lfs track '*.qch'
+
 	git add *
 	git add .nojekyll
 	git commit -a -m 'Add updated docu'
