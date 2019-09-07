f957f3736a46e21589f7e4b46ff61d54b95de157
juangpc <juangpc@gmail.com>
2019-09-07 14:40:28 -0500

    settings controller multiple files and concurrency

--- applications/mne_anonymize/settingscontroller.cpp
+++ applications/mne_anonymize/settingscontroller.cpp
@@ -82,134 +82,285 @@ using namespace FIFFANONYMIZER;
 // DEFINE MEMBER METHODS
 //=============================================================================================================
 
-SettingsController::SettingsController()
-{
+//SettingsController::SettingsController()
+//{
 
-}
+//}
 
 SettingsController::SettingsController(QCoreApplication * qtApp)
+    :m_bMultipleInFiles(false),
+     m_pQCoreApp(qtApp)
 {
 
-    FIFFANONYMIZER::FiffAnonymizer app;
+    m_pQCoreApp->setApplicationName(m_anonymizer.name);
+    m_pQCoreApp->setApplicationVersion(m_anonymizer.versionStr);
 
-    QCoreApplication::setApplicationName(app.name);
-    QCoreApplication::setApplicationName(app.versionStr);
+    initParser();
+    parseInputs();
 
-    QCommandLineParser parser;
-
-    parser.setApplicationDescription(app.description);
-    parser.addHelpOption();
-    parser.addVersionOption();
-
-    parser.addPositionalArgument("in-file",QCoreApplication::translate("main","File to anonymize <infile>"),"infile");
-
-    QCommandLineOption outFileOpt(QStringList() << "o" << "output_file",
-                                  QCoreApplication::translate("main","Output file <outfile>."),
-                                  QCoreApplication::translate("main","outfile"),
-                                  QCoreApplication::applicationDirPath() + "/MNE-sample-data/MEG/sample/testing_output.fif");
-    parser.addOption(outFileOpt);
-
-    QCommandLineOption verboseOpt(QStringList() << "v" << "verbose",
-                                  QCoreApplication::translate("main","Verbose mode. Prints out more information."));
-    parser.addOption(verboseOpt);
-
-    QCommandLineOption measDateOpt("set_measurement_date",
-                                   QCoreApplication::translate("main","Specify the measurement date. Format: YYYMMDD "),
-                                   QCoreApplication::translate("main","days"));
-    parser.addOption(measDateOpt);
-
-    QCommandLineOption measDateOffsetOpt("set_measurement_date_offset",
-                                   QCoreApplication::translate("main","Specify number of days to subtract to the measurement date."),
-                                   QCoreApplication::translate("main","date"));
-    parser.addOption(measDateOffsetOpt);
-
-    QCommandLineOption birthdayOpt("set_subject_birthday",
-                                   QCoreApplication::translate("main","Specify the subject's birthday. Format: YYYMMDD "),
-                                   QCoreApplication::translate("main","date"));
-    parser.addOption(birthdayOpt);
-
-    QCommandLineOption birthdayOffsetOpt("set_subject_birthday_offset",
-                                   QCoreApplication::translate("main","Specify number of days to subtract to the subject's birthday."),
-                                   QCoreApplication::translate("main","days"));
-    parser.addOption(birthdayOffsetOpt);
-
-    QCommandLineOption deleteInFileOpt(QStringList() << "d" << "delete_input_file_after",
-                                       QCoreApplication::translate("main","Delete input fiff file after anonymization."));
-    parser.addOption(deleteInFileOpt);
-
-    QCommandLineOption deleteInFileConfirmOpt("avoid_delete_confirmation",
-                                              QCoreApplication::translate("main","Avoid confirming the deletion of the input fiff file."));
-    parser.addOption(deleteInFileConfirmOpt);
-
-    QCommandLineOption bruteOpt(
-                "brute",QCoreApplication::translate("main","Anonymize weight, height XXX if present in the input fiff file."));
-    parser.addOption(bruteOpt);
-
-    QCommandLineOption quietOpt("quiet",QCoreApplication::translate("main","Show no output."));
-    parser.addOption(quietOpt);
-
-    parser.process(*qtApp);
-
-    const QStringList args = parser.positionalArguments();
-    QString fileInName(args.at(0));
-    QFile fileIn(fileInName);
-    app.setFileIn(&fileIn);
-
-    QString fileOutName;
-    if(parser.isSet(outFileOpt))
-    {
-        fileOutName = parser.value(outFileOpt);
-    } else {
-        QFileInfo fInfo(fileIn.fileName());
-        fileOutName = QDir(fInfo.absolutePath()).filePath(
-                    fInfo.baseName() + "_anonymized." + fInfo.completeSuffix());
-    }
-    QFile fileOut(fileOutName);
-    app.setFileOut(&fileOut);
-
-    if(parser.isSet(verboseOpt))
-    {
-        app.setVerboseMode(true);
-    }
-
-    if(parser.isSet(bruteOpt))
-    {
-        app.setBruteMode(true);
-    }
-
-    if(parser.isSet(quietOpt))
-    {
-        if(app.getVerboseMode())
-        {
-            app.setVerboseMode(false);
-        }
-        app.setQuietMode(true);
-    }
-
-    if(parser.isSet(measDateOffsetOpt))
-    {
-        QString d(parser.value(measDateOffsetOpt));
-        app.setMeasurementDayOffset(d.toInt());
-    }
-
-    if(parser.isSet(birthdayOffsetOpt))
-    {
-        QString d(parser.value(birthdayOffsetOpt));
-        app.setSubjectBirthdayOffset(d.toInt());
-    }
-
-    if(parser.isSet(measDateOpt))
-    {
-        app.setMeasurementDay(parser.value(measDateOpt));
-    }
-
-    if(parser.isSet(birthdayOpt))
-    {
-        app.setSubjectBirthday(parser.value(birthdayOpt));
-    }
-
-    app.anonymizeFile();
+    generate_anonymizer_instances();
+    execute();
 
 }
 
 //*************************************************************************************************************
+
+void SettingsController::initParser()
+{
+
+    m_parser.setApplicationDescription(m_anonymizer.description);
+    m_parser.addHelpOption();
+    m_parser.addVersionOption();
+
+    QCommandLineOption verboseOpt(QStringList() << "v" << "verbose",
+                                  QCoreApplication::translate("main","Prints out more information."));
+    m_parser.addOption(verboseOpt);
+
+    QCommandLineOption quietOpt("quiet",QCoreApplication::translate("main","Show no output."));
+    m_parser.addOption(quietOpt);
+
+    QCommandLineOption deleteInFileOpt("delete_input_file_after",
+                                       QCoreApplication::translate("main","Delete input fiff file after anonymization."));
+    m_parser.addOption(deleteInFileOpt);
+
+    QCommandLineOption deleteInFileConfirmOpt("avoid_delete_confirmation",
+                                              QCoreApplication::translate("main","Avoid confirming the deletion of the input fiff file."));
+    m_parser.addOption(deleteInFileConfirmOpt);
+
+    QCommandLineOption bruteOpt("brute",
+                                QCoreApplication::translate("main","Anonymize weight, height XXX if present in the input fiff file."));
+    m_parser.addOption(bruteOpt);
+
+    QCommandLineOption inFileOpt("in",QCoreApplication::translate("main","File to anonymize. Wildcards are allowed and seveal \"-in <infile>\" statements can be present."),
+                                 QCoreApplication::translate("main","infile"));
+    m_parser.addOption(inFileOpt);
+
+    QCommandLineOption outFileOpt("out",QCoreApplication::translate("main","Output file <outfile>. Only allowed when anonymizing one single file."),
+                                  QCoreApplication::translate("main","outfile"));
+    m_parser.addOption(outFileOpt);
+
+
+    QCommandLineOption measDateOpt("measurement_date",
+                                   QCoreApplication::translate("main","Specify the measurement date. Only when anonymizing a single file. Format: YYYMMDD "),
+                                   QCoreApplication::translate("main","days"));
+    m_parser.addOption(measDateOpt);
+
+    QCommandLineOption measDateOffsetOpt("measurement_date_offset",
+                                         QCoreApplication::translate("main","Specify number of days to subtract to the measurement <date>. Only allowed when anonymizing a single file."),
+                                         QCoreApplication::translate("main","date"));
+    m_parser.addOption(measDateOffsetOpt);
+
+    QCommandLineOption birthdayOpt("subject_birthday",
+                                   QCoreApplication::translate("main","Specify the subject's birthday <date>. Only allowed when anonymizing a single file. Format: YYYMMDD "),
+                                   QCoreApplication::translate("main","date"));
+    m_parser.addOption(birthdayOpt);
+
+    QCommandLineOption birthdayOffsetOpt("subject_birthday_offset",
+                                         QCoreApplication::translate("main","Specify number of <days> to subtract to the subject's birthday. Only allowed when anonymizing a single file. "),
+                                         QCoreApplication::translate("main","days"));
+    m_parser.addOption(birthdayOffsetOpt);
+
+}
+
+
+//*************************************************************************************************************
+
+void SettingsController::parseInputs()
+{
+    m_parser.process(*m_pQCoreApp);
+
+    parseInputAndOutputFiles();
+
+    if(m_parser.isSet("v") || m_parser.isSet("verbose"))
+    {
+        m_anonymizer.setVerboseMode(true);
+    }
+
+    if(m_parser.isSet("brute"))
+    {
+        m_anonymizer.setBruteMode(true);
+    }
+
+    if(m_parser.isSet("quiet"))
+    {
+        if(m_anonymizer.getVerboseMode())
+        {
+            m_anonymizer.setVerboseMode(false);
+        }
+        m_anonymizer.setQuietMode(true);
+    }
+
+    if(m_parser.isSet("delete_input_file_after"))
+    {
+        m_anonymizer.setDeleteInputFileAfter(true);
+    }
+
+    if(m_parser.isSet("avoid_delete_confirmation"))
+    {
+        m_anonymizer.setDeleteInputFileAfterConfirmation(false);
+    }
+
+    if(m_parser.isSet("measurement_date"))
+    {
+        if(m_bMultipleInFiles)
+        {
+            qDebug() << "Error. Multiple Input files. You cannot specify the option \"measurement_date\".";
+            m_parser.showHelp();
+        }
+        QString d(m_parser.value("measurement_date"));
+        m_anonymizer.setMeasurementDay(d);
+    }
+
+    if(m_parser.isSet("measurement_date_offset"))
+    {
+        if(m_bMultipleInFiles)
+        {
+            qDebug() << "Error. Multiple Input files. You cannot specify the option \"measurement_date_offset\".";
+            m_parser.showHelp();
+        }
+        QString doffset(m_parser.value("measurement_date_offset"));
+        m_anonymizer.setMeasurementDayOffset(doffset.toInt());
+    }
+
+    if(m_parser.isSet("subject_birthday"))
+    {
+        if(m_bMultipleInFiles)
+        {
+            qDebug() << "Error. Multiple Input files. You cannot specify the option \"subject_birthday\".";
+            m_parser.showHelp();
+        }
+        QString birthday(m_parser.value("subject_birthday"));
+        m_anonymizer.setSubjectBirthday(birthday);
+    }
+
+    if(m_parser.isSet("subject_birthday_offset"))
+    {
+        if(m_bMultipleInFiles)
+        {
+            qDebug() << "Error. Multiple Input files. You cannot specify the option \"subject_birthday_offset\".";
+            m_parser.showHelp();
+        }
+        QString bdoffset(m_parser.value("subject_birthday_offset"));
+        m_anonymizer.setSubjectBirthdayOffset(bdoffset.toInt());
+    }
+}
+
+
+//*************************************************************************************************************
+
+void SettingsController::parseInputAndOutputFiles()
+{
+    if(m_parser.isSet("in"))
+    {
+        QStringList inFilesAux(m_parser.values("in"));
+        for(QString f: inFilesAux)
+        {
+            QDir d;
+            d.setNameFilters({f});
+            for(QFileInfo fi: d.entryInfoList())
+            {
+                if(fi.isFile() && fi.isReadable())
+                {
+                    fi.makeAbsolute();
+                    m_slInFiles.append(fi.fileName());
+                }
+            }
+        }
+    }
+
+    if(m_slInFiles.size() == 0)
+    {
+        qDebug() << "Error. no valid input files. OMG!!";
+        m_parser.showHelp();
+    } else if(m_slInFiles.size() == 1)
+    {
+        m_bMultipleInFiles = false;
+    } else {
+        m_bMultipleInFiles = true;
+    }
+
+    if(m_bMultipleInFiles)
+    {
+        //        QStringList opts;
+        //        opts << "out" << "measurement_date" << "measurement_date_offset" << "subject_birthday" << "subject_birthday_offset";
+        //        for(QString opi:opts)
+        //        {
+        //            if(m_parser.isSet(opi))
+        //            {
+        //                qDebug() << "Error. Multiple Input files. You cannot specify the option " << opi;
+        //                m_parser.showHelp();
+        //            }
+        //        }
+
+        for(QString fi:m_slInFiles)
+        {
+            QFileInfo fInfo(fi);
+            QString fout = QDir(fInfo.absolutePath()).filePath(
+                        fInfo.baseName() + "_anonymized." + fInfo.completeSuffix());
+            m_slOutFiles.append(fout);
+        }
+    } else {
+        QString fileOutName;
+        if(m_parser.isSet("out"))
+        {
+            fileOutName = m_parser.value("out");
+        } else {
+            QFileInfo fInfo(m_slInFiles.at(0));
+            fileOutName = QDir(fInfo.absolutePath()).filePath(
+                        fInfo.baseName() + "_anonymized." + fInfo.completeSuffix());
+        }
+    }
+
+    if(m_slInFiles.size() != m_slOutFiles.size())
+    {
+        qDebug() << "Error. something went wrong while parsing the input files.";
+    }
+}
+
+
+//*************************************************************************************************************
+
+void SettingsController::generate_anonymizer_instances()
+{
+    if(m_bMultipleInFiles)
+    {
+        for(int i=0; i< m_slInFiles.size(); ++i)
+        {
+            //generate copy of m_anonymizer --> need a copy constructor
+            QSharedPointer<FIFFANONYMIZER::FiffAnonymizer> pAnonymizerApp;
+
+            m_appList.append(pAnonymizerApp);
+        }
+    } else {
+        QFile fileIn(m_slInFiles.at(0));
+        QFile fileOut(m_slOutFiles.at(0));
+        m_anonymizer.setFileIn(&fileIn);
+        m_anonymizer.setFileOut(&fileOut);
+    }
+}
+
+
+//*************************************************************************************************************
+
+void SettingsController::execute()
+{
+
+    if(m_bMultipleInFiles)
+    {
+        QtConcurrent::map(m_appList,&SettingsController::signal_execution_start);
+    } else {
+        m_anonymizer.anonymizeFile();
+    }
+}
+
+
+//*************************************************************************************************************
+
+void SettingsController::signal_execution_start(QSharedPointer<FIFFANONYMIZER::FiffAnonymizer> app)
+{
+    app->anonymizeFile();
+}
+
+
+//*************************************************************************************************************
+
