76b665ff8de5ddfe319908744397a15d2a01e2b5
LorenzE <lorenzesch@hotmail.com>
2019-09-22 16:58:45 -0400

    Implement initial structure for the test. Remove noApplications since we now need the mne_anonymize cmd line to anonymize the files first.

--- tools/travis/linux_tests/script.sh
+++ tools/travis/linux_tests/script.sh
@@ -5,7 +5,7 @@
 cppcheck --enable=all -f -q -i./include/3rdParty/eigen3 -i/opt/qt510/include ./MNE ./applications ./testframes ./examples
 
 # Configure with dynamic code analysis
-qmake -r MNECPP_CONFIG+=withCodeCov MNECPP_CONFIG+=noExamples MNECPP_CONFIG+=noApplications
+qmake -r MNECPP_CONFIG+=withCodeCov MNECPP_CONFIG+=noExamples
 
 # Build
 make -j2
