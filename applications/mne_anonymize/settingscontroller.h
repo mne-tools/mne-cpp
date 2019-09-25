0c48be4a24ae990de068643cfe890353d7709f3e
jgpc <juangpc@gmail.com>
2019-09-25 00:20:08 -0500

    [unfinished] Doxygen documentation added to FiffAnonymizer class

--- applications/mne_anonymize/settingscontroller.h
+++ applications/mne_anonymize/settingscontroller.h
@@ -13,8 +13,8 @@
 *
 * Copyright (C) 2019, Juan Garcia-Prieto and Matti Hamalainen. All rights reserved.
 *
-* Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 * the following conditions are met:
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 *     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
 *       following disclaimer.
 *     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
