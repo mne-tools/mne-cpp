5be1fa6c5d49b7fee88e9d4379125784589e0d86
LorenzE <lorenzesch@hotmail.com>
2019-10-04 17:13:14 -0400

    Add output file name to bem download in win ci

--- tools/appveyor/windows_tests/before_test.bat
+++ tools/appveyor/windows_tests/before_test.bat
@@ -4,9 +4,9 @@ cd bin
 :: Clone MNE-CPP test data
 call git clone https://github.com/mne-tools/mne-cpp-test-data.git mne-cpp-test-data
 
-curl https://www.dropbox.com/s/464j97jbaef7q3n/sample-5120-5120-5120-bem-sol.fif?dl=1 -L
+curl https://www.dropbox.com/s/464j97jbaef7q3n/sample-5120-5120-5120-bem-sol.fif?dl=1 -L -o sample-5120-5120-5120-bem-sol.fif
 
-curl https://www.dropbox.com/s/tkrl3p1kifbzjo1/sample-5120-5120-5120-bem.fif?dl=1 -L
+curl https://www.dropbox.com/s/tkrl3p1kifbzjo1/sample-5120-5120-5120-bem.fif?dl=1 -L -o sample-5120-5120-5120-bem.fif
 
 :: Go back to mne-cpp root
 cd ..
