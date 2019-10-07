e40a4fe64537bc4294e5f4f5c16228520f18dced
LorenzE <lorenzesch@hotmail.com>
2019-10-07 13:30:03 -0400

    Replace with 1280-1280-1280 bem. rename test.

--- tools/travis/linux_tests/after_success.sh
+++ tools/travis/linux_tests/after_success.sh
@@ -5,16 +5,11 @@ set -e
 # Clone MNE-CPP test data
 git clone https://github.com/mne-tools/mne-cpp-test-data.git mne-cpp-test-data
 
-# Temporary fix to run test_forward_solution on the large sample-5120-5120-5120-bem-sol.fif file.
-wget https://www.dropbox.com/s/464j97jbaef7q3n/sample-5120-5120-5120-bem-sol.fif?dl=1 -O sample-5120-5120-5120-bem-sol.fif 
-
-wget https://www.dropbox.com/s/tkrl3p1kifbzjo1/sample-5120-5120-5120-bem.fif?dl=1 -O sample-5120-5120-5120-bem.fif 
-
 # Set Environment variable
 MNECPP_ROOT=$(pwd)
 
 # Tests to run - TODO: find required tests automatically with grep
-tests=( test_codecov test_fiff_rwr test_dipole_fit test_fiff_mne_types_io test_fiff_cov test_fiff_digitizer test_mne_msh_display_surface_set test_geometryinfo test_interpolation test_spectral_connectivity test_forward_solution)
+tests=( test_codecov test_fiff_rwr test_dipole_fit test_fiff_mne_types_io test_fiff_cov test_fiff_digitizer test_mne_msh_display_surface_set test_geometryinfo test_interpolation test_spectral_connectivity test_mne_forward_solution)
 
 for test in ${tests[*]};
 do
