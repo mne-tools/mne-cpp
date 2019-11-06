a448a42492926cb7e56c163df9b46a79ff361b94
LorenzE <lorenzesch@hotmail.com>
2019-11-06 17:10:23 -0500

    Exclude some tests (temporary)

--- tools/travis/linux_tests/after_success.sh
+++ tools/travis/linux_tests/after_success.sh
@@ -9,7 +9,9 @@ git clone https://github.com/mne-tools/mne-cpp-test-data.git mne-cpp-test-data
 MNECPP_ROOT=$(pwd)
 
 # Tests to run - TODO: find required tests automatically with grep
-tests=( test_codecov test_fiff_rwr test_dipole_fit test_fiff_mne_types_io test_fiff_cov test_fiff_digitizer test_mne_msh_display_surface_set test_geometryinfo test_interpolation test_spectral_connectivity test_mne_forward_solution)
+tests=( test_dipole_fit test_fiff_rwr test_spectral_connectivity test_mne_forward_solution)
+
+#tests=( test_codecov test_fiff_rwr test_dipole_fit test_fiff_mne_types_io test_fiff_cov test_fiff_digitizer test_mne_msh_display_surface_set test_geometryinfo test_interpolation test_spectral_connectivity test_mne_forward_solution)
 
 for test in ${tests[*]};
 do
