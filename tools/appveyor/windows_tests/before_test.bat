a0b00e301f37d3143b3e42e928b6eba8f546a65e
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2019-11-26 13:53:14 -0500

    undid windows script change

--- tools/appveyor/windows_tests/before_test.bat
+++ tools/appveyor/windows_tests/before_test.bat
@@ -2,7 +2,7 @@
 cd bin
 
 :: Clone MNE-CPP test data
-call git clone https://github.com/mne-tools/mne-cpp-test-data.git ./bin/mne-cpp-test-data
+call git clone https://github.com/mne-tools/mne-cpp-test-data.git mne-cpp-test-data
 
 :: Go back to mne-cpp root
 cd ..
