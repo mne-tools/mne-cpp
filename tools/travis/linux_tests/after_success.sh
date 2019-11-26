bbb97456f6937c4fa7a7495dc9f8a4c8c7f0ff26
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2019-11-26 16:35:00 -0500

    removed test list

--- tools/travis/linux_tests/after_success.sh
+++ tools/travis/linux_tests/after_success.sh
@@ -5,16 +5,16 @@ set -e
 # Clone MNE-CPP test data
 git clone https://github.com/mne-tools/mne-cpp-test-data.git ./bin/mne-cpp-test-data
 
-# Set Environment variable
-MNECPP_ROOT=$(pwd)
+# # Set Environment variable
+# MNECPP_ROOT=$(pwd)
 
 # Tests to run - TODO: find required tests automatically with grep
-tests=( test_fiff_rwr test_dipole_fit test_fiff_mne_types_io test_fiff_cov test_fiff_digitizer test_mne_msh_display_surface_set test_geometryinfo test_interpolation test_spectral_connectivity test_mne_forward_solution)
+# tests=( test_fiff_rwr test_dipole_fit test_fiff_mne_types_io test_fiff_cov test_fiff_digitizer test_mne_msh_display_surface_set test_geometryinfo test_interpolation test_spectral_connectivity test_mne_forward_solution)
 
-for test in ${tests[*]};
+for test in ./bin/test_*;
 do
     echo ">> Starting $test"
-	./bin/$test
+	./$test
 
 	# Find all .cpp files, cd to their folder and run gcov
 	find ./libraries -type f -name "*.cpp" -execdir gcov {} \; > /dev/null
