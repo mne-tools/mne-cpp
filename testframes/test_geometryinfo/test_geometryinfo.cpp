51bdfa44a587c751b00384374af5fb8dd92fa72c
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2019-11-26 11:58:29 -0500

    Changed file paths in test_geometryinfo.cpp to applicationDirPath

--- testframes/test_geometryinfo/test_geometryinfo.cpp
+++ testframes/test_geometryinfo/test_geometryinfo.cpp
@@ -98,7 +98,7 @@ TestGeometryInfo::TestGeometryInfo() {
 //*************************************************************************************************************
 void TestGeometryInfo::initTestCase() {
     //acquire real surface data
-    QFile t_filesensorSurfaceVV(QDir::currentPath()+"/mne-cpp-test-data/subjects/sample/bem/sample-5120-bem.fif");
+    QFile t_filesensorSurfaceVV(QCoreApplication::applicationDirPath()+"/MNE-sample-data/subjects/sample/bem/sample-5120-bem.fif");
     MNEBem t_sensorSurfaceVV(t_filesensorSurfaceVV);
     realSurface = t_sensorSurfaceVV[0];
 
@@ -138,7 +138,7 @@ void TestGeometryInfo::initTestCase() {
 
 void TestGeometryInfo::testBadChannelFiltering() {
     //acquire real sensor positions
-    QFile t_fileEvoked(QDir::currentPath()+"/mne-cpp-test-data/MEG/sample/sample_audvis-ave.fif");
+    QFile t_fileEvoked(QCoreApplication::applicationDirPath()+"/MNE-sample-data/MEG/sample/sample_audvis-ave.fif");
     fiff_int_t setno = 0;
     QPair<QVariant, QVariant> baseline(QVariant(), 0);
     FiffEvoked evoked(t_fileEvoked, setno, baseline);
