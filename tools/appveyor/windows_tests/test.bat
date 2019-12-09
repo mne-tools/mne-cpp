2598f9528cbd21950ffcdbd4c39837b90a064c29
doerfelruben@aol.com <doerfelruben@aol.com>
2019-12-09 18:04:28 -0500

    rename test in test.bat

--- tools/appveyor/windows_tests/test.bat
+++ tools/appveyor/windows_tests/test.bat
@@ -2,7 +2,7 @@
 cd bin
 
 :: Array of tests to run
-set tests=test_fiff_rwr test_dipole_fit test_fiff_mne_types_io test_fiff_cov test_fiff_digitizer test_mne_msh_display_surface_set test_geometryinfo test_interpolation test_spectral_connectivity test_mne_forward_solution test_rtfilter_filterdata
+set tests=test_fiff_rwr test_dipole_fit test_fiff_mne_types_io test_fiff_cov test_fiff_digitizer test_mne_msh_display_surface_set test_geometryinfo test_interpolation test_spectral_connectivity test_mne_forward_solution test_filtering
 
 :: Run tests
 (for %%t in (%tests%) do ( 
