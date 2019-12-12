df62cc831cf718505f6c6f24a724b91279e7679a
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2019-12-12 16:12:03 -0500

    added includes and switched out deprecated functions

--- libraries/disp/viewers/spectrumview.cpp
+++ libraries/disp/viewers/spectrumview.cpp
@@ -54,6 +54,7 @@
 #include <QGridLayout>
 #include <QHeaderView>
 #include <QTableView>
+#include <QMouseEvent>
 
 
 //*************************************************************************************************************
