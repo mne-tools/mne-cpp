906fb49bd5a40b1737dbba5e399b7f7921726deb
RDoerfel <rdorfel@mgh.harvard.edu>
2020-02-06 10:46:38 -0500

    apply coding conventions

--- libraries/utils/generics/applicationlogger.h
+++ libraries/utils/generics/applicationlogger.h
@@ -1,19 +1,77 @@
+//=============================================================================================================
+/**
+ * @file     applicationlogger.h
+ * @author   Mainak Jas <mjas@mgh.harvard.edu>;
+ *           Ruben Dörfel <rdorfel@mgh.harvard.edu>
+ * @version  dev
+ * @date     July, 2019
+ *
+ * @section  LICENSE
+ *
+ * Copyright (C) 2013, Mainak Jas, Ruben Dörfel. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ * the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *       following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *       the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *     * Neither the name of MNE-CPP authors nor the names of its contributors may be used
+ *       to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * @brief    Contains the declaration of the applicationlogger class.
+ *
+ */
+
 #ifndef APPLICATIONLOGGER_H
 #define APPLICATIONLOGGER_H
 
+//*************************************************************************************************************
+//=============================================================================================================
+// INCLUDES
+//=============================================================================================================
+
 #include "../utils_global.h"
+
+//*************************************************************************************************************
+//=============================================================================================================
+// Qt INCLUDES
+//=============================================================================================================
 
 #include <QSharedPointer>
 
 class QMutex;
 
+//*************************************************************************************************************
+//=============================================================================================================
+// DEFINE NAMESPACE UTILSLIB
+//=============================================================================================================
+
 namespace UTILSLIB
 {
+//=============================================================================================================
+/**
+ * Color command line outputs
+ *
+ * @brief Apllicationlogger
+ */
 class UTILSSHARED_EXPORT ApplicationLogger
 {
-    public:
-        ApplicationLogger();
-        static void myCustomLogWriter(QtMsgType type, const QMessageLogContext &context, const QString &msg);
+public:
+
+    ApplicationLogger();
+
+    static void myCustomLogWriter(QtMsgType type, const QMessageLogContext &context, const QString &msg);
 
 };
 }
