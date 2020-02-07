eb0740b480f1687df1ed8fb74e41e652eadbdda9
Andrey Parfenov <a1994ndrey@gmail.com>
2020-02-08 00:58:37 +0300

    temp
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- applications/mne_scan/plugins/brainflowboard/FormFiles/brainflowstreamingwidget.cpp
+++ applications/mne_scan/plugins/brainflowboard/FormFiles/brainflowstreamingwidget.cpp
@@ -7,60 +7,12 @@ BrainFlowStreamingWidget::BrainFlowStreamingWidget(BrainFlowBoard *board, QWidge
     ui(new Ui::BrainFlowStreamingWidget)
 {
     ui->setupUi(this);
-
-    // notch filter
-    ui->notchFilter->addItem("None");
-    ui->notchFilter->addItem("50");
-    ui->notchFilter->addItem("60");
-    ui->notchFilter->setCurrentIndex(1);
-    // bandpass
-    ui->bandPassFilter->addItem("None");
-    ui->bandPassFilter->addItem("1-50");
-    ui->bandPassFilter->addItem("1-30");
-    ui->bandPassFilter->addItem("1-4");
-    ui->bandPassFilter->addItem("4-8");
-    ui->bandPassFilter->addItem("8-13");
-    ui->bandPassFilter->addItem("13-30");
-    ui->bandPassFilter->setCurrentIndex(2);
-    // filter type
-    ui->filterType->addItem("BUTTERWORTH");
-    ui->filterType->addItem("CHEBYSHEV_TYPE_1");
-    ui->filterType->addItem("BESSEL");
-    ui->filterType->setCurrentIndex(0);
-    // filter order
-    ui->filterOrder->setRange(1, 7);
-    ui->filterOrder->setValue(4);
-    // ripple
-    ui->ripple->setText("0.5");
-
     connect(ui->sendToBoard, &QPushButton::clicked, this, &BrainFlowStreamingWidget::configureBoard);
-    connect(ui->submitFilters, &QPushButton::clicked, this, &BrainFlowStreamingWidget::applyFilters);
 }
 
 void BrainFlowStreamingWidget::configureBoard()
 {
     m_pBrainFlowBoard->configureBoard(ui->stringToSend->text().toStdString());
-}
-
-void BrainFlowStreamingWidget::applyFilters()
-{
-    int notchFreq = 0;
-    int bandStart = 0;
-    int bandStop = 0;
-    int filterType = ui->filterType->currentIndex();
-    int order = ui->filterOrder->text().toInt();
-    double ripple = ui->ripple->text().toDouble();
-    if (ui->notchFilter->currentIndex() != 0)
-    {
-        notchFreq = ui->notchFilter->currentText().toInt();
-    }
-    if (ui->bandPassFilter->currentIndex() != 0)
-    {
-        QStringList splitted = ui->bandPassFilter->currentText().split("-");
-        bandStart = splitted[0].toInt();
-        bandStop = splitted[1].toInt();
-    }
-    m_pBrainFlowBoard->applyFilters(notchFreq, bandStart, bandStop, filterType, order, ripple);
 }
 
 BrainFlowStreamingWidget::~BrainFlowStreamingWidget()
--- applications/mne_scan/plugins/brainflowboard/brainflowboard.h
+++ applications/mne_scan/plugins/brainflowboard/brainflowboard.h
@@ -4,7 +4,14 @@
 #include "brainflowboard_global.h"
 
 #include <scShared/Interfaces/ISensor.h>
-#include <scMeas/realtimesamplearray.h>
+
+namespace SCMEASLIB {
+    class RealTimeMultiSampleArray;
+}
+
+namespace FIFFLIB {
+    class FiffInfo;
+}
 
 #include "board_shim.h"
 
@@ -34,7 +41,6 @@ public:
     void releaseSession(bool useQmessage = true);
     void prepareSession(BrainFlowInputParams params, std::string streamerParams, int boardId, int dataType, int vertScale);
     void configureBoard(std::string config);
-    void applyFilters(int notchFreq, int bandStart, int bandStop, int filterType, int filterOrder, double ripple);
     void showSettings();
 
 protected:
@@ -48,13 +54,8 @@ private:
     int m_iNumChannels;
     int *m_pChannels;
     int m_iSamplingRate;
-    PluginOutputData<RealTimeSampleArray>::SPtr *m_pOutput;
-    volatile int m_iNotchFreq;
-    volatile int m_iBandStart;
-    volatile int m_iBandStop;
-    volatile int m_iFilterOrder;
-    volatile int m_iFilterType;
-    volatile double m_dRipple;
+    QSharedPointer<FIFFLIB::FiffInfo> m_pFiffInfo;
+    SCSHAREDLIB::PluginOutputData<SCMEASLIB::RealTimeMultiSampleArray>::SPtr m_pOutput;
     volatile bool m_bIsRunning;
 
     QAction *m_pShowSettingsAction;
