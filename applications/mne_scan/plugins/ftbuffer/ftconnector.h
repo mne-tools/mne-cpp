5cdf58f13dd3de98e1d5ecf60ef85ac629eb9777
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2020-02-25 17:57:49 -0500

    read header chunk works. hacky solution, need to fix libraries and change fiftag.next

--- applications/mne_scan/plugins/ftbuffer/ftconnector.h
+++ applications/mne_scan/plugins/ftbuffer/ftconnector.h
@@ -262,6 +262,11 @@ public:
      */
     void resetEmitData();
 
+    //=========================================================================================================
+    /**
+     * @brief parseNeuromagHeader parses the extended header chunk with neuromag data and gets the relevant fiff info
+     * @return returns the FiffInfo from the parsed fif file from the neuromag header chunk
+     */
     FIFFLIB::FiffInfo parseNeuromagHeader();
 
 private:
@@ -350,25 +355,25 @@ private:
     int totalBuffSamples();
 
 
-    int                 m_iNumSamples;                          /**< Number of samples we've read from the buffer */
-    int                 m_iNumNewSamples;                       /**< Number of total samples (read and unread) in the buffer */
-    int                 m_iMsgSamples;                          /**< Number of samples in the latest buffer transmission receied */
-    int                 m_iNumChannels;                         /**< Number of channels in the buffer data */
-    int                 m_iDataType;                            /**< Type of data in the buffer */
-    int                 m_iNeuromagHeader;                      /**< Size of neuromag header chunk */
-    int                 m_iPort             = 1972;             /**< Port where the ft bufferis found */
+    int                                     m_iNumSamples;                          /**< Number of samples we've read from the buffer */
+    int                                     m_iNumNewSamples;                       /**< Number of total samples (read and unread) in the buffer */
+    int                                     m_iMsgSamples;                          /**< Number of samples in the latest buffer transmission receied */
+    int                                     m_iNumChannels;                         /**< Number of channels in the buffer data */
+    int                                     m_iDataType;                            /**< Type of data in the buffer */
+    int                                     m_iNeuromagHeader;                      /**< Size of neuromag header chunk */
+    int                                     m_iPort             = 1972;             /**< Port where the ft bufferis found */
 
-    bool                m_bNewData;                             /**< Indicate whether we've received new data */
+    bool                                    m_bNewData;                             /**< Indicate whether we've received new data */
 
-    float               m_fSampleFreq;                          /**< Sampling frequency of data in the buffer */
+    float                                   m_fSampleFreq;                          /**< Sampling frequency of data in the buffer */
 
-    QString             m_sAddress          = "127.0.0.1";      /**< Address where the ft buffer is found */
+    QString                                 m_sAddress          = "127.0.0.1";      /**< Address where the ft buffer is found */
 
-    QTcpSocket*         m_pSocket;                              /**< Socket that manages the connection to the ft buffer */
+    QTcpSocket*                             m_pSocket;                              /**< Socket that manages the connection to the ft buffer */
 
-    Eigen::MatrixXd*    m_pMatEmit;                             /**< Container to format data to tansmit to FtBuffProducer */
+    Eigen::MatrixXd*                        m_pMatEmit;                             /**< Container to format data to tansmit to FtBuffProducer */
 
-    QSharedPointer<FIFFLIB::FiffRawData> m_pNeuromagData;
+    QSharedPointer<FIFFLIB::FiffRawData>    m_pNeuromagData;                        /**< Storage for Fiff data for parsing neuromag header chink and extracting relevant fiff info */
 };
 
 }//namespace
