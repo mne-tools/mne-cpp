33c2f32b92da5750b38bb8f492a075da64552229
LorenzE <lorenzesch@hotmail.com>
2020-03-03 16:07:48 -0500

    ENH: Refactor thread handling in the FiffSimulator plugin

--- applications/mne_scan/plugins/fiffsimulator/fiffsimulatorproducer.cpp
+++ applications/mne_scan/plugins/fiffsimulator/fiffsimulatorproducer.cpp
@@ -61,11 +61,9 @@ using namespace FIFFLIB;
 
 FiffSimulatorProducer::FiffSimulatorProducer(FiffSimulator* p_pFiffSimulator)
 : m_pFiffSimulator(p_pFiffSimulator)
-, m_pRtDataClient(0)
 , m_bDataClientIsConnected(false)
 , m_iDataClientId(-1)
 , m_bFlagInfoRequest(false)
-, m_bFlagMeasuring(false)
 {
 }
 
@@ -82,32 +80,25 @@ FiffSimulatorProducer::~FiffSimulatorProducer()
 
 void FiffSimulatorProducer::connectDataClient(QString p_sRtSeverIP)
 {
-    if(m_pRtDataClient.isNull())
+    if(m_pRtDataClient.isNull()) {
         m_pRtDataClient = QSharedPointer<RtDataClient>(new RtDataClient);
-    else if(m_bDataClientIsConnected)
+    } else if(m_bDataClientIsConnected) {
         return;
+    }
 
     m_pRtDataClient->connectToHost(p_sRtSeverIP);
     m_pRtDataClient->waitForConnected(1000);
 
-    if(m_pRtDataClient->state() == QTcpSocket::ConnectedState)
-    {
+    if(m_pRtDataClient->state() == QTcpSocket::ConnectedState) {
         producerMutex.lock();
-        if(!m_bDataClientIsConnected)
-        {
-            //
+        if(!m_bDataClientIsConnected) {
             // get client ID
-            //
             m_iDataClientId = m_pRtDataClient->getClientId();
 
-            //
             // set data client alias -> for convinience (optional)
-            //
             m_pRtDataClient->setClientAlias(m_pFiffSimulator->m_sFiffSimulatorClientAlias); // used in option 2 later on
 
-            //
             // set new state
-            //
             m_bDataClientIsConnected = true;
             emit dataConnectionChanged(m_bDataClientIsConnected);
         }
@@ -119,8 +110,7 @@ void FiffSimulatorProducer::connectDataClient(QString p_sRtSeverIP)
 
 void FiffSimulatorProducer::disconnectDataClient()
 {
-    if(m_bDataClientIsConnected)
-    {
+    if(m_bDataClientIsConnected) {
         m_pRtDataClient->disconnectFromHost();
         if (m_pRtDataClient->state() != QAbstractSocket::UnconnectedState) {
             m_pRtDataClient->waitForDisconnected();
@@ -132,21 +122,21 @@ void FiffSimulatorProducer::disconnectDataClient()
         producerMutex.unlock();
         emit dataConnectionChanged(m_bDataClientIsConnected);
     }
+    qDebug() << "[FiffSimulatorProducer::disconnectDataClient] m_bDataClientIsConnected" << m_bDataClientIsConnected;
+}
+
+//=============================================================================================================
+
+void FiffSimulatorProducer::start()
+{
+
+    QThread::start();
 }
 
 //=============================================================================================================
 
 void FiffSimulatorProducer::stop()
 {
-    //Wait until this thread (Producer) is stopped
-    m_bFlagMeasuring = false;
-
-    if(m_pFiffSimulator->m_bCmdClientIsConnected) //ToDo replace this with is running
-    {
-        // Stop Measurement at rt_Server
-        (*m_pFiffSimulator->m_pRtCmdClient)["stop-all"].send();
-    }
-
     requestInterruption();
     wait();
 }
@@ -159,18 +149,13 @@ void FiffSimulatorProducer::run()
     this->connectDataClient(m_pFiffSimulator->m_sFiffSimulatorIP);
 
     qint32 count = 0;
-    while(m_pRtDataClient->state() != QTcpSocket::ConnectedState)
-    {
+    while(m_pRtDataClient->state() != QTcpSocket::ConnectedState) {
         msleep(100);
         this->connectDataClient(m_pFiffSimulator->m_sFiffSimulatorIP);
         ++count;
-        if(count > 10 || !m_pFiffSimulator->isRunning())
+        if(count > 10)
             return;
     }
-
-    msleep(1000);
-
-    m_bFlagMeasuring = true;
 
     // Inits
     MatrixXf t_matRawBuffer;
@@ -192,18 +177,22 @@ void FiffSimulatorProducer::run()
         }
         producerMutex.unlock();
 
-        if(m_bFlagMeasuring) {
-            m_pRtDataClient->readRawBuffer(m_pFiffSimulator->m_pFiffInfo->nchan, t_matRawBuffer, kind);
+        if(m_pFiffSimulator->m_pFiffInfo) {
+            m_pRtDataClient->readRawBuffer(m_pFiffSimulator->m_pFiffInfo->nchan,
+                                           t_matRawBuffer,
+                                           kind);
 
             if(kind == FIFF_DATA_BUFFER) {
                 to += t_matRawBuffer.cols();
                 from += t_matRawBuffer.cols();
-                emit dataReceived(t_matRawBuffer);
+                if(!isInterruptionRequested()) {
+                    emit dataReceived(t_matRawBuffer);
+                }
             } else if(FIFF_DATA_BUFFER == FIFF_BLOCK_END) {
-                m_bFlagMeasuring = false;
+                break;
             }
         }
     }
 
-    this->disconnectDataClient();
+    disconnectDataClient();
 }
