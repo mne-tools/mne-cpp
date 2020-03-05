19e9a493bb39757eea4f9285af775ac52330497f
LorenzE <lorenzesch@hotmail.com>
2020-03-05 12:02:49 -0500

    ENH: Refactor Covariance plugin

--- applications/mne_scan/plugins/covariance/FormFiles/covariancesettingswidget.cpp
+++ applications/mne_scan/plugins/covariance/FormFiles/covariancesettingswidget.cpp
@@ -1,6 +1,47 @@
+//=============================================================================================================
+/**
+ * @file     covariancesettingswidget.cpp
+ * @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
+ *           Lorenz Esch <lesch@mgh.harvard.edu>
+ * @version  dev
+ * @date     February, 2013
+ *
+ * @section  LICENSE
+ *
+ * Copyright (C) 2013, Christoph Dinh, Lorenz Esch. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ * the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *       following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *       the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *     * Neither the name of MNE-CPP authors nor the names of its contributors may be used
+ *       to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * @brief    Definition of the CovarianceSettingsWidget class.
+ *
+ */
+
+//=============================================================================================================
+// INCLUDES
+//=============================================================================================================
+
 #include "covariancesettingswidget.h"
 
-#include "../covariance.h"
+//=============================================================================================================
+// QT INCLUDES
+//=============================================================================================================
 
 #include <QGridLayout>
 #include <QSpinBox>
@@ -12,9 +53,12 @@
 
 using namespace COVARIANCEPLUGIN;
 
-CovarianceSettingsWidget::CovarianceSettingsWidget(Covariance *toolbox, QWidget *parent)
+//=============================================================================================================
+// DEFINE MEMBER METHODS
+//=============================================================================================================
+
+CovarianceSettingsWidget::CovarianceSettingsWidget(QWidget *parent)
 : QWidget(parent)
-, m_pCovarianceToolbox(toolbox)
 {
     this->setWindowTitle("Covariance Settings");
     this->setMinimumWidth(330);
@@ -22,22 +66,34 @@ CovarianceSettingsWidget::CovarianceSettingsWidget(Covariance *toolbox, QWidget 
 
     QGridLayout* t_pGridLayout = new QGridLayout;
 
-//    if(m_pCovarianceToolbox->m_pFiffInfo)
-//    {
     QLabel* t_pLabelNumSamples = new QLabel;
     t_pLabelNumSamples->setText("Number of Samples");
     t_pGridLayout->addWidget(t_pLabelNumSamples,0,0,1,1);
 
-    qint32 minSamples = 600;//(qint32)m_pCovarianceToolbox->m_pFiffInfo->sfreq;
+    qint32 minSamples = 600;
 
     m_pSpinBoxNumSamples = new QSpinBox;
     m_pSpinBoxNumSamples->setMinimum(minSamples);
     m_pSpinBoxNumSamples->setMaximum(minSamples*60);
     m_pSpinBoxNumSamples->setSingleStep(minSamples);
-    m_pSpinBoxNumSamples->setValue(toolbox->m_iEstimationSamples);
     connect(m_pSpinBoxNumSamples, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
-            m_pCovarianceToolbox, &Covariance::changeSamples);
+            this, &CovarianceSettingsWidget::samplesChanged);
     t_pGridLayout->addWidget(m_pSpinBoxNumSamples,0,1,1,1);
-//    }
     this->setLayout(t_pGridLayout);
+}
+
+//=============================================================================================================
+
+void CovarianceSettingsWidget::setCurrentSamples(int iSamples)
+{
+    m_pSpinBoxNumSamples->setValue(iSamples);
+}
+
+//=============================================================================================================
+
+void CovarianceSettingsWidget::setMinSamples(int iSamples)
+{
+    m_pSpinBoxNumSamples->setMinimum(iSamples);
+    m_pSpinBoxNumSamples->setMaximum(iSamples*60);
+
 }
--- applications/mne_scan/plugins/covariance/FormFiles/covariancesettingswidget.h
+++ applications/mne_scan/plugins/covariance/FormFiles/covariancesettingswidget.h
@@ -1,5 +1,44 @@
+//=============================================================================================================
+/**
+ * @file     covariancesettingswidget.h
+ * @author   Christoph Dinh <chdinh@nmr.mgh.harvard.edu>;
+ *           Lorenz Esch <lesch@mgh.harvard.edu>
+ * @version  dev
+ * @date     February, 2013
+ *
+ * @section  LICENSE
+ *
+ * Copyright (C) 2013, Christoph Dinh, Lorenz Esch. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ * the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *       following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *       the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *     * Neither the name of MNE-CPP authors nor the names of its contributors may be used
+ *       to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * @brief    Contains the declaration of the CovarianceSettingsWidget class.
+ *
+ */
+
 #ifndef COVARIANCESETTINGSWIDGET_H
 #define COVARIANCESETTINGSWIDGET_H
+
+//=============================================================================================================
+// INCLUDES
+//=============================================================================================================
 
 //=============================================================================================================
 // QT INCLUDES
@@ -20,33 +59,39 @@ namespace COVARIANCEPLUGIN
 {
 
 //=============================================================================================================
-// USED NAMESPACES
+// COVARIANCEPLUGIN FORWARD DECLARATIONS
 //=============================================================================================================
-
-//=============================================================================================================
-// FORWARD DECLARATIONS
-//=============================================================================================================
-
-class Covariance;
 
 class CovarianceSettingsWidget : public QWidget
 {
     Q_OBJECT
 
-    friend class Covariance;
-
 public:
     typedef QSharedPointer<CovarianceSettingsWidget> SPtr;         /**< Shared pointer type for CovarianceAdjustmentWidget. */
     typedef QSharedPointer<CovarianceSettingsWidget> ConstSPtr;    /**< Const shared pointer type for CovarianceAdjustmentWidget. */
 
-    explicit CovarianceSettingsWidget(Covariance *toolbox, QWidget *parent = 0);
+    explicit CovarianceSettingsWidget(QWidget *parent = 0);
+
+    //=========================================================================================================
+    /**
+     * Set current samples to gather until a new covariance is calculated.
+     *
+     * @param[in] iSamples     new number samples
+     */
+    virtual void setCurrentSamples(int iSamples);
+
+    //=========================================================================================================
+    /**
+     * Set minimum number of samples to gather until a new covariance is calculated.
+     *
+     * @param[in] iSamples     new minimum number of samples
+     */
+    virtual void setMinSamples(int iSamples);
 
 signals:
-
-public slots:
+    void samplesChanged(int iSamples);
 
 private:
-    Covariance* m_pCovarianceToolbox;
     QSpinBox* m_pSpinBoxNumSamples;
 };
 } // NAMESPACE
