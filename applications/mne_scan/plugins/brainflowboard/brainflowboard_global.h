c546791bdd8c07ad7498dfd03ef10fc4cca3dbea
LorenzE <lorenzesch@hotmail.com>
2020-03-10 18:47:25 -0400

    ENH: Rename brainflowboard library config flag

--- applications/mne_scan/plugins/brainflowboard/brainflowboard_global.h
+++ applications/mne_scan/plugins/brainflowboard/brainflowboard_global.h
@@ -45,7 +45,7 @@
 // PREPROCESSOR DEFINES
 //=============================================================================================================
 
-#if defined(BRAINFLOWBOARD_LIBRARY)
+#if defined(BRAINFLOWBOARD_PLUGIN)
 #  define BRAINFLOWBOARD_EXPORT Q_DECL_EXPORT   /**< Q_DECL_EXPORT must be added to the declarations of symbols used when compiling a shared library. */
 #else
 #  define BRAINFLOWBOARD_EXPORT Q_DECL_IMPORT   /**< Q_DECL_IMPORT must be added to the declarations of symbols used when compiling a client that uses the shared library. */
