e7c8db200461841d534174fd1b8b07d2a0d21cc7
LorenzE <lorenzesch@hotmail.com>
2020-03-12 14:47:50 -0400

    ENH: Work on hpi plugin's GUI control

--- libraries/disp/viewers/hpisettingsview.cpp
+++ libraries/disp/viewers/hpisettingsview.cpp
@@ -3,11 +3,11 @@
  * @file     hpisettingsview.cpp
  * @author   Lorenz Esch <lesch@mgh.harvard.edu>
  * @version  dev
- * @date     March, 2017
+ * @date     March, 2020
  *
  * @section  LICENSE
  *
- * Copyright (C) 2017, Lorenz Esch. All rights reserved.
+ * Copyright (C) 2020, Lorenz Esch. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  * the following conditions are met:
