5307415f180f09b2a9ab2c75fa30eba77db4aea6
RDoerfel <doerfelruben@aol.com>
2020-04-23 14:48:30 +0200

    ENH,MAINT: Add new member variables to be consistend with MNEForwardSolution; Problem: Transformation old to new structs

--- libraries/fwd/computeFwd/compute_fwd.h
+++ libraries/fwd/computeFwd/compute_fwd.h
@@ -49,9 +49,12 @@
 #include <mne/c/mne_ctf_comp_data_set.h>
 #include "../fwd_eeg_sphere_model_set.h"
 #include "../fwd_bem_model.h"
+
 #include <mne/c/mne_named_matrix.h>
 #include <mne/c/mne_nearest.h>
 #include <mne/c/mne_source_space_old.h>
+#include <mne/mne_sourcespace.h>
+#include <mne/mne_forwardsolution.h>
 
 #include <fiff/c/fiff_sparse_matrix.h>
 
@@ -136,10 +139,28 @@ public:
     QString qPath;
     QFile file;
 
+public:
+    //=========================================================================================================
+    FIFFLIB::FiffInfoBase info;                 /**< light weighted measurement info */
+    FIFFLIB::fiff_int_t source_ori;             /**< Source orientation: fixed or free */
+    bool surf_ori;                              /**< If surface oriented */
+    FIFFLIB::fiff_int_t coord_frame;            /**< Coil coordinate system definition */
+    FIFFLIB::fiff_int_t nsource;                /**< Number of source dipoles */
+    FIFFLIB::fiff_int_t nchan;                  /**< Number of channels */
+    FIFFLIB::FiffNamedMatrix::SDPtr sol;        /**< Forward solution */
+    FIFFLIB::FiffNamedMatrix::SDPtr sol_grad;   /**< ToDo... */
+    FIFFLIB::FiffCoordTrans mri_head_t;         /**< MRI head coordinate transformation */
+    MNELIB::MNESourceSpace src;                 /**< Geometric description of the source spaces (hemispheres) */
+    Eigen::MatrixX3f source_rr;                 /**< Source locations */
+    Eigen::MatrixX3f source_nn;                 /**< Source normals (number depends on fixed or free orientation) */
+
+    MNELIB::MNEForwardSolution mneFwdSol;        /**< The class contatining all the solutions */
+
 private:
     //=========================================================================================================
     /**
      * init the member variables
+     * ToDo: refactor so that most of the old member variables can be deleted and we only need the protected ones (consistend with MneForwardSolution)
      */
     void initFwd();
 
@@ -183,7 +204,7 @@ private:
      * @param [out] id                  The FiffID
      *
      */
-    int readChannels(QSharedPointer<FIFFLIB::FiffInfo> pFiffInfo,
+    int readChannels(FIFFLIB::FiffInfoBase infoBase,
                      QList<FIFFLIB::FiffChInfo>& listMegCh,	 /* MEG channels */
                      int& iNMeg,
                      QList<FIFFLIB::FiffChInfo>& listMegComp,
@@ -192,7 +213,6 @@ private:
                      int& iNEeg,
                      FIFFLIB::FiffCoordTransOld** transDevHeadOld,
                      FIFFLIB::FiffId& id);	         /* The measurement ID */
-
 };
 
 //=============================================================================================================
