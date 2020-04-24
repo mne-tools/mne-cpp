b7139c4e3e974de33f9dd8c9f1957a1451ac29d5
jgpc <juangpc@gmail.com>
2020-04-24 10:44:04 -0500

    create window obj

---
+++ applications/mne_anonymize/mainwindow.h
@@ -0,0 +1,17 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QWidget>
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // MAINWINDOW_H
