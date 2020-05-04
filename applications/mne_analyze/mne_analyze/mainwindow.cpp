d8c9294756979025e4f6575d2f645c7633cf15a3
LorenzE <lorenzesch@hotmail.com>
2020-05-04 11:29:51 -0400

    MAINT: Change version flag in mne scan and analyze

--- applications/mne_analyze/mne_analyze/mainwindow.cpp
+++ applications/mne_analyze/mne_analyze/mainwindow.cpp
@@ -384,7 +384,12 @@ void MainWindow::about()
         m_textEdit_aboutText->setOverwriteMode(true);
         m_textEdit_aboutText->setTextInteractionFlags(Qt::LinksAccessibleByKeyboard|Qt::LinksAccessibleByMouse|Qt::TextBrowserInteraction|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
 
-        gridLayout->addWidget(m_textEdit_aboutText, 1, 0, 1, 1);
+        QLabel* pLabel = new QLabel();
+        pLabel->setText(QString("Version: ") + CInfo::AppVersion());
+
+        gridLayout->addWidget(pLabel, 1, 0, 1, 1);
+
+        gridLayout->addWidget(m_textEdit_aboutText, 2, 0, 1, 1);
 
         m_pAboutWindow->setWindowTitle(tr("About"));
         m_label_splashcreen->setText(QString());
