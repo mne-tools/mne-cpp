ad00179709d7a321fd68c07a2828226c7f7434b4
jgpc <juangpc@gmail.com>
2020-05-05 22:19:12 -0500

    add sex and handedness to string helpers

--- applications/mne_anonymize/fiffanonymizer.cpp
+++ applications/mne_anonymize/fiffanonymizer.cpp
@@ -480,9 +480,31 @@ void FiffAnonymizer::censorTag() const
     default:
     {   }
     }
+}
+
+inline QString FiffAnonymizer::subjectSexToString(const int sexCode) const
+{
+    static QStringList subjectSexRefList = { "unknown" , "male" , "female" };
+    if (sexCode > -1 && sexCode < subjectSexRefList.size())
+    {
+        return subjectSexRefList.at(sexCode);
+    } else {
+        qCritical() << "Invalid subject sex code. [0 = unknown, 1 = male, 2 = female]. The code of the subject is: " << QString::number(sexCode);
+        return QString("invalid-code");
     }
 }
 
+inline QString FiffAnonymizer::subjectHandToString(const int handCode) const
+{
+    static QStringList subjectHandRefList = { "unknown", "right", "left", "ambidextrous" };
+    if ((handCode > -1) && (handCode < subjectHandRefList.size()))
+    {
+        return subjectHandRefList.at(handCode);
+    } else {
+        qCritical() << "Invalid subject handedness code. [0 = unknown, 1 = right, 2 = left, 3 = ambidextrous]. The code of the subject is: " << QString::number(handCode);
+        return QString("invalid-code");
+    }
+}
 //=============================================================================================================
 
 void FiffAnonymizer::readTag()
