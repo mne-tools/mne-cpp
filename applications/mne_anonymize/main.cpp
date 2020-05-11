686bf706f76262b2c4b14e1732c5a6986ebf32c7
jgpc <juangpc@gmail.com>
2020-05-11 15:59:59 -0500

    @ver to @since

--- applications/mne_anonymize/main.cpp
+++ applications/mne_anonymize/main.cpp
@@ -7,7 +7,7 @@
  *           John C. Mosher <John.C.Mosher@uth.tmc.edu>;
  *           Matti Hamalainen <msh@nmr.mgh.harvard.edu>;
  * @version  dev
- * @date     September, 2019
+ * @since    0.1.0
  *
  * @section  LICENSE
  *
