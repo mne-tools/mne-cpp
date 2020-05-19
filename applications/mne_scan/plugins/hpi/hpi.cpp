2c676bf3476520f12cb84a0eda30f595e1c2964b
LorenzE <lorenzesch@hotmail.com>
2020-05-19 12:45:36 -0400

    ENH: Some minor improvements

--- applications/mne_scan/plugins/hpi/hpi.cpp
+++ applications/mne_scan/plugins/hpi/hpi.cpp
@@ -8,7 +8,7 @@
  *
  * @section  LICENSE
  *
- * Copyright (C) 2020, Lorenz Esch and Ruben Dörfel. All rights reserved.
+ * Copyright (C) 2020, Lorenz Esch, Ruben Dörfel. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  * the following conditions are met:
