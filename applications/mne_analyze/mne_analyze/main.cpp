162274adfc851d8b04db77abfcf18aa6248f6373
LorenzE <lorenzesch@hotmail.com>
2020-06-04 09:33:47 -0400

    FIX: Add new qrc files to static (wasm) builds

--- applications/mne_analyze/mne_analyze/main.cpp
+++ applications/mne_analyze/mne_analyze/main.cpp
@@ -79,6 +79,12 @@ AnalyzeCore *pAnalyzeCore;
 
 int main(int argc, char *argv[])
 {
+    // When building a static version of MNE Scan we have to init all resource (.qrc) files here manually
+    #ifdef STATICBUILD
+    Q_INIT_RESOURCE(styles);
+    Q_INIT_RESOURCE(fonts);
+    #endif
+
     QApplication app(argc, argv);
 
     // Set default font
