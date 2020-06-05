19feb583db5c84a5128bb3933f26c12457ab8c0f
LorenzE <lorenzesch@hotmail.com>
2020-06-05 15:20:44 -0400

    FIX: Add missing filtering plugin to mne analyze static builds

--- applications/mne_analyze/mne_analyze/main.cpp
+++ applications/mne_analyze/mne_analyze/main.cpp
@@ -72,6 +72,7 @@ Q_IMPORT_PLUGIN(DataLoader)
 Q_IMPORT_PLUGIN(DataManager)
 Q_IMPORT_PLUGIN(RawDataViewer)
 Q_IMPORT_PLUGIN(AnnotationManager)
+Q_IMPORT_PLUGIN(Filtering)
 #endif
 
 //=============================================================================================================
