006d35b9adc5baa166e5e82fcf6d7aa13ea4299f
LorenzE <lorenzesch@hotmail.com>
2020-06-10 13:49:48 -0400

    ENH: Make first fitlering work in MNE Analyze

--- applications/mne_analyze/plugins/rawdataviewer/rawdataviewer.cpp
+++ applications/mne_analyze/plugins/rawdataviewer/rawdataviewer.cpp
@@ -75,6 +75,7 @@ using namespace UTILSLIB;
 RawDataViewer::RawDataViewer()
 : m_iVisibleBlocks(10)
 , m_iBufferBlocks(10)
+, m_pFiffRawView(Q_NULLPTR)
 {
 }
 
@@ -186,6 +187,7 @@ QWidget *RawDataViewer::getView()
 
 void RawDataViewer::handleEvent(QSharedPointer<Event> e)
 {
+    qDebug() << "RawDataViewer::handleEvent";
     switch (e->getType()) {
     case TRIGGER_REDRAW:
         if(m_pFiffRawView) {
@@ -193,34 +195,27 @@ void RawDataViewer::handleEvent(QSharedPointer<Event> e)
         }
         break;
     case TRIGGER_VIEWER_MOVE:
-        if(m_pFiffRawView) {
-            m_pFiffRawView->updateScrollPosition();
-        }
+        m_pFiffRawView->updateScrollPosition();
         break;
     case SELECTED_MODEL_CHANGED:
         onModelChanged(e->getData().value<QSharedPointer<ANSHAREDLIB::AbstractModel> >());
         break;
     case FILTER_CHANNEL_TYPE_CHANGED:
-        if(m_pFiffRawView) {
-            m_pFiffRawView->setFilterChannelType(e->getData().toString());
-            qDebug() << "FILTER_CHANNEL_TYPE_CHANGED";
-        }
+        m_pFiffRawView->setFilterChannelType(e->getData().toString());
+        qDebug() << "FILTER_CHANNEL_TYPE_CHANGED";
         break;
     case FILTER_ACTIVE_CHANGED:
-        if(m_pFiffRawView) {
-            m_pFiffRawView->setFilterActive(e->getData().toBool());
-            qDebug() << "FILTER_ACTIVE_CHANGED";
-        }
+        qDebug() << "FILTER_ACTIVE_CHANGED";
+        m_pFiffRawView->setFilterActive(e->getData().toBool());
         break;
     case FILTER_DESIGN_CHANGED:
-        if(m_pFiffRawView) {
-            m_pFiffRawView->setFilter(e->getData().value<FilterData>());
-            qDebug() << "FILTER_DESIGN_CHANGED";
-        }
+        m_pFiffRawView->setFilter(e->getData().value<FilterData>());
+        qDebug() << "FILTER_DESIGN_CHANGED";
         break;
     default:
         qWarning() << "[RawDataViewer::handleEvent] Received an Event that is not handled by switch cases.";
     }
+    qDebug() << "RawDataViewer::handleEvent end";
 }
 
 //=============================================================================================================
