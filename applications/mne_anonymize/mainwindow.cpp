4f1c55a1f018c6c7b0f1eeba92a3baee964001d6
jgpc <juangpc@gmail.com>
2020-06-18 02:30:51 -0500

    improve documentation mainwindow.h

--- applications/mne_anonymize/mainwindow.cpp
+++ applications/mne_anonymize/mainwindow.cpp
@@ -11,7 +11,7 @@
  *
  * @section  LICENSE
  *
- * Copyright (C) 2019, Wayne Mead, Juan Garcia-Prieto, Lorenz Esch, Matti Hamalainen, John C. Mosher. All rights reserved.
+ * Copyright (C) 2020, Wayne Mead, Juan Garcia-Prieto, Lorenz Esch, Matti Hamalainen, John C. Mosher. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  * the following conditions are met:
