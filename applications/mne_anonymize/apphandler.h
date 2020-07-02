817f00b022054d57eca78bd375b3efa96d01a6f0
jgpc <juangpc@gmail.com>
2020-07-02 16:51:03 -0500

    main in mne_anonymzie now gui by default

--- applications/mne_anonymize/apphandler.h
+++ applications/mne_anonymize/apphandler.h
@@ -39,6 +39,8 @@
 // INCLUDES
 //=============================================================================================================
 
+#include "settingscontrollercl.h"
+
 //=============================================================================================================
 // QT INCLUDES
 //=============================================================================================================
@@ -90,21 +92,19 @@ public:
      * @brief Creates a QApplication or a QCoreApplication according to user's preference for a command line or a
      * GUI application.
      *
-     * @details Handles input arguments and searches for a "--gui" option, or the compilation context. If found, this will create a
+     * @details Handles input arguments and searches for a "--no-gui" option. If found, this will create a
      *  QCoreApplication so that main can execute the appplication as a command line one. If not found, it creates a
-     *  QApplication so that main can execute a GUI. Boolean member variable m_bGuiMode is updated here. forceGUI flag can be easily
-     * defined during compilation and thus allows for forcing a GUI application during compilation for a specific context (i.e. WASM).
+     *  QApplication so that main can execute a GUI. Boolean member variable m_bGuiMode is updated here.
      *
      * @see QT Documentation
      * @see https://doc.qt.io/qt-5/qapplication.html#details
      *
      * @param [in] argc (argument count) number of arguments on the command line.
      * @param [in] argv (argument vector) an array of pointers to arrays of characters.
-     * @param [in] forceGUI Allows to ensure a GUI application depending on a boolean value flag.
      *
      * @return Pointer to a QCoreApplication.
      */
-    QCoreApplication* createApplication(int& argc, char * argv[], bool forceGUI = false);
+    QCoreApplication* createApplication(int& argc, char * argv[]);
 
     //=========================================================================================================
     /**
@@ -120,13 +120,9 @@ public:
      * @param [in] argc (argument count) number of arguments on the command line.
      * @param [in] argv (argument vector) an array of pointers to arrays of characters.
      *
-     * @return Pointer to a QObject.
+     * @return Pointer to a SettingsControllerCl object.
      */
-    /**
-    * Constructs an appropiate controller QApplication (GUI application).
-    */
-    //    . when inputing a QCoreApplication (command-line application).
-    QObject* createController(const QStringList& args);
+    SettingsControllerCl* createController(const QStringList& args);
 
 protected:
 
