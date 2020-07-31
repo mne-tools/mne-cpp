7d2d933912612f2b6f302d434a4fca09f049ec17
gabrielbmotta <gabrielbenmotta@gmail.com>
2020-07-31 10:01:31 -0400

    ENH: Selection for 2D layout, scaling resets

--- applications/mne_analyze/libs/anShared/Utils/types.h
+++ applications/mne_analyze/libs/anShared/Utils/types.h
@@ -102,8 +102,10 @@ namespace ANSHAREDLIB
         FILTER_CHANNEL_TYPE_CHANGED,// send when the channel type to be filtered changed
         FILTER_ACTIVE_CHANGED,      // send when the filter active state was toggled
         FILTER_DESIGN_CHANGED,      // send when the designed filter changed
-        SET_CHANNEL_SELECTION,
-        SET_CHANNEL_SELECTION_TEMP
+        CHANNEL_SELECTION_INDICES,
+        CHANNEL_SELECTION_ITEMS,
+        CHANNEL_SELECTION_MAP,
+        CHANNEL_SELECTION_CHANNELS
     };
 } //NAMESPACE
 
