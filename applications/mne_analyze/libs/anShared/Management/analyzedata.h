6c7af7f50025efe6374b1eb5c05e9e0bf94c1091
gabrielbmotta <gabrielbenmotta@gmail.com>
2020-09-17 13:37:52 -0400

    MAINT: Add mutex to prevent multiples of delete/load/save of models

--- applications/mne_analyze/libs/anShared/Management/analyzedata.h
+++ applications/mne_analyze/libs/anShared/Management/analyzedata.h
@@ -57,6 +57,7 @@
 #include <QFileInfo>
 #include <QStandardItemModel>
 #include <QDateTime>
+#include <QMutex>
 
 //=============================================================================================================
 // FORWARD DECLARATIONS
@@ -174,6 +175,7 @@ public:
     QSharedPointer<T> loadModel(const QString& sPath,
                                 const QByteArray& byteLoadedData = QByteArray())
     {
+        QMutexLocker locker(&m_Mutex);
         // check if model was already loaded
         if(QSharedPointer<AbstractModel> pModel = getModelByPath(sPath)) {
             qInfo() << "[AnalyzeData::loadModel] Data has been loaded already.";
@@ -207,6 +209,7 @@ public:
     //=========================================================================================================
     template<class T>
     QSharedPointer<T> addModel(QSharedPointer<T> pNewModel, const QString& sModelName){
+        QMutexLocker locker(&m_Mutex);
         QSharedPointer<AbstractModel> temp = qSharedPointerCast<AbstractModel>(pNewModel);
         QStandardItem* pItem = new QStandardItem(sModelName);
         pItem->setEditable(false);
@@ -222,10 +225,10 @@ public:
         return pNewModel;
     }
 
-
+    QMutex                              m_Mutex;        /**< Mutex for thread-safing loading and deleting models */
 
 private:
-    QPointer<AnalyzeDataModel>            m_pData;         /**< The loaded models in form of a QStandardItemModel. */
+    QPointer<AnalyzeDataModel>          m_pData;        /**< The loaded models in form of a QStandardItemModel. */
 
 signals:
     //=========================================================================================================
