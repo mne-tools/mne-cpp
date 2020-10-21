dce5c4a35adb0ab0dd7902bc6988e8329f168f43
gabrielbmotta <gabrielbenmotta@gmail.com>
2020-10-21 16:33:19 -0400

    DOC: update authors

--- applications/mne_analyze/plugins/dataloader/dataloader.h
+++ applications/mne_analyze/plugins/dataloader/dataloader.h
@@ -2,12 +2,13 @@
 /**
  * @file     dataloader.h
  * @author   Lorenz Esch <lesch@mgh.harvard.edu>
+ *           Gabriel Motta <gbmotta@mgh.harvard.edu>
  * @since    0.1.0
  * @date     November, 2019
  *
  * @section  LICENSE
  *
- * Copyright (C) 2019, Lorenz Esch. All rights reserved.
+ * Copyright (C) 2019, Lorenz Esch, Gabriel Motta. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  * the following conditions are met:
