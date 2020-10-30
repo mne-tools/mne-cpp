d47d22db0c9f1563c88f3cae17b58d845e2ea505
gabrielbmotta <gabrielbenmotta@gmail.com>
2020-10-30 14:17:42 -0400

    ENH: add skeleton for new plugin

---
+++ applications/mne_analyze/plugins/dipolefit/dipolefit.h
@@ -0,0 +1,117 @@
+//=============================================================================================================
+/**
+ * @file     dipolefit.h
+ * @author   Gabriel Motta <gbmotta@mgh.harvard.edu>
+ * @since    0.1.7
+ * @date     October, 2020
+ *
+ * @section  LICENSE
+ *
+ * Copyright (C) 2020, Gabriel Motta. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ * the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *       following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *       the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *     * Neither the name of MNE-CPP authors nor the names of its contributors may be used
+ *       to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * @brief    Contains the declaration of the DipoleFit class.
+ *
+ */
+
+#ifndef DIPOLEFIT_H
+#define DIPOLEFIT_H
+
+//=============================================================================================================
+// INCLUDES
+//=============================================================================================================
+
+#include "dipolefit_global.h"
+#include <anShared/Plugins/abstractplugin.h>
+
+//=============================================================================================================
+// QT INCLUDES
+//=============================================================================================================
+
+#include <QtWidgets>
+#include <QtCore/QtPlugin>
+#include <QDebug>
+#include <QPointer>
+
+//=============================================================================================================
+// FORWARD DECLARATIONS
+//=============================================================================================================
+
+namespace ANSHAREDLIB {
+    class Communicator;
+}
+
+//=============================================================================================================
+// DEFINE NAMESPACE SURFERPLUGIN
+//=============================================================================================================
+
+namespace DIPOLEFITPLUGIN
+{
+
+//=============================================================================================================
+/**
+ * DipoleFit Plugin
+ *
+ * @brief The DipoleFit class provides a view with all currently loaded models.
+ */
+class DIPOLEFITSHARED_EXPORT DipoleFit : public ANSHAREDLIB::AbstractPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "ansharedlib/1.0" FILE "dipolefit.json") //New Qt5 Plugin system replaces Q_EXPORT_PLUGIN2 macro
+    // Use the Q_INTERFACES() macro to tell Qt's meta-object system about the interfaces
+    Q_INTERFACES(ANSHAREDLIB::AbstractPlugin)
+
+public:
+    //=========================================================================================================
+    /**
+     * Constructs a DipoleFit.
+     */
+    DipoleFit();
+
+    //=========================================================================================================
+    /**
+     * Destroys the DipoleFit.
+     */
+    virtual ~DipoleFit() override;
+
+    // AbstractPlugin functions
+    virtual QSharedPointer<AbstractPlugin> clone() const override;
+    virtual void init() override;
+    virtual void unload() override;
+    virtual QString getName() const override;
+    virtual QMenu* getMenu() override;
+    virtual QDockWidget* getControl() override;
+    virtual QWidget* getView() override;
+    virtual void handleEvent(QSharedPointer<ANSHAREDLIB::Event> e) override;
+    virtual QVector<ANSHAREDLIB::EVENT_TYPE> getEventSubscriptions() const override;
+
+private:
+
+    QPointer<ANSHAREDLIB::Communicator> m_pCommu;
+};
+
+//=============================================================================================================
+// INLINE DEFINITIONS
+//=============================================================================================================
+
+} // NAMESPACE
+
+#endif // DIPOLEFIT_H
---
+++ applications/mne_analyze/plugins/dipolefit/dipolefit.pro
@@ -0,0 +1,109 @@
+#==============================================================================================================
+#
+# @file     dipolefit.pro
+# @author   Lorenz Esch <lesch@mgh.harvard.edu>;
+#           Lars Debor <Lars.Debor@tu-ilmenau.de>;
+#           Simon Heinke <Simon.Heinke@tu-ilmenau.de>
+# @since    0.1.0
+# @date     March, 2017
+#
+# @section  LICENSE
+#
+# Copyright (C) 2017, Lorenz Esch, Lars Debor, Simon Heinke. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+# the following conditions are met:
+#     * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+#       following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+#       the following disclaimer in the documentation and/or other materials provided with the distribution.
+#     * Neither the name of MNE-CPP authors nor the names of its contributors may be used
+#       to endorse or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# @brief    This project file generates the makefile for the dipolefit plugin.
+#
+#==============================================================================================================
+
+include(../../../../mne-cpp.pri)
+
+TEMPLATE = lib
+
+QT += gui widgets
+
+CONFIG += skip_target_version_ext plugin
+
+DEFINES += DIPOLEFIT_PLUGIN
+
+DESTDIR = $${MNE_BINARY_DIR}/mne_analyze_plugins
+
+contains(MNECPP_CONFIG, static) {
+    CONFIG += staticlib
+    DEFINES += STATICBUILD
+} else {
+    CONFIG += shared
+}
+
+TARGET = dipolefit
+CONFIG(debug, debug|release) {
+    TARGET = $$join(TARGET,,,d)
+}
+
+LIBS += -L$${MNE_LIBRARY_DIR}
+CONFIG(debug, debug|release) {
+    LIBS += -lmnecppUtilsd \
+            -lmnecppFiffd \
+            -lanSharedd \
+            -lmnecppDispd \
+} else {
+    LIBS += -lmnecppUtils \
+            -lmnecppFiff \
+            -lanShared \
+            -lmnecppDisp \
+}
+
+SOURCES += \
+    dipolefit.cpp
+
+HEADERS += \
+    dipolefit_global.h \
+    dipolefit.h
+
+OTHER_FILES += dipolefit.json
+
+INCLUDEPATH += $${EIGEN_INCLUDE_DIR}
+INCLUDEPATH += $${MNE_INCLUDE_DIR}
+INCLUDEPATH += $${MNE_ANALYZE_INCLUDE_DIR}
+
+unix:!macx {
+    QMAKE_RPATHDIR += $ORIGIN/../../lib
+}
+
+# Activate FFTW backend in Eigen
+contains(MNECPP_CONFIG, useFFTW) {
+    DEFINES += EIGEN_FFTW_DEFAULT
+    INCLUDEPATH += $$shell_path($${FFTW_DIR_INCLUDE})
+    LIBS += -L$$shell_path($${FFTW_DIR_LIBS})
+
+    win32 {
+        # On Windows
+        LIBS += -llibfftw3-3 \
+                -llibfftw3f-3 \
+                -llibfftw3l-3 \
+    }
+
+    unix:!macx {
+        # On Linux
+        LIBS += -lfftw3 \
+                -lfftw3_threads \
+    }
+}
