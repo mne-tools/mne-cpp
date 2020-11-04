e3c0ea0adc8cb9346d16c21a301b910f3b78406e
Lorenz Esch <lorenzesch@hotmail.com>
2020-11-04 13:16:08 -0500

    MAINT: Add static deployment scripts. add missing deployment steps to static deployment.

---
+++ tools/deployment/deploy_win_static.bat
@@ -0,0 +1,8 @@
+# Delete folders which we do not want to ship
+Remove-Item 'bin/mne_rt_server_plugins' -Recurse
+Remove-Item 'bin/mne-cpp-test-data' -Recurse
+Remove-Item 'bin/mne_scan_plugins' -Recurse
+Remove-Item 'bin/mne_analyze_plugins' -Recurse
+
+# Creating archive of everything in the bin directory
+7z a mne-cpp-windows-static-x86_64.zip ./bin
