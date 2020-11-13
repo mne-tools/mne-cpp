a31dd0b4bf3645c64650bd305807691ad5b2506a
Lorenz Esch <lorenzesch@hotmail.com>
2020-11-12 22:02:47 -0500

    ENH: Reset cfg file to default if provided file cannot be found.

--- applications/mne_rt_server/mne_rt_server/main.cpp
+++ applications/mne_rt_server/mne_rt_server/main.cpp
@@ -104,9 +104,7 @@ int main(int argc, char *argv[])
         if (QFileInfo(parser.value(inFileOpt)).exists()) {
             if (file.open(QIODevice::Truncate | QIODevice::Text | QIODevice::WriteOnly)) {
                 QTextStream stream(&file);
-
                 stream << QString("simFile = %1").arg(parser.value(inFileOpt));;
-
                 file.close();
 
                 qInfo() << QString("[MneRtServer::main] Streaming file %1").arg(parser.value(inFileOpt));
@@ -115,6 +113,14 @@ int main(int argc, char *argv[])
             }
         } else {
             qWarning("[MneRtServer::main] Provided file does not exist. Falling back to default one.");
+
+            if (file.open(QIODevice::Truncate | QIODevice::Text | QIODevice::WriteOnly)) {
+                QTextStream stream(&file);
+                stream << QString("simFile = <pathTo>/MNE-sample-data/MEG/sample/sample_audvis_raw.fif");
+                file.close();
+            } else {
+                qWarning() << QString("[MneRtServer::main] Could not open %1").arg(QCoreApplication::applicationDirPath() + "/resources/mne_rt_server/plugins/fiffsimulator/FiffSimulation.cfg");
+            }
         }
     }
 
