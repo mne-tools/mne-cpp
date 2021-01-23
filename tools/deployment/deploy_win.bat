fa99989a096e5d228d8b2e12bd4c8cce6da54977
juangpc <juangpc@gmail.com>
2021-01-23 02:54:53 -0500

    make deploy_win callable from anywhere

--- tools/deployment/deploy_win.bat
+++ tools/deployment/deploy_win.bat
@@ -1,14 +1,15 @@
 Rem This script needs to be run from the top level mne-cpp repo folder
 Rem Solve for dependencies only mne_scan.exe and mnecppDisp3D.dll since it links all needed qt and mne-cpp libs
-windeployqt .\bin\mne_scan.exe
-windeployqt .\bin\mnecppDisp3D.dll
+SET scriptPath=%~dp0
+windeployqt %scriptPath%..\..\bin\mne_scan.exe
+windeployqt %scriptPath%..\..\bin\mnecppDisp3D.dll
 
 Rem Copy LSL and Brainflowlibraries manually
-xcopy .\applications\mne_scan\plugins\brainflowboard\brainflow\installed\lib\* .\bin\ /s /i
-xcopy .\applications\mne_scan\plugins\lsladapter\liblsl\build\install\bin\lsl.dll .\bin\ /i
+xcopy %scriptPath%..\..\applications\mne_scan\plugins\brainflowboard\brainflow\installed\lib\* %scriptPath%..\..\bin\ /s /i
+xcopy %scriptPath%..\..\applications\mne_scan\plugins\lsladapter\liblsl\build\install\bin\lsl.dll %scriptPath%..\..\bin\ /i
 
 Rem Delete folders which we do not want to ship
-Remove-Item 'bin/mne-cpp-test-data' -Recurse
+Remove-Item '%scriptPath%..\..\bin/mne-cpp-test-data' -Recurse
 
 Rem Creating archive of all win deployed applications
-7z a mne-cpp-windows-dynamic-x86_64.zip ./bin
+7z a mne-cpp-windows-dynamic-x86_64.zip %scriptPath%..\../bin
