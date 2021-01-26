1acf37151b15de846f9d310e5afdca84f3b962e2
juangpc <juangpc@gmail.com>
2021-01-25 22:52:11 -0500

    add script location as variable and use it in file paths

--- tools/deployment/deploy_win_static.bat
+++ tools/deployment/deploy_win_static.bat
@@ -1,9 +1,9 @@
-# This script needs to be run from the top level mne-cpp repo folder
+SET scriptPath=%~dp0
 # Delete folders which we do not want to ship
-Remove-Item 'bin/mne_rt_server_plugins' -Recurse
-Remove-Item 'bin/mne-cpp-test-data' -Recurse
-Remove-Item 'bin/mne_scan_plugins' -Recurse
-Remove-Item 'bin/mne_analyze_plugins' -Recurse
+Remove-Item '%scriptPath%..\..\bin/mne_rt_server_plugins' -Recurse
+Remove-Item '%scriptPath%..\..\bin/mne-cpp-test-data' -Recurse
+Remove-Item '%scriptPath%..\..\bin/mne_scan_plugins' -Recurse
+Remove-Item '%scriptPath%..\..\bin/mne_analyze_plugins' -Recurse
 
 # Creating archive of everything in the bin directory
-7z a mne-cpp-windows-static-x86_64.zip ./bin
+7z a mne-cpp-windows-static-x86_64.zip %scriptPath%..\../bin
