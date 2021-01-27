fdc1040c07682ad46ee25e2feb6a1c54b5585a8b
juangpc <juangpc@gmail.com>
2021-01-27 13:28:16 -0500

    set old scripts as their current version in master

--- tools/deployment/deploy_win_static.bat
+++ tools/deployment/deploy_win_static.bat
@@ -1,9 +1,9 @@
-SET scriptPath=%~dp0
+# This script needs to be run from the top level mne-cpp repo folder
 # Delete folders which we do not want to ship
-Remove-Item '%scriptPath%..\..\bin/mne_rt_server_plugins' -Recurse
-Remove-Item '%scriptPath%..\..\bin/mne-cpp-test-data' -Recurse
-Remove-Item '%scriptPath%..\..\bin/mne_scan_plugins' -Recurse
-Remove-Item '%scriptPath%..\..\bin/mne_analyze_plugins' -Recurse
+Remove-Item 'bin/mne_rt_server_plugins' -Recurse
+Remove-Item 'bin/mne-cpp-test-data' -Recurse
+Remove-Item 'bin/mne_scan_plugins' -Recurse
+Remove-Item 'bin/mne_analyze_plugins' -Recurse
 
 # Creating archive of everything in the bin directory
-7z a mne-cpp-windows-static-x86_64.zip %scriptPath%..\../bin
+7z a mne-cpp-windows-static-x86_64.zip ./bin
