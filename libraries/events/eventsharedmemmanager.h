629633c5784f2b874af366805fb91d7382ccde2c
juangpc <juangpc@gmail.com>
2021-02-12 23:44:10 -0500

    use optional

--- libraries/events/eventsharedmemmanager.h
+++ libraries/events/eventsharedmemmanager.h
@@ -2,32 +2,50 @@
 #define EVENTSHAREDMEMMANAGER_EVENTS_H
 
 #include <string>
+#include <memory>
+#include <stack>
+#include <QSharedMemory>
+
+#include "event.h"
+#include "eventgroup.h"
+
+namespace EVENTSLIB {
+
+enum SharedMemoryMode { READ, WRITE };
+class EventManager;
+}
 
 namespace EVENTSINTERNAL {
 
-struct EventUpdate {
-   //event
-   //action create or delete event
-   //epoch of change
+enum EventUpdateType { AddEvent, DeleteEvent };
+struct EventUpdate
+{
+    EventUpdateType type;
+    int             sample;
+    //timestamp
 };
-
 
 class EventSharedMemManager
 {
 public:
-    EventSharedMemManager();
+
+    EventSharedMemManager(EVENTSLIB::EventManager* parent = nullptr);
+    bool init(EVENTSLIB::SharedMemoryMode mode);
+    bool stop();
+
     bool isInit() const;
 
-
 private:
-    bool            m_bSharedMemoryInitState;
-    std::string     m_sGroupName;
-    int             m_iTimeLastCheck;
+    EVENTSLIB::EventManager*        m_parent;
+    QSharedMemory                   m_SharedMemory;
+    bool                            m_bIsInit;
+    std::string                     m_sGroupName;
+    EventUpdate*                    m_localBuffer;
+
+ //   int                             m_iTimeLastCheck;
 
  //create a buffer std::queue of EventUpdate objs.
 };
-
-
 
 } //namespace
 #endif // EVENTSHAREDMEMMANAGER_EVENTS_H
