e408b1e6da3746a229fdb2ef69d1074200a2daa0
juangpc <juangpc@gmail.com>
2021-03-04 17:24:11 -0500

    delete old platform and linkage specific deployment scripts

--- tools/deployment/deploy_win_static.bat
+++
@@ -1,9 +0,0 @@
-# This script needs to be run from the top level mne-cpp repo folder
-# Delete folders which we do not want to ship
-Remove-Item 'bin/mne_rt_server_plugins' -Recurse
-Remove-Item 'bin/mne-cpp-test-data' -Recurse
-Remove-Item 'bin/mne_scan_plugins' -Recurse
-Remove-Item 'bin/mne_analyze_plugins' -Recurse
-
-# Creating archive of everything in the bin directory
-7z a mne-cpp-windows-static-x86_64.zip ./bin
