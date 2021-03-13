2bafd6a30118ba0291cc393bd64dc907df1d2aed
juangpc <juangpc@gmail.com>
2021-03-13 03:16:29 -0500

    generate a list of valid web documents and web structure

--- doc/pdfDocumentationGenerator.py
+++ doc/pdfDocumentationGenerator.py
@@ -1,12 +1,15 @@
-# print("Hello world!")
+import sys
 from pathlib import Path
-from os import scandir, path
+from os import scandir, path 
 from shutil import copyfile
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
 from PIL import Image
 
-def extractPathNameExt(file):
+def currentPath():
+    return path.abspath(path.dirname(sys.argv[0]))
+
+def extractFilePathNameExt(file):
     basePath = path.split(file)
     filePath = basePath[0]
     fileName = basePath[1].split(".")[0]
@@ -15,78 +18,141 @@ def extractPathNameExt(file):
 
 def svg2png(file):
     drawing = svg2rlg(file)
-    fPath, fName, _ = extractPathNameExt(file)
+    fPath, fName, _ = extractFilePathNameExt(file)
     pngFile = path.join(fPath, fName + ".png")
-    # renderPM.drawToFile(drawing, pngFile, fmt="PNG")
+    renderPM.drawToFile(drawing, pngFile, fmt="PNG")
 
 def jpg2png(file):
     print(file)
     im1 = Image.open(file)
-    fPath, fName, _ = extractPathNameExt(file)
+    fPath, fName, _ = extractFilePathNameExt(file)
     im1.save(path.join(fPath, fName + ".png"))
-
-class Page:
-
-    def __init__(self, title):
-        self.title = title
-        self.parentTitle = ""
+class Document:
+    def __init__(self, path):
+        self.title = ""
+        self.parent = ""
         self.nav_order = 0
         self.has_children = False
         self.nav_exclude = False
-        self.fullPath = ""
+        self.fullPath = path
+    
+    def setTitle(self, title):
+        self.title = title
+
+    def setParent(self, parent):
+        self.parent = parent
+
+    def setNavOrder(self, nav_order):
+        self.nav_order = nav_order
+    
+    def setHasChildren(self, hasChildren):
+        self.has_children = hasChildren
+    
+    def setNavExclude(self, navExclude):
+        self.nav_exclude = navExclude
+    
+    def setFullPath(self, path):
+        self.fullPath = path
+
+    def __str__(self):
+        s = ""
+        # s += "--- Class: " + type(self).__name__ + " ---\n"
+        # s += "path: " + self.fullPath + "\n"
+        s += "title: " + self.title + "\n"
+        # s += "parent: " + self.parent + "\n"
+        # s += "nav_order: " + str(self.nav_order) + "\n"
+        # s += "has_children: " + str(self.has_children) + "\n"
+        # s += "nav_exclude: " + str(self.nav_exclude) + "\n"
+        return s
+
+    def __repr__(self):
+        return str(self)
+
+class Page:
+    def __init__(self,doc):
+        self.doc = doc
+        self.children = []
+    def insert(self,d2):
+        if d2.parent == self.doc.title:
+            self.children.append(Page(d2))
+            self.children.sort(key=lambda p:p.doc.nav_order)
+            return True
+        else:
+            for c in self.children:
+                found = c.insert(d2)
+                if found:
+                    return True
+            return False
+    def print(self, spaces = 0):
+        s = " " * spaces + self.doc.title + "\n"
+        for p in self.children:
+            s += "  " * spaces + p.print(spaces+2)
+        return s
+
+    def __str__(self):
+        return self.print()
+
+    def __repr__(self):
+        return str(self)      
 
 def parse(s,key):
     if s.lstrip().startswith(key):
         return s.split(":")[1].lstrip()
 
-def parseFile(file):
+def parseFile(file, verboseMode = False):
     with open(file, 'r', encoding="utf8") as fileOpened:
-        isInsideHeader = False
-        isCodeText = False
+        insideHeader = False
+        codeText = False
+        validContentFile = False
+        doc = Document(file.path)
         for line in fileOpened:
-            if not isCodeText and line.startswith("```") or line.count("```")%2 != 0:
-                isCodeText = True
+            # print(line)
+            if not codeText and (line.startswith("```") or line.count("```")%2 != 0):
+                codeText = True
                 continue
-            if isCodeText and line.startswith("```") or line.count("```")%2 != 0:
-                isCodeText = False
+            if codeText and (line.startswith("```") or line.count("```")%2 != 0):
+                codeText = False
                 continue
-            if line.startswith("---") and not isInsideHeader:
-                isInsideHeader = True
+            if line.startswith("---") and not insideHeader:
+                insideHeader = True
                 continue
-            if line.startswith("---") and isInsideHeader:
-                isInsideHeader = False
+            if line.startswith("---") and insideHeader:
+                insideHeader = False
                 break
-            if isInsideHeader:
+            if insideHeader and not codeText:
                 if line.lstrip().startswith("title"):
-                    print("title: " + line.split(":")[1].lstrip().rstrip())
+                    doc.setTitle(line.split(":")[1].lstrip().rstrip())
+                    validContentFile = True
                     continue
                 if line.lstrip().startswith("parent"):
-                    print("parent: " + line.split(":")[1].lstrip().rstrip())
+                    doc.setParent(line.split(":")[1].lstrip().rstrip())
                     continue
                 if line.lstrip().startswith("nav_order"):
-                    print("nav_order: " + line.split(":")[1].lstrip().rstrip())
+                    doc.setNavOrder(int(line.split(":")[1].lstrip().rstrip()))
                     continue
                 if line.lstrip().startswith("has_children"):
-                    print("has_children: " + line.split(":")[1].lstrip().rstrip())
+                    doc.setHasChildren(bool(line.split(":")[1].lstrip().rstrip()))
                     continue
-                if line.lstrip().startswith("nav"):
-                    print("nav: " + line.split(":")[1].lstrip().rstrip())
+                if line.lstrip().startswith("nav_exclude"):
+                    doc.setNavExclude(bool(line.split(":")[1].lstrip().rstrip()))
                     continue
+        if validContentFile and verboseMode:
+            print(doc)
+    return doc, validContentFile
 
-def scanFolder(folderPath):
+def scanFolder(folderPath, documents = []):
     for file in scandir(folderPath):
-        if file.is_file() and file.name.endswith("md") and file.name != "README.md":
-            print(">>>>")
-            print(file.path)
-            parseFile(file)
+        if file.is_file() and file.name.endswith("md"):
+            # print("parsing file: " + file.path)
+            doc, valid = parseFile(file)
+            if valid:
+                documents.append(doc)
         if file.is_dir():
-            scanFolder(file)
-
-# myPath = path.join("doc","gh-pages")
-# scanFolder(myPath)
+            scanFolder(file, documents)
+    return documents
 
 def processImage(imageFile):
-    _, _, iExt = extractPathNameExt(imageFile)
+    _, _, iExt = extractFilePathNameExt(imageFile)
     if iExt == "jpg" or iExt == "jpeg":
         jpg2png(imageFile)
     if iExt == "svg2":
@@ -99,15 +165,21 @@ def recursiveProcess(folderPath, func):
         if file.is_file():
             func(file)
 
-
-
 # imagesFolder = path.join("gh-pages", "images")
-
 # svg2png(svgFile)
-
-
 # jpg2png("gh-pages/images/1280px-EEGoSportsGUI.jpg")
 
+myPath = path.join(currentPath(),"gh-pages")
+# print(myPath)
+documents = scanFolder(myPath)
 
+print(documents)
 
+web = Page(Document(""))
+while documents:
+    for d in documents:
+        if web.insert(d):
+            documents.remove(d)
 
+print("Printing Web Structure:")
+print(web)
