3c216ce023ea81277c0cd02216d6bb136322e278
RDoerfel <doerfelruben@aol.com>
2021-03-13 17:54:38 +0100

    Enh:only call updateProjectors() if necessary

--- applications/mne_scan/plugins/hpi/hpi.cpp
+++ applications/mne_scan/plugins/hpi/hpi.cpp
@@ -191,7 +191,13 @@ void Hpi::update(SCMEASLIB::Measurement::SPtr pMeasurement)
         // Check if data is present
         if(pRTMSA->getMultiSampleArray().size() > 0) {
             //If bad channels changed, recalcluate projectors
-            updateProjections();
+            if(m_iNumberBadChannels != m_pFiffInfo->bads.size()
+                || m_matCompProjectors.rows() == 0
+                || m_matCompProjectors.cols() == 0) {
+                updateProjections();
+                m_iNumberBadChannels = m_pFiffInfo->bads.size();
+            }
+
 
             m_mutex.lock();
             bool bDoSingleHpi = m_bDoSingleHpi;
@@ -282,19 +288,6 @@ void Hpi::initPluginControlWidgets()
 void Hpi::updateProjections()
 {
     if(m_pFiffInfo) {
-        m_mutex.lock();
-        if(m_iNumberBadChannels != m_pFiffInfo->bads.size()
-           || m_matCompProjectors.rows() == 0
-           || m_matCompProjectors.cols() == 0) {
-            m_iNumberBadChannels = m_pFiffInfo->bads.size();
-        } /*else {
-            m_mutex.unlock();
-            return;
-        }*/
-        m_mutex.unlock();
-
-        qDebug() << "updating projectors";
-
         Eigen::MatrixXd matProjectors = Eigen::MatrixXd::Identity(m_pFiffInfo->chs.size(), m_pFiffInfo->chs.size());
         Eigen::MatrixXd matComp = Eigen::MatrixXd::Identity(m_pFiffInfo->chs.size(), m_pFiffInfo->chs.size());
 
@@ -363,7 +356,7 @@ void Hpi::onAllowedRotationChanged(double dAllowedRotation)
 
 void Hpi::onDigitizersChanged(const QList<FIFFLIB::FiffDigPoint>& lDigitzers,
                               const QString& sFilePath)
-{    
+{
     m_mutex.lock();
     if(m_pFiffInfo) {
         m_pFiffInfo->dig = lDigitzers;
