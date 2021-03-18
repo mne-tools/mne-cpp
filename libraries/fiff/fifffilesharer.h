9cc6092ce90f4499ce26055b6729c36e4fb3c3ff
gabrielbmotta <gabrielbenmotta@gmail.com>
2021-03-18 16:33:37 -0400

    DOC: document new FiffFileSharer

--- libraries/fiff/fifffilesharer.h
+++ libraries/fiff/fifffilesharer.h
@@ -47,44 +47,91 @@
 
 #include <QFileSystemWatcher>
 
+//=============================================================================================================
+// DEFINE NAMESPACE FIFFLIB
+//=============================================================================================================
+
 namespace FIFFLIB
 {
+
+//=============================================================================================================
+/**
+ * FiffFileSharer provides a way to share fiff files while they are being saved in real time
+ */
 class FIFFSHARED_EXPORT FiffFileSharer : public QObject
 {
     Q_OBJECT
 public:
 
     //=========================================================================================================
+    /**
+     * Constructs a FiffFileSharer with default paramaters (based on m_sDefaultDirectory and m_sDefaultFileName)
+     */
     FiffFileSharer();
 
     //=========================================================================================================
+    /**
+     * Constructs a FiffFileSharer to watch/save to sDirName
+     *
+     * @param[in] sDirName      Directory to/from which data will saved/read
+     */
     FiffFileSharer(const QString& sDirName);
 
     //=========================================================================================================
+    /**
+     * Copies unfinished fiff file to set directory and appends relevant end tags to copy
+     *
+     * @param[in] sSourcePath   source file to ber copied
+     */
     void copyRealtimeFile(const QString &sSourcePath);
 
     //=========================================================================================================
+    /**
+     * Sets member QFileSystemWatcher to watch set directory
+     */
     void initWatcher();
 
 private:
 
     //=========================================================================================================
+    /**
+     * Creates specified shared direcotry if it does not exist.
+     *
+     * @return Retruns whether shared direcotry exists in file structure.
+     */
     bool initSharedDirectory();
 
     //=========================================================================================================
+    /**
+     * Called when directory QFileSystemWatcher m_fileWatcher is watching gets updated/changed.
+     * Adds next realtime file to be watched to m_fileWatcher
+     *
+     * @param[in] sPath     Path of directory that was changed.
+     */
     void onDirectoryChanged(const QString &sPath);
 
     //=========================================================================================================
+    /**
+     * Called when a file QFileSystemWatcher m_fileWatcher is watching gets updated.
+     * Emits path of new file and stops watching it.
+     *
+     * @param sPath
+     */
     void onFileChanged(const QString &sPath);
 
+    QFileSystemWatcher      m_fileWatcher;          /**< Watches m_sDirectory for new files. */
 
-
-    QFileSystemWatcher      m_fileWatcher;
-
-    QString                 m_sDirectory;
-    int                     m_iFileIndex;
+    QString                 m_sDirectory;           /**< Directory where files will be saved to / read from. */
+    int                     m_iFileIndex;           /**< File counter to give files unique name */
 
 signals:
+
+    //=========================================================================================================
+    /**
+     * Emits path of new shared fiff file.
+     *
+     * @param[in] sPath     Path of new shared fiff file.
+     */
     void newFileAtPath(const QString& sPath);
 
 };
