ca679ac93524c96cbdfcedf2e06842d304a0e489
juangpc <juangpc@gmail.com>
2021-04-01 13:29:46 -0400

    clean up add example demo script for module

---
+++ tools/python/demo_recursive_folder_process.py
@@ -0,0 +1,19 @@
+import mne_cpp.core
+# import mne_cpp.pdf_doc
+from os import stat
+
+mne_cpp.core.version()
+
+projectFolder = mne_cpp.core.baseFolder()
+
+#  Recursively list all the files in a directory and order by size and print results.
+listOfFiles = []
+mne_cpp.core.recursiveFolderProcess(projectFolder + 'doc/gh-pages', lambda f:                     \
+                                                                        listOfFiles.append((f, stat(f).st_size))    \
+                                                                        if f.name.endswith('.md')  \
+                                                                        else None )
+listOfFiles.sort(reverse=True, key=lambda f:f[1])
+for f in listOfFiles:
+    print('File: ' + f[0].path + ' - (' + mne_cpp.core.size_human_readable(f[1]) + ')')
+
+    
