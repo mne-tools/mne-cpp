534f4910e44fe69968723d58f8a2a27a2a1cb0a7
juangpc <juangpc@gmail.com>
2021-05-21 10:32:03 -0400

    add deleteall option to mnetracer.py

--- tools/python/mnetracer.py
+++ tools/python/mnetracer.py
@@ -1,6 +1,9 @@
 import re
 import sys
 
+num_spaces_tab = 4
+tracer_macro = "MNE_TRACE()"
+
 def parseInputArguments(argsToParse, **opts):
     mainMode = False
     caseSensitive = True
@@ -49,9 +52,7 @@ def noneIfEmpty(s):
 
 def add_tracer_macro(in_text):
     # STUDY REGEX https://regex101.com/r/Qa7BOx/1/ -or- https://regex101.com/r/NDUE03/1
-    num_spaces_tab = 4
-    tracer_macro = "MNE_TRACE()"
-    tracer_macro_line  = "\n" + " " * num_spaces_tab + tracer_macro
+    tracer_macro_line = "\n" + " " * num_spaces_tab + tracer_macro
     regExp = "(?P<methodDef>([^;\n ]+\s?)?\w+::~?\w+\([^{;]*\)[^;\n]*\n([^{]*\n)*{)(?!" + tracer_macro_line.replace("(","\(").replace(")","\)") + ")"
     match = re.search(regExp, in_text)
     if match:
@@ -63,9 +64,7 @@ def add_tracer_macro(in_text):
 
 def delete_tracer_macro(in_text):
     #STUDY https://regex101.com/r/pzKnIC/1
-    num_spaces_tab = 4
-    tracer_macro = "MNE_TRACE()"
-    tracer_macro_line  = "\n" + " " * num_spaces_tab + tracer_macro
+    tracer_macro_line = "\n" + " " * num_spaces_tab + tracer_macro
     regExp = "(?P<trace>" + tracer_macro_line.replace("(","\(").replace(")","\)") + ")"
     match = re.search(regExp, in_text)
     if match:
@@ -75,25 +74,42 @@ def delete_tracer_macro(in_text):
     else:
         return in_text
 
+def delete_all_tracer_macro(in_text):
+    #STUDY https://regex101.com/r/pzKnIC/1
+    tracer_macro_line = tracer_macro
+    regExp = "(?P<trace>" + "([ \t]*)" + tracer_macro_line.replace("(","\(").replace(")","\)") + "\n?" + ")"
+    match = re.search(regExp, in_text)
+    if match:
+        methodDef = noneIfEmpty(match.group("trace"))
+        outText = in_text[:match.start(0)] + in_text[match.end(0):]
+        return delete_all_tracer_macro(outText)
+    else:
+        return in_text        
+
 if __name__ == "__main__":
 
     opts = (("file", ""),("verboseMode", False),("mode","add"))
-    (file_name, verboseMode, mode) = parseInputArguments(sys.argv, opts = opts, admit_unknown_options=True, main_mode=True)
+    (input_file_name, verboseMode, mode) = parseInputArguments(sys.argv, opts = opts, admit_unknown_options=True, main_mode=True)
 
-    if file_name == "":
+    if input_file_name == "":
         print("You need to specify an input file")
     else:
         if mode == "add":
-            with open(file_name, 'r') as in_file:
+            with open(input_file_name, 'r') as in_file:
                 out_text = add_tracer_macro(in_file.read())
-            with open(file_name, 'w') as in_file:
-                in_file.write(out_text)
+            with open(input_file_name, 'w') as out_file:
+                out_file.write(out_text)
         if mode == "delete":
-            with open(file_name, 'r') as in_file:
+            with open(input_file_name, 'r') as in_file:
                 out_text = delete_tracer_macro(in_file.read())
-            with open(file_name, 'w') as in_file:
-                in_file.write(out_text)
-        
+            with open(input_file_name, 'w') as out_file:
+                out_file.write(out_text)
+        if mode == "deleteAll":
+            with open(input_file_name, 'r') as in_file:
+                out_text = delete_all_tracer_macro(in_file.read())
+            with open(input_file_name, 'w') as out_file:
+                out_file.write(out_text)
+
 
 
 
