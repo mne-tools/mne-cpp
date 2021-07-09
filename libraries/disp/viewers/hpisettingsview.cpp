79bb107e7f7a1bac3fc5aa5481acc71f43ace1eb
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2021-07-09 10:32:37 -0400

    Reenabling align fiducaials for dig data from buffer

--- libraries/disp/viewers/hpisettingsview.cpp
+++ libraries/disp/viewers/hpisettingsview.cpp
@@ -308,7 +308,7 @@ void HpiSettingsView::onLoadDigitizers()
         if (checkFile.exists() && checkFile.isFile()) {
             // Stop cont HPI first
             m_pUi->m_checkBox_continousHPI->setChecked(false);
-            emit digitizersChanged(readPolhemusDig(fileName_HPI), fileName_HPI);
+            emit digitizersChanged(readDigitizersFromFile(fileName_HPI), fileName_HPI);
         } else {
             QMessageBox msgBox;
             msgBox.setText("File could not be loaded!");
@@ -413,7 +413,7 @@ void HpiSettingsView::onRemoveCoil()
 
 //=============================================================================================================
 
-QList<FiffDigPoint> HpiSettingsView::readPolhemusDig(const QString& fileName)
+QList<FiffDigPoint> HpiSettingsView::readDigitizersFromFile(const QString& fileName)
 {
     resetTables();
 
