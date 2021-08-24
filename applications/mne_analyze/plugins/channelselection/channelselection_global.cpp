51111df9f7efaddc4de787c309667be09515cd32
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2021-08-24 12:15:30 -0400

    MAINT: change to date and time foe mne analyze functions

--- applications/mne_analyze/plugins/channelselection/channelselection_global.cpp
+++ applications/mne_analyze/plugins/channelselection/channelselection_global.cpp
@@ -1,3 +1,5 @@
 #include "channelselection_global.h"
 
-const char* CHANNELSELECTIONPLUGIN::BUILD_TIMESTAMP(){ return BUILDINFO::time();};
+const char* CHANNELSELECTIONPLUGIN::BUILD_TIME(){ return BUILDINFO::time();};
+
+const char* CHANNELSELECTIONPLUGIN::BUILD_DATE(){ return BUILDINFO::date();};
