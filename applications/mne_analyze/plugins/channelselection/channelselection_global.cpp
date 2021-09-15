3705bde402755438999586b4fdac9285661ba429
juangpc <juangpc@gmail.com>
2021-09-15 17:53:10 -0400

    start renaming export symbols for each library

--- applications/mne_analyze/plugins/channelselection/channelselection_global.cpp
+++ applications/mne_analyze/plugins/channelselection/channelselection_global.cpp
@@ -1,5 +1,5 @@
 #include "channelselection_global.h"
 
-const char* CHANNELSELECTIONPLUGIN::BUILD_TIME(){ return BUILDINFO::time();};
+const char* CHANNELSELECTIONPLUGIN::BUILD_DATETIME(){ return BUILDINFO::dateTime();};
 
 const char* CHANNELSELECTIONPLUGIN::BUILD_DATE(){ return BUILDINFO::date();};
