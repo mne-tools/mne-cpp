92e92f5881fbc1ee28355208209319d394e08ab8
juangpc <juangpc@gmail.com>
2021-09-15 17:53:10 -0400

    start renaming export symbols for each library

--- applications/mne_analyze/plugins/averaging/averaging_global.cpp
+++ applications/mne_analyze/plugins/averaging/averaging_global.cpp
@@ -1,5 +1,3 @@
 #include "averaging_global.h"
 
-const char* AVERAGINGPLUGIN::BUILD_TIME(){ return BUILDINFO::time();};
-
-const char* AVERAGINGPLUGIN::BUILD_DATE(){ return BUILDINFO::date();};
+const char* AVERAGINGPLUGIN::BUILD_TIMETIME(){ return BUILDINFO::dateTime();};
