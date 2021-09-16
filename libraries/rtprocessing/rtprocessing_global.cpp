38711d3dbc39975957df24920c2646a66b46610a
juangpc <juangpc@gmail.com>
2021-09-16 17:16:49 -0400

    use buildinfo updated in rtprocessing lib

--- libraries/rtprocessing/rtprocessing_global.cpp
+++ libraries/rtprocessing/rtprocessing_global.cpp
@@ -1,5 +1,8 @@
 #include "rtprocessing_global.h"
 
-const char* RTPROCESSINGLIB::BUILD_TIME(){ return BUILDINFO::time();};
+const char* RTPROCESSINGLIB::BUILD_DATETIME(){ return BUILDINFO::dateTime();};
 
-const char* RTPROCESSINGLIB::BUILD_DATE(){ return BUILDINFO::date();};
+const char* RTPROCESSINGLIB::BUILD_HASH(){ return BUILDINFO::gitHash();};
+
+const char* RTPROCESSINGLIB::BUILD_HASH_LONG(){ return BUILDINFO::gitHashLong();};
+
