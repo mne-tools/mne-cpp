89be21657c08fcedee7e0f36c93ed359040a0df8
juangpc <juangpc@gmail.com>
2021-09-16 22:36:52 -0400

    export buildinfo in communication library

--- libraries/communication/communication_global.cpp
+++ libraries/communication/communication_global.cpp
@@ -1,5 +1,7 @@
 #include "communication_global.h"
 
-const char* COMMUNICATIONLIB::BUILD_TIME(){ return BUILDINFO::time();};
+const char* COMMUNICATIONLIB::BUILD_DATETIME(){ return BUILDINFO::dateTime();};
 
-const char* COMMUNICATIONLIB::BUILD_DATE(){ return BUILDINFO::date();};
+const char* COMMUNICATIONLIB::BUILD_HASH(){ return BUILDINFO::gitHash();};
+
+const char* COMMUNICATIONLIB::BUILD_HASH_LONG(){ return BUILDINFO::gitHashLong();};
