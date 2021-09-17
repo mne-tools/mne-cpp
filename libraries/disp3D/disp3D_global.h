c2153ecf844b61fd21dd7314a42fd51b74cb70be
juangpc <juangpc@gmail.com>
2021-09-16 22:25:38 -0400

    export buildinfo in disp3d library

--- libraries/disp3D/disp3D_global.h
+++ libraries/disp3D/disp3D_global.h
@@ -8,7 +8,7 @@
  *
  * @section  LICENSE
  *
- * Copyright (C) 2018, Lorenz Esch, Christoph Dinh. All rights reserved.
+ * Copyright (C) 2021, Lorenz Esch, Christoph Dinh. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  * the following conditions are met:
@@ -56,8 +56,9 @@
 #endif
 
 namespace DISP3DLIB{
-    DISP3DLIB_DISP3D_GLOBAL_H const char* BUILD_TIME();
-    DISP3DLIB_DISP3D_GLOBAL_H const char* BUILD_DATE();
+    DISP3DLIB_DISP3D_GLOBAL_H const char* BUILD_DATETIME();
+    DISP3DLIB_DISP3D_GLOBAL_H const char* BUILD_HASH();
+    DISP3DLIB_DISP3D_GLOBAL_H const char* BUILD_HASH_LONG();
 }
 
 #endif // DISP3DLIB_GLOBAL_H
