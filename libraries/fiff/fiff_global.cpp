e15de06b9706d6f04430588edadfedacc71d97e6
juangpc <juangpc@gmail.com>
2021-09-16 21:59:44 -0400

    export buildinfo in fiff library

--- libraries/fiff/fiff_global.cpp
+++ libraries/fiff/fiff_global.cpp
@@ -1,6 +1,7 @@
 #include "fiff_global.h"
 
-const char* FIFFLIB::BUILD_TIME(){ return BUILDINFO::time();};
+const char* FIFFLIB::BUILD_DATETIME(){ return BUILDINFO::dateTime();};
 
-const char* FIFFLIB::BUILD_DATE(){ return BUILDINFO::date();};
+const char* FIFFLIB::BUILD_HASH(){ return BUILDINFO::gitHash();};
 
+const char* FIFFLIB::BUILD_HASH_LONG(){ return BUILDINFO::gitHashLong();};
