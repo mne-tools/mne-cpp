0a564a9daa17c5056ff865e01128be07fe865765
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2021-09-23 12:18:04 -0400

    MAINT: add missed unsaved files

--- libraries/inverse/inverse_global.cpp
+++ libraries/inverse/inverse_global.cpp
@@ -1,7 +1,7 @@
 #include "inverse_global.h"
 
-const char* INVERSELIB::buildDateTime(){ return BUILDINFO::dateTimeNow();};
+const char* INVERSELIB::buildDateTime(){ return UTILSLIB::dateTimeNow();};
 
-const char* INVERSELIB::buildHash(){ return BUILDINFO::gitHash();};
+const char* INVERSELIB::buildHash(){ return UTILSLIB::gitHash();};
 
-const char* INVERSELIB::buildHashLong(){ return BUILDINFO::gitHashLong();};
+const char* INVERSELIB::buildHashLong(){ return UTILSLIB::gitHashLong();};
