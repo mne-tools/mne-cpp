442f47213fbbccf3b56c2e60b15374b97f3b3a0d
juangpc <juangpc@gmail.com>
2021-10-05 03:36:55 -0400

    improve windows testing

--- tools/testing/test_win.ps1
+++ tools/testing/test_win.ps1
@@ -6,10 +6,14 @@ $CURRENT_PATH = pwd
 cd $PSScriptRoot/../..
 
 Get-ChildItem -Filter bin/test_*.exe | ForEach {
-  Write-Output "" "" Starting $_.Fullname; 
-  &$_.Fullname; if (($lastexitcode -ne 0) -and $ErrorActionPreference -eq "Stop") { 
+  Write-Output "" "" "Starting $_" ""; 
+  &$_.Fullname; 
+  if (($lastexitcode -ne 0) -and $ErrorActionPreference -eq "Stop") { 
+    Write-Output "" "" "$_ IS FAILING" "" "";
     exit $lastexitcode
-  }  
+  }  else { 
+    Write-Output "" "" "$_ FINISHED OK!" "" "";
+  }
 }
 
 # Write-Host "$output"
