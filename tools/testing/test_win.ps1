4363647d0c4abfe0c7c9d09cd84732aa4fce3368
juangpc <juangpc@gmail.com>
2021-10-05 03:22:05 -0400

    win test script bail on errors

--- tools/testing/test_win.ps1
+++ tools/testing/test_win.ps1
@@ -1,4 +1,16 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
 $CURRENT_PATH = pwd
+
 cd $PSScriptRoot/../..
-Get-ChildItem -Filter bin/test_*.exe | ForEach {Write-Output "" "" Starting $_.Fullname; &$_.Fullname}
+
+Get-ChildItem -Filter bin/test_*.exe | ForEach {
+  Write-Output "" "" Starting $_.Fullname; 
+  &$_.Fullname; if (($lastexitcode -ne 0) -and $ErrorActionPreference -eq "Stop") { 
+    exit $lastexitcode
+  }  
+}
+
+# Write-Host "$output"
 cd $CURRENT_PATH
