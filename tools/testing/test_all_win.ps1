91fe7c796f0a738f2f48b3ae357feeab1f314c26
juangpc <juangpc@gmail.com>
2021-10-13 12:32:09 -0400

    update testing script versions

---
+++ tools/testing/test_all_win.ps1
@@ -0,0 +1,56 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "SilentlyContinue"
+$PrintOutput = "False"
+$StopOnFirstTestFail = "False"
+
+$verboseMode=$args[0]
+$exitMode=$args[1]
+
+if ( $verboseMode -eq "verbose" ) {
+  $PrintOutput = "True"
+}
+
+if ( $exitMode -eq "exitOnFail" ) {
+  $StopOnFirstTestFail = "True"
+}
+
+$global:CompoundOutput = 0
+
+function compoundReturnValue( $e )
+{
+  if( $e -ne 0)
+  {
+    $global:CompoundOutput = $global:CompoundOutput + 1;
+  }
+  # Write-Host "compound output: $global:CompoundOutput";
+}
+
+$CURRENT_PATH = pwd
+cd $PSScriptRoot/../..
+
+Write-Host "" ""
+
+Get-ChildItem -Filter bin/test_*.exe | ForEach {
+  if ( $PrintOutput -eq "True" ) {
+    &$_.FullName;
+  } else {
+    Write-Host " $_  => " -NoNewline
+    # ($out = &$_.Fullname) ;
+    ($out = &$_.Fullname) 2>&1 | out-null;
+    # &$_.Fullname 2>&1 | Out-Null;
+  }
+  compoundReturnValue $lastexitcode;
+  if ( $lastexitcode -ne 0 ) { 
+    Write-Host " FAIL!"  -ForegroundColor Red;
+    if ($StopOnFirstTestFail -eq "True" ) {
+      exit $lastexitcode
+    }
+  }  else { 
+    Write-Host " RockSolid!" -ForegroundColor Green;
+  }
+}
+
+cd $CURRENT_PATH
+
+exit $CompoundOutput;
+
