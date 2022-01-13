0375822e00255109bec4a64620c98e26df0ef736
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2022-01-13 11:46:04 -0500

    DOC: updating author lists.

--- libraries/utils/warp.cpp
+++ libraries/utils/warp.cpp
@@ -1,13 +1,14 @@
 //=============================================================================================================
 /**
  * @file     warp.cpp
- * @author   Lorenz Esch <lesch@mgh.harvard.edu>
+ * @author   Lorenz Esch <lesch@mgh.harvard.edu>;
+ *           Gabriel Motta <gbmotta@mgh.harvard.edu>
  * @since    0.1.0
  * @date     November, 2015
  *
  * @section  LICENSE
  *
- * Copyright (C) 2015, Lorenz Esch. All rights reserved.
+ * Copyright (C) 2015, Lorenz Esch, Gabriel Motta. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  * the following conditions are met:
