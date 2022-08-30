890b626d3741c9268445da761449db5e0a938a4d
Gabriel Motta <gabrielbenmotta@gmail.com>
2022-08-30 11:01:17 -0400

    ENH: add wasm build script

---
+++ tools/deployment/wasm.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+## User editable through flags
+SOURCE_REPO=""
+EMSDK_VERSION="latest"
+QT_VERSION="5.15"
+
+## Stop at first error
+set -e
+
+for (( j=0; j<argc; j++)); do
+    if [ "${argv[j]}" == "--source" ] || [ "${argv[j]}" == "-s" ]; then
+        SOURCE_REPO="${argv[j+1]}" 
+    elif [ "${argv[j]}" == "--emsdk" ] || [ "${argv[j]}" == "-e" ]; then
+        EMSDK_VERSION="${argv[j+1]}" 
+    elif [ "${argv[j]}" == "--qt" ] || [ "${argv[j]}" == "-q" ]; then
+        QT_VERSION="${argv[j+1]}" 
+    fi
+done
+
+if [ "$1" ]; then
+	SOURCE_REPO="$1"
+	echo "Source repo set to ${SOURCE_REPO}"
+else
+	echo "No source specified."
+	echo "Please set a source with the '--source' flag."
+	exit 1
+fi
+
+## emsdk
+echo "Setting up emsdk ${EMSDK_VERSION}"
+git clone https://github.com/emscripten-core/emsdk.git
+cd emsdk
+git pull
+./emsdk install ${EMSDK_VERSION}
+./emsdk activate ${EMSDK_VERSION}
+source ./emsdk_env.sh
+cd ..
+echo "emsdk set up."
+
+## Qt
+echo "Setting up Qt ${QT_VERSION}"
+git clone https://code.qt.io/qt/qt5.git -b ${QT_VERSION}
+cd qt5
+./init-repository -f --module-subset=qtbase,qtcharts,qtsvg
+./configure -xplatform wasm-emscripten -feature-thread -skip webengine -nomake tests -nomake examples -no-dbus -no-ssl -no-pch -opensource -confirm-license -prefix "$PWD/../Qt5_binaries"
+make module-qtbase module-qtsvg module-qtcharts -j4
+make install -j4
+cd ..
+echo "Qt set up."
+
+## Building the project
+echo "Building source repo..."
+git clone ${SOURCE_REPO}
+REPO_FOLDER=$(echo "${SOURCE_REPO}" | grep -oP '[^/]*(?=\.git$)')
+cd ${REPO_FOLDER}
+../Qt5_binaries/bin/qmake -r MNECPP_CONFIG=wasm
+make -j4
+GIT_HASH=$(git rev-parse HEAD)
+cd ..
+echo "Done"
+
+## Build log
+echo "Saving build and system parameters..."
+touch build_info.txt
+echo "System Info: $(uname -rvop)" >> build_info.txt
+echo "emsdk version: ${EMSDK_VERSION}" >> build_info.txt
+echo "Qt version: ${QT_VERSION}" >> build_info.txt
+echo "Build time: $(date)" >> build_info.txt
+echo "Build git hash: ${GIT_HASH}"
+echo "Saved."
+
+exit 0
