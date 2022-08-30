27d9749d9774d7fce9142ef864c4d8cf4ef30e13
Gabriel Motta <gabrielbenmotta@gmail.com>
2022-08-30 13:29:18 -0400

    Add sed replacment and repo check

--- tools/deployment/wasm.sh
+++ tools/deployment/wasm.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+###
+# It is recommended you run this script while in an empty directory.
+# The script will download and build the necessary components, and 
+# reuse them if they are already present. 
+###
+
 argc=$#
 argv=("$@")
 
@@ -21,11 +27,9 @@ for (( j=0; j<argc; j++)); do
     fi
 done
 
-echo "${SOURCE_REPO}"
-
-echo "${EMSDK_VERSION}"
-
-echo "${QT_VERSION}"
+# echo "${SOURCE_REPO}"
+# echo "${EMSDK_VERSION}"
+# echo "${QT_VERSION}"
 
 if [ "${SOURCE_REPO}" ]; then
 	echo "Source repo set to ${SOURCE_REPO}"
@@ -36,8 +40,13 @@ else
 fi
 
 ## emsdk
-echo "Setting up emsdk ${EMSDK_VERSION}"
-git clone https://github.com/emscripten-core/emsdk.git
+echo "Setting up emsdk ${EMSDK_VERSION}..."
+if [ -d "emsdk" ]; then
+	echo "Found emsdk folder."
+else
+	echo "emsdk folder not found. Fetching repo..."
+	git clone https://github.com/emscripten-core/emsdk.git
+fi
 cd emsdk
 git pull
 ./emsdk install ${EMSDK_VERSION}
@@ -47,26 +56,47 @@ cd ..
 echo "emsdk set up."
 
 ## Qt
-echo "Setting up Qt ${QT_VERSION}"
-git clone https://code.qt.io/qt/qt5.git -b ${QT_VERSION}
-cd qt5
-./init-repository -f --module-subset=qtbase,qtcharts,qtsvg
-./configure -xplatform wasm-emscripten -feature-thread -skip webengine -nomake tests -nomake examples -no-dbus -no-ssl -no-pch -opensource -confirm-license -prefix "$PWD/../Qt5_binaries"
-make module-qtbase module-qtsvg module-qtcharts -j4
-make install -j4
-cd ..
+echo "Setting up Qt ${QT_VERSION}..."
+if [ -d "Qt5_binaries" ]; then
+	echo "Found Qt5_binaries folder."
+else
+	echo "Qt5_binaries folder not found. Fetching repo and building..."
+	git clone https://code.qt.io/qt/qt5.git -b ${QT_VERSION}
+	cd qt5
+	./init-repository -f --module-subset=qtbase,qtcharts,qtsvg
+	./configure -xplatform wasm-emscripten -feature-thread -skip webengine -nomake tests -nomake examples -no-dbus -no-ssl -no-pch -opensource -confirm-license -prefix "$PWD/../Qt5_binaries"
+	make module-qtbase module-qtsvg module-qtcharts -j4
+	make install -j4
+	cd ..
+fi
 echo "Qt set up."
 
 ## Building the project
 echo "Building source repo..."
-git clone ${SOURCE_REPO}
 REPO_FOLDER=$(echo "${SOURCE_REPO}" | grep -oP '[^/]*(?=\.git$)')
+if [ -d "${REPO_FOLDER}" ]; then
+	echo "Found existing source repo."
+else
+	echo "Cloning source repo..."
+	git clone ${SOURCE_REPO}
+fi
 cd ${REPO_FOLDER}
 ../Qt5_binaries/bin/qmake -r MNECPP_CONFIG=wasm
 make -j4
 GIT_HASH=$(git rev-parse HEAD)
 cd ..
 echo "Done"
+
+## WASM coi fix
+if [ -d "coi-serviceworker" ]; then
+	echo "Found existing coi fix repo."
+else
+	echo "Cloning coi fix repo..."
+	git clone https://github.com/gzuidhof/coi-serviceworker.git
+fi
+cp coi-serviceworker/coi-serviceworker.js mne-cpp/bin
+sed -i '0,/.*<script.*/s//\t<script src=\"coi-serviceworker.js\"><\/script>\n&/' mne-cpp/bin/mne_anonymize.html
+sed -i '0,/.*<script.*/s//\t<script src=\"coi-serviceworker.js\"><\/script>\n&/' mne-cpp/bin/mne_analyze.html
 
 ## Build log
 echo "Saving build and system parameters..."
@@ -75,7 +105,7 @@ echo "System Info: $(uname -rvop)" >> build_info.txt
 echo "emsdk version: ${EMSDK_VERSION}" >> build_info.txt
 echo "Qt version: ${QT_VERSION}" >> build_info.txt
 echo "Build time: $(date)" >> build_info.txt
-echo "Build git hash: ${GIT_HASH}"
+echo "Build git hash: ${GIT_HASH}" >> build_info.txt
 echo "Saved."
 
 exit 0
