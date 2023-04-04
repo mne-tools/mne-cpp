f5cdce4a69215205496f206705a51c963df2f3ef
JuanGPC <juangpc@users.noreply.github.com>
2023-04-04 19:43:46 -0400

    update main plugin to show import resolution

--- resources/mne_scan/plugins/fieldline/main.py
+++ resources/mne_scan/plugins/fieldline/main.py
@@ -1,1 +1,12 @@
-print("hello pepe!!")
+"""This is my super module"""
+import sys
+
+for path in sys.path:
+    print(path)
+
+print("sys.executable: " + str(sys.executable))
+
+print("hello pepe vamos vamos!!!")
+
+from test import core
+core.sayHi("Antonio")
